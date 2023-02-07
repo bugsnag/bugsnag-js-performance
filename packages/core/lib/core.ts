ed97b99b0f30e1656fe51acd753342b7e89989aa
Ben Wilson <ben.wilson@smartbear.com>
2023-02-07 15:12:51 +0000

    validate start method configuration

--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -1,18 +1,59 @@
+interface Logger {
+  debug: (msg: string) => void
+  info: (msg: string) => void
+  warn: (msg: string) => void
+  error: (msg: string) => void
+}
+
 export interface Configuration {
   apiKey: string
   endpoint?: string
   releaseStage?: string
+  logger?: Logger
 }
 
 export interface BugsnagPerformance {
   start: (config: Configuration | string) => void
 }
 
+function isObject (obj: unknown): obj is Record<string, unknown> {
+  return !!obj && typeof obj === 'object'
+}
+
+function validate (config: unknown): Configuration {
+  if (typeof config === 'string') { config = { apiKey: config } }
+
+  if (!isObject(config) || typeof config.apiKey !== 'string') {
+    throw new Error('No Bugsnag API Key set')
+  }
+
+  const cleanConfiguration: Configuration = {
+    apiKey: config.apiKey
+  }
+
+  if (config.endpoint) {
+    if (typeof config.endpoint === 'string') {
+      cleanConfiguration.endpoint = config.endpoint
+    } else {
+      console.warn(`Invalid configuration. endpoint should be a string, got ${typeof config.endpoint}`)
+    }
+  }
+
+  if (config.releaseStage) {
+    if (typeof config.releaseStage === 'string') {
+      cleanConfiguration.releaseStage = config.releaseStage
+    } else {
+      console.warn(`Invalid configuration. releaseStage should be a string, got ${typeof config.releaseStage}`)
+    }
+  }
+
+  return cleanConfiguration
+}
+
 export function createClient (): BugsnagPerformance {
   return {
     start: (config: Configuration | string) => {
-      if (typeof config === 'string') { config = { apiKey: config } }
-      if (!config?.apiKey) throw new Error('No Bugsnag API Key set')
+      config = validate(config)
     }
   }
 }
