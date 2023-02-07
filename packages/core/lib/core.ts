08173af0f38d947df3bd7e35a6663017cf6999ad
Ben Wilson <ben.wilson@smartbear.com>
2023-02-07 12:13:10 +0000

    feat: :sparkles: add start method and interfaces to core createClient

--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -1,7 +1,20 @@
-interface StartParams {
+export interface Configuration {
   apiKey: string
+  endpoint?: string
+  releaseStage?: string
 }
 
-export const createClient = () => ({
-  start: ({ apiKey }: StartParams) => {}
-})
+export interface BugsnagPerformance {
+  start: (config: Configuration | string) => void
+}
+
+export function createClient (): BugsnagPerformance {
+  return {
+    start: (config: Configuration | string) => {
+      if (typeof config === 'string') { config = { apiKey: config } }
+      if (!config?.apiKey) throw new Error('No Bugsnag API Key set')
+    }
+  }
+}
+
+export default createClient
