714b36b673db0e77837890c19f413423885344bb
Ben Wilson <ben.wilson@smartbear.com>
2023-02-07 15:33:11 +0000

    refactor: :recycle: reduce nesting in config validator

--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -31,20 +31,16 @@ function validate (config: unknown): Configuration {
     apiKey: config.apiKey
   }
 
-  if (config.endpoint) {
-    if (typeof config.endpoint === 'string') {
-      cleanConfiguration.endpoint = config.endpoint
-    } else {
-      console.warn(`Invalid configuration. endpoint should be a string, got ${typeof config.endpoint}`)
-    }
+  if ('endpoint' in config) {
+    typeof config.endpoint === 'string'
+      ? cleanConfiguration.endpoint = config.endpoint
+      : console.warn(`Invalid configuration. endpoint should be a string, got ${typeof config.endpoint}`)
   }
 
-  if (config.releaseStage) {
-    if (typeof config.releaseStage === 'string') {
-      cleanConfiguration.releaseStage = config.releaseStage
-    } else {
-      console.warn(`Invalid configuration. releaseStage should be a string, got ${typeof config.releaseStage}`)
-    }
+  if ('releaseStage' in config) {
+    typeof config.releaseStage === 'string'
+      ? cleanConfiguration.releaseStage = config.releaseStage
+      : console.warn(`Invalid configuration. releaseStage should be a string, got ${typeof config.releaseStage}`)
   }
 
   return cleanConfiguration
