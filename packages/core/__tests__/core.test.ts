5ab15b82d333f60e2936b14fe970f26cc6f47a06
Benedict Wilson <ben.wilson@smartbear.com>
2023-02-08 14:09:45 +0000

    [PLAT-9513] Create core client and configuration classes (#6)
    * add basic jest test for core package
    
    * feat: :sparkles: add start method and interfaces to core createClient
    
    * test: :white_check_mark: add unit tests for createClient and start method
    
    * remove unnecessary build script

--- packages/core/__tests__/core.test.ts
+++ packages/core/__tests__/core.test.ts
@@ -1,7 +1,51 @@
-import core from '../lib/core'
+import { createClient } from '../lib/core'
 
-describe('core', () => {
-  it('returns a string', () => {
-    expect(core()).toStrictEqual('Hello from core')
+describe('Core', () => {
+  describe('createClient()', () => {
+    it('returns a performance client', () => {
+      const testClient = createClient()
+      expect(testClient).toMatchObject({
+        start: expect.any(Function)
+      })
+    })
+
+    describe('BugsnagPerformance', () => {
+      describe('start()', () => {
+        it('accepts a string', () => {
+          const testClient = createClient()
+          expect(() => { testClient.start('test-api-key') }).not.toThrow()
+        })
+
+        it('accepts a minimal valid configuration object', () => {
+          const testClient = createClient()
+          expect(() => { testClient.start({ apiKey: 'test-api-key' }) }).not.toThrow()
+        })
+
+        it('accepts a complete configuration object', () => {
+          const testClient = createClient()
+          expect(() => { testClient.start({ apiKey: 'test-api-key', endpoint: '/test', releaseStage: 'test' }) }).not.toThrow()
+        })
+
+        it('throws if no configuration is provided', () => {
+          const testClient = createClient()
+          // @ts-expect-error no configuration provided
+          expect(() => { testClient.start() }).toThrow('No Bugsnag API Key set')
+        })
+
+        it.each([
+          [],
+          { invalidConfiguration: 'test' },
+          null,
+          true,
+          false,
+          1,
+          Symbol('test symbol')
+        ])('throws if provided configuration is %s', (config) => {
+          const testClient = createClient()
+          // @ts-expect-error invalid configuration provided
+          expect(() => { testClient.start(config) }).toThrow('No Bugsnag API Key set')
+        })
+      })
+    })
   })
 })
--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -1,5 +1,20 @@
-export function core () {
-  return 'Hello from core'
+export interface Configuration {
+  apiKey: string
+  endpoint?: string
+  releaseStage?: string
 }
 
-export default core
+export interface BugsnagPerformance {
+  start: (config: Configuration | string) => void
+}
+
+export function createClient (): BugsnagPerformance {
+  return {
+    start: (config: Configuration | string) => {
+      if (typeof config === 'string') { config = { apiKey: config } }
+      if (!config?.apiKey) throw new Error('No Bugsnag API Key set')
+    }
+  }
+}
+
+export default createClient
