a8ff0e6ce525adf8675e563f8ede21160bf1c5ec
Benedict Wilson <ben.wilson@smartbear.com>
2023-02-08 16:06:44 +0000

    [PLAT-9513] Validate BugsnagPerformance.start() config (#7)
    * validate start method configuration
    
    * refactor: :recycle: reduce nesting in config validator
    
    * Use config.logger if provided
    
    * Simplify checks for endpoint & releaseStage
    
    * disable jest/no-standalone-expect
    
    * Remove 'expect' from 'afterEach'
    
    ---------
    
    Co-authored-by: Joe Haines <hello@joehaines.co.uk>

--- packages/core/__tests__/core.test.ts
+++ packages/core/__tests__/core.test.ts
@@ -11,19 +11,96 @@ describe('Core', () => {
 
     describe('BugsnagPerformance', () => {
       describe('start()', () => {
+        beforeEach(() => {
+          jest.restoreAllMocks()
+          jest.spyOn(console, 'debug')
+          jest.spyOn(console, 'info')
+          jest.spyOn(console, 'warn')
+          jest.spyOn(console, 'error')
+        })
+
         it('accepts a string', () => {
           const testClient = createClient()
-          expect(() => { testClient.start('test-api-key') }).not.toThrow()
+          testClient.start('test-api-key')
+
+          expect(console.warn).not.toHaveBeenCalled()
         })
 
         it('accepts a minimal valid configuration object', () => {
           const testClient = createClient()
-          expect(() => { testClient.start({ apiKey: 'test-api-key' }) }).not.toThrow()
+          testClient.start({ apiKey: 'test-api-key' })
+
+          expect(console.warn).not.toHaveBeenCalled()
         })
 
         it('accepts a complete configuration object', () => {
           const testClient = createClient()
-          expect(() => { testClient.start({ apiKey: 'test-api-key', endpoint: '/test', releaseStage: 'test' }) }).not.toThrow()
+          const logger = {
+            debug: jest.fn(),
+            info: jest.fn(),
+            warn: jest.fn(),
+            error: jest.fn()
+          }
+
+          testClient.start({
+            apiKey: 'test-api-key',
+            endpoint: '/test',
+            releaseStage: 'test',
+            logger
+          })
+
+          expect(logger.warn).not.toHaveBeenCalled()
+        })
+
+        const invalidParameters = [
+          { type: 'bigint', value: BigInt(9007199254740991) },
+          { type: 'boolean', value: true },
+          { type: 'function', value: () => {} },
+          { type: 'number', value: 12345 },
+          { type: 'object', value: { property: 'test' } },
+          { type: 'object', value: [] },
+          { type: 'symbol', value: Symbol('test') }
+        ]
+
+        test.each(invalidParameters)('warns if config.endpoint is invalid ($type)', ({ value, type }) => {
+          jest.spyOn(console, 'warn').mockImplementation()
+          const testClient = createClient()
+          // @ts-expect-error endpoint should be a string
+          testClient.start({ apiKey: 'test-api-key', endpoint: value })
+          expect(console.warn).toHaveBeenCalledWith(`Invalid configuration. endpoint should be a string, got ${type}`)
+        })
+
+        test.each(invalidParameters)('warns if config.releaseStage is invalid ($type)', ({ value, type }) => {
+          jest.spyOn(console, 'warn').mockImplementation()
+          const testClient = createClient()
+          // @ts-expect-error releaseStage should be a string
+          testClient.start({ apiKey: 'test-api-key', releaseStage: value })
+          expect(console.warn).toHaveBeenCalledWith(`Invalid configuration. releaseStage should be a string, got ${type}`)
+        })
+
+        test.each(invalidParameters)('warns if config.logger is invalid ($type)', ({ value, type }) => {
+          jest.spyOn(console, 'warn').mockImplementation()
+          const testClient = createClient()
+          // @ts-expect-error logger should be a logger object
+          testClient.start({ apiKey: 'test-api-key', logger: value })
+          expect(console.warn).toHaveBeenCalledWith(`Invalid configuration. logger should be a Logger object, got ${type}`)
+        })
+
+        test.each(invalidParameters)('uses config.logger if it is valid', ({ value, type }) => {
+          const testClient = createClient()
+
+          const logger = {
+            debug: jest.fn(),
+            info: jest.fn(),
+            warn: jest.fn(),
+            error: jest.fn()
+          }
+
+          // @ts-expect-error logger should be a logger object
+          testClient.start({ apiKey: 'test-api-key', logger, endpoint: value, releaseStage: value })
+
+          expect(logger.warn).toHaveBeenCalledWith(`Invalid configuration. endpoint should be a string, got ${type}`)
+          expect(logger.warn).toHaveBeenCalledWith(`Invalid configuration. releaseStage should be a string, got ${type}`)
         })
 
         it('throws if no configuration is provided', () => {
@@ -33,14 +110,16 @@ describe('Core', () => {
         })
 
         it.each([
-          [],
-          { invalidConfiguration: 'test' },
-          null,
-          true,
-          false,
-          1,
-          Symbol('test symbol')
-        ])('throws if provided configuration is %s', (config) => {
+          { type: 'a bigint', config: BigInt(9007199254740991) },
+          { type: 'a function', config: () => {} },
+          { type: 'a number', config: 12345 },
+          { type: 'boolean (true)', config: true },
+          { type: 'boolean (false)', config: false },
+          { type: 'null', config: null },
+          { type: 'an invalid configuration object', config: { property: 'test' } },
+          { type: 'an array', config: [] },
+          { type: 'a symbol', config: Symbol('test') }
+        ])('throws if provided configuration is $type', ({ config }) => {
           const testClient = createClient()
           // @ts-expect-error invalid configuration provided
           expect(() => { testClient.start(config) }).toThrow('No Bugsnag API Key set')
--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -1,18 +1,81 @@
+interface Logger {
+  debug: (msg: string) => void
+  info: (msg: string) => void
+  warn: (msg: string) => void
+  error: (msg: string) => void
+}
+
 export interface Configuration {
   apiKey: string
   endpoint?: string
   releaseStage?: string
+  logger?: Logger
 }
+
+type InternalConfiguration = Required<Configuration>
 
 export interface BugsnagPerformance {
   start: (config: Configuration | string) => void
 }
 
+function isObject (obj: unknown): obj is Record<string, unknown> {
+  return !!obj && typeof obj === 'object'
+}
+
+function isLogger (object: unknown): object is Logger {
+  return isObject(object) &&
+    typeof object.debug === 'function' &&
+    typeof object.info === 'function' &&
+    typeof object.warn === 'function' &&
+    typeof object.error === 'function'
+}
+
+const defaultLogger: Logger = {
+  debug (message: string) { console.debug(message) },
+  info (message: string) { console.info(message) },
+  warn (message: string) { console.warn(message) },
+  error (message: string) { console.error(message) }
+}
+
+function validate (config: unknown): InternalConfiguration {
+  if (typeof config === 'string') { config = { apiKey: config } }
+
+  if (!isObject(config) || typeof config.apiKey !== 'string') {
+    throw new Error('No Bugsnag API Key set')
+  }
+
+  const cleanConfiguration: InternalConfiguration = {
+    apiKey: config.apiKey,
+    endpoint: 'https://otlp.bugsnag.com/v1/traces',
+    releaseStage: 'production', // TODO: this should have a sensible default based on platform
+    logger: defaultLogger
+  }
+
+  if (isLogger(config.logger)) {
+    cleanConfiguration.logger = config.logger
+  } else if (config.logger !== undefined) {
+    cleanConfiguration.logger.warn(`Invalid configuration. logger should be a Logger object, got ${typeof config.logger}`)
+  }
+
+  if (typeof config.endpoint === 'string') {
+    cleanConfiguration.endpoint = config.endpoint
+  } else if (config.endpoint !== undefined) {
+    cleanConfiguration.logger.warn(`Invalid configuration. endpoint should be a string, got ${typeof config.endpoint}`)
+  }
+
+  if (typeof config.releaseStage === 'string') {
+    cleanConfiguration.releaseStage = config.releaseStage
+  } else if (config.releaseStage !== undefined) {
+    cleanConfiguration.logger.warn(`Invalid configuration. releaseStage should be a string, got ${typeof config.releaseStage}`)
+  }
+
+  return cleanConfiguration
+}
+
 export function createClient (): BugsnagPerformance {
   return {
     start: (config: Configuration | string) => {
-      if (typeof config === 'string') { config = { apiKey: config } }
-      if (!config?.apiKey) throw new Error('No Bugsnag API Key set')
+      config = validate(config)
     }
   }
 }
