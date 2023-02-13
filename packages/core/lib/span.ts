710576a2b7db1fdb6c32ec8f7bf2c09e917d58ab
Ben Wilson <ben.wilson@smartbear.com>
2023-02-13 17:30:36 +0000

    add ResourceAttributes interface and SpanAttributes class

--- packages/core/lib/span.ts
+++ packages/core/lib/span.ts
@@ -9,6 +9,40 @@ export interface SpanInternal {
   readonly name: string
   readonly kind: 'internal' | 'server' | 'client' | 'producer' | 'consumer'
   readonly traceId: string // 128 bit random string
+  readonly attributes: SpanAttributes
   readonly startTime: number // stored in the format returned from Clock.now (see clock.ts)
   endTime?: number // stored in the format returned from Clock.now (see clock.ts) - written once when 'end' is called
+}
+
+export interface ResourceAttributes {
+  brands: Array<{ name: string, version: string }> // browser.brands
+  platform: string // browser.platform
+  mobile: boolean // browser.mobile
+  userAgent: string // browser.user_agent
+  releaseStage: string // deployment.environment
+  sdkName: string // telemetry.sdk.name
+  sdkVersion: string // telemetry.sdk.version
+}
+
+type Attribute = string | number | boolean
+
+export type DefaultSpanAttributes = Record<string, Attribute | (() => Attribute)>
+
+export class SpanAttributes {
+  private readonly attributes = new Map()
+
+  constructor (initialValues?: Record<string, Attribute>) {
+    if (initialValues) {
+      this.attributes = new Map(Object.entries(initialValues))
+    }
+  }
+
+  public set (name: string, value: Attribute) {
+    // TODO: Validation
+    this.attributes.set(name, value)
+  }
+
+  public remove (name: string) {
+    this.attributes.delete(name)
+  }
 }
