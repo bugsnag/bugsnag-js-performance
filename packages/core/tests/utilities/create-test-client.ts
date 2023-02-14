467ba609fafcf6c291c4f95c14958f224a388f90
Ben Wilson <ben.wilson@smartbear.com>
2023-02-14 09:16:21 +0000

    create resource attributes for test client

--- packages/core/tests/utilities/create-test-client.ts
+++ packages/core/tests/utilities/create-test-client.ts
@@ -1,11 +1,12 @@
 import { createClient } from '../../lib/core'
 import type { ClientOptions, BugsnagPerformance } from '../../lib/core'
-import { InMemoryProcessor, StableIdGenerator, IncrementingClock } from '.'
+import { InMemoryProcessor, StableIdGenerator, IncrementingClock, resourceAttributes } from '.'
 
 const defaultOptions = () => ({
   processor: new InMemoryProcessor(),
   idGenerator: new StableIdGenerator(),
-  clock: new IncrementingClock()
+  clock: new IncrementingClock(),
+  resourceAttributes
 })
 
 function createTestClient (optionOverrides: Partial<ClientOptions> = {}): BugsnagPerformance {
---
+++ packages/core/tests/utilities/resource-attributes.ts
@@ -0,0 +1,11 @@
+import { type ResourceAttributes } from '../../lib/span'
+
+export const resourceAttributes: ResourceAttributes = {
+  brands: [{ name: 'Test Browser', version: '2.0' }],
+  mobile: false,
+  platform: 'test fixture',
+  releaseStage: 'test',
+  sdkName: 'bugsnag.performance.browser',
+  sdkVersion: '1.2.3',
+  userAgent: 'Mozilla/5.0 (darwin) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/20.0.3'
+}
