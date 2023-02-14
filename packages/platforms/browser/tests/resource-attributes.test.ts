b99619d30aacfa439f79ae249144adc56e1b58ad
Ben Wilson <ben.wilson@smartbear.com>
2023-02-14 09:35:44 +0000

    test: :white_check_mark: update unit test to expect useragent regex

--- packages/platforms/browser/tests/resource-attributes.test.ts
+++ packages/platforms/browser/tests/resource-attributes.test.ts
@@ -4,12 +4,10 @@
 
 import { resourceAttributes } from '../lib/resource-attributes'
 
-const jsDomUserAgent = 'Mozilla/5.0 (darwin) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/20.0.3'
-
 describe('resourceAttributes', () => {
   it('includes navigator.userAgent', () => {
     expect(resourceAttributes).toEqual(expect.objectContaining({
-      userAgent: jsDomUserAgent
+      userAgent: expect.stringMatching(/\((?<info>.*?)\)(\s|$)|(?<name>.*?)\/(?<version>.*?)(\s|$)/gm)
     }))
   })
 })
