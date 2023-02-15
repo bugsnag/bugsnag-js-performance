449d4cd662519275593b355cb17545d7b27650fa
Ben Wilson <ben.wilson@smartbear.com>
2023-02-15 16:30:55 +0000

    remove unnecessary console log

--- packages/platforms/browser/tests/span-attributes-source.test.ts
+++ packages/platforms/browser/tests/span-attributes-source.test.ts
@@ -6,9 +6,6 @@ import { spanAttributesSource } from '../lib/span-attributes-source'
 
 describe('spanAttributesSource', () => {
   it('includes the userAgent', () => {
-    const attributes = spanAttributesSource()
-
-    console.log({ attributes })
     expect(spanAttributesSource()).toEqual(expect.objectContaining({
       'browser.page.url': expect.stringMatching(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}(\.[a-z]{2,4})?\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g) // URL Regex including http://localhost
     }))
