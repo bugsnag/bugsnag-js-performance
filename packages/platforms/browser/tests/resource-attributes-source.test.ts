587c3ab1f9310c19bd97f8bfa94b046343a46f77
Ben Wilson <ben.wilson@smartbear.com>
2023-02-16 11:58:46 +0000

    test: :white_check_mark: fix resourceAttributesSource test

--- packages/platforms/browser/tests/resource-attributes-source.test.ts
+++ packages/platforms/browser/tests/resource-attributes-source.test.ts
@@ -2,10 +2,11 @@
  * @jest-environment jsdom
  */
 
-import { resourceAttributesSource } from '../lib/resource-attributes-source'
+import createResourceAttributesSource from '../lib/resource-attributes-source'
 
 describe('resourceAttributesSource', () => {
   it('includes the userAgent', () => {
+    const resourceAttributesSource = createResourceAttributesSource(navigator)
     expect(resourceAttributesSource()).toEqual(expect.objectContaining({
       userAgent: expect.stringMatching(/\((?<info>.*?)\)(\s|$)|(?<name>.*?)\/(?<version>.*?)(\s|$)/gm),
       releaseStage: expect.any(String),
