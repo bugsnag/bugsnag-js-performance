f5aaf877d30bd60219372ba220a6dcca810f3ef6
Ben Wilson <ben.wilson@smartbear.com>
2023-02-16 11:28:20 +0000

    chore: :label: simplify user agent data types

--- packages/platforms/browser/navigator-user-agent-data.d.ts
+++ packages/platforms/browser/navigator-user-agent-data.d.ts
@@ -1,39 +1,14 @@
-interface NavigatorUABrandVersion {
+declare interface Navigator {
+  readonly userAgentData?: NavigatorUserAgentData
+}
+
+interface NavigatorUserAgentData {
+  readonly brands: BrandVersion[]
+  readonly mobile: boolean
+  readonly platform: string
+}
+
+interface BrandVersion {
   readonly brand: string
   readonly version: string
 }
-
-interface UADataValues {
-  readonly brands?: NavigatorUABrandVersion[]
-  readonly mobile?: boolean
-  readonly architecture?: string
-  readonly bitness?: string
-  readonly model?: string
-  readonly platform?: string
-  readonly platformVersion?: string
-  /** @deprecated in favour of fullVersionList */
-  readonly uaFullVersion?: string
-  readonly wow64?: boolean
-  readonly fullVersionList?: NavigatorUABrandVersion[]
-}
-
-interface UALowEntropyJSON {
-  readonly brands: NavigatorUABrandVersion[]
-  readonly mobile: boolean
-  readonly platform: string
-}
-
-interface NavigatorUAData {
-  readonly brands: NavigatorUABrandVersion[]
-  readonly mobile: boolean
-  readonly platform: string
-  getHighEntropyValues: (hints: string[]) => Promise<UADataValues>
-  toJSON: () => UALowEntropyJSON
-}
-
-declare interface NavigatorUA {
-  readonly userAgentData?: NavigatorUAData
-}
-
-declare interface Navigator extends NavigatorUA {}
-declare interface WorkerNavigator extends NavigatorUA {}
