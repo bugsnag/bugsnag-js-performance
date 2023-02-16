99d35718d3bb4eff553188042dfda739cfc8f8c5
Ben Wilson <ben.wilson@smartbear.com>
2023-02-16 11:54:37 +0000

    refactor: :recycle: wrap browser resourceAttributes in closure
    ensure resourceAttributesSource has access to global navigator property at time of client creation

--- packages/platforms/browser/lib/browser.ts
+++ packages/platforms/browser/lib/browser.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@bugsnag/js-performance-core'
 import clock from './clock'
 import idGenerator from './id-generator'
-import { resourceAttributesSource } from './resource-attributes-source'
+import createResourceAttributesSource from './resource-attributes-source'
 import { spanAttributesSource } from './span-attributes-source'
 
 const BugsnagPerformance = createClient({
   clock,
-  resourceAttributesSource,
+  resourceAttributesSource: createResourceAttributesSource(navigator),
   spanAttributesSource,
   idGenerator,
   processor: { add: () => {} }
--- packages/platforms/browser/lib/resource-attributes-source.ts
+++ packages/platforms/browser/lib/resource-attributes-source.ts
@@ -1,20 +1,22 @@
 import type { ResourceAttributes } from '@bugsnag/js-performance-core/lib/span'
 
-export function resourceAttributesSource (): ResourceAttributes {
-  return {
-    releaseStage: process.env.NODE_ENV || 'production',
-    sdkName: 'bugsnag.performance.browser',
-    sdkVersion: process.env.NPM_PACKAGE_VERSION || 'unknown',
-    userAgent: navigator.userAgent,
-    // chromium only
-    ...(navigator.userAgentData
-      ? {
-          brands: navigator.userAgentData.brands.map(({ brand, version }) => ({ name: brand, version })),
-          platform: navigator.userAgentData.platform,
-          mobile: navigator.userAgentData.mobile
-        }
-      : {})
+function createResourceAttributesSource (navigator: Navigator): () => ResourceAttributes {
+  return function resourceAttributesSource () {
+    return {
+      releaseStage: process.env.NODE_ENV || 'production',
+      sdkName: 'bugsnag.performance.browser',
+      sdkVersion: process.env.NPM_PACKAGE_VERSION || 'unknown',
+      userAgent: navigator.userAgent,
+      // chromium only
+      ...(navigator.userAgentData
+        ? {
+            brands: navigator.userAgentData.brands.map(({ brand, version }) => ({ name: brand, version })),
+            platform: navigator.userAgentData.platform,
+            mobile: navigator.userAgentData.mobile
+          }
+        : {})
+    }
   }
 }
 
-export default resourceAttributesSource
+export default createResourceAttributesSource
