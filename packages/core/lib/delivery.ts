314d7422283b4918d89e821a769f2be7ced49b3e
Ben Wilson <ben.wilson@smartbear.com>
2023-02-20 15:32:23 +0000

    feat: :label: create Delivery type definition

---
+++ packages/core/lib/delivery.ts
@@ -0,0 +1,11 @@
+import type { ResourceAttributes } from './attributes'
+import type { SpanInternal } from './span'
+
+export interface Delivery {
+  send: (
+    endpoint: string,
+    apiKey: string,
+    spans: SpanInternal[],
+    resourceAtrributes: ResourceAttributes
+  ) => Promise<void> // this will become some kind of Response type when we capture p-values, for now we don't care
+}
