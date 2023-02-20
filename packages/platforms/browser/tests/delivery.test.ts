2cf9526c4908ea1476e0912cf81cb1f0ecb15f8a
Ben Wilson <ben.wilson@smartbear.com>
2023-02-20 15:36:17 +0000

    test: :white_check_mark: write basic delivery test (mocking fetch)

---
+++ packages/platforms/browser/tests/delivery.test.ts
@@ -0,0 +1,39 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { SpanAttributes, type SpanInternal } from '@bugsnag/js-performance-core/lib/span'
+import delivery from '../lib/delivery'
+import createResourseAttributesSource from '../lib/resource-attributes-source'
+import createSpanAttributesSource from '../lib/span-attributes-source'
+
+// @ts-expect-error don't overwrite global.fetch
+global.fetch = jest.fn(() =>
+  Promise.resolve()
+)
+
+beforeEach(() => {
+// @ts-expect-error don't overwrite global.fetch
+  fetch.mockClear()
+})
+
+describe('delivery', () => {
+  it('delivers a span', () => {
+    const resourceAttributesSource = createResourseAttributesSource(navigator)
+    const spanAttributesSource = createSpanAttributesSource()
+    const spanAttributes = new SpanAttributes(spanAttributesSource)
+    const spans: SpanInternal[] = [{
+      id: 'unique-span-id',
+      kind: 'internal',
+      name: 'test span',
+      startTime: 12345,
+      traceId: 'trace-span-id',
+      endTime: 56789,
+      attributes: spanAttributes
+    }]
+
+    delivery.send('/test', 'test-api-key', spans, resourceAttributesSource())
+
+    expect(global.fetch).toHaveBeenCalledWith('/test', expect.objectContaining({ body: expect.stringContaining('test span') }))
+  })
+})
