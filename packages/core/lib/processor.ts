c4d093f6eb43cce40e3379e812bb6fb3155820f0
Ben Wilson <ben.wilson@smartbear.com>
2023-02-20 15:31:31 +0000

    feat: :label: add configure method to Processor interface

--- packages/core/lib/processor.ts
+++ packages/core/lib/processor.ts
@@ -1,7 +1,10 @@
+import type { ResourceAttributes } from './attributes'
+import type { Configuration } from './core'
 import type { SpanInternal } from './span'
 
 // processor.add is called by a Span when 'Span.end' is called
 // it can then add to a queue or send immediately
 export interface Processor {
   add: (span: SpanInternal) => void
+  configure: (config: Required<Configuration>, resourceAttributesSource: () => ResourceAttributes) => void
 }
