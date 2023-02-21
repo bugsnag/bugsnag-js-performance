0576abae9cab505314de43366c87e805f19c4eb6
Ben Wilson <ben.wilson@smartbear.com>
2023-02-21 15:39:41 +0000

    test: :white_check_mark: test configuration, delivery and payload formatting in processor

---
+++ packages/platforms/browser/tests/processor.test.ts
@@ -0,0 +1,90 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { SpanAttributes, type SpanInternal } from '@bugsnag/js-performance-core/lib/span'
+import createProcessor from '../lib/processor'
+import resourceAttributesSource from '../lib/resource-attributes-source'
+
+const testLogger = {
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn()
+}
+
+describe('Browser Processor', () => {
+  it('prevents delivery until configure has been called', () => {
+    const span: SpanInternal = {
+      id: 'test-span-id',
+      kind: 'consumer',
+      name: 'test-span',
+      startTime: 12345,
+      endTime: 56789,
+      traceId: 'trace-id',
+      attributes: new SpanAttributes(new Map())
+    }
+
+    const mockDelivery = { send: jest.fn() }
+    const processor = createProcessor(mockDelivery)
+    processor.add(span)
+
+    expect(mockDelivery.send).not.toHaveBeenCalled()
+  })
+
+  it('accepts a valid configuration', () => {
+    const mockDelivery = { send: jest.fn() }
+    const processor = createProcessor(mockDelivery)
+
+    // TODO: How to make this assertion more useful?
+    expect(() => {
+      processor.configure({
+        apiKey: 'test-api-key',
+        endpoint: '/traces',
+        releaseStage: 'test',
+        logger: testLogger
+      }, resourceAttributesSource(navigator))
+    }).not.toThrow()
+  })
+
+  it('correctly formats the delivery payload', () => {
+    const span: SpanInternal = {
+      id: 'test-span-id',
+      kind: 'consumer',
+      name: 'test-span',
+      startTime: 12345,
+      endTime: 56789,
+      traceId: 'trace-id',
+      attributes: new SpanAttributes(new Map())
+    }
+
+    const mockDelivery = { send: jest.fn() }
+    const processor = createProcessor(mockDelivery)
+    processor.configure({
+      apiKey: 'test-api-key',
+      endpoint: '/traces',
+      releaseStage: 'test',
+      logger: testLogger
+    }, resourceAttributesSource(navigator))
+    processor.add(span)
+
+    expect(mockDelivery.send).toHaveBeenCalledWith(
+      '/traces', // Endpoint
+      'test-api-key', // API Key
+      expect.objectContaining({
+        resourceSpans: expect.arrayContaining([
+          expect.objectContaining({
+            resource: expect.objectContaining({
+              attributes: expect.any(Array)
+            }),
+            scopeSpans: expect.arrayContaining([
+              expect.objectContaining({
+                spans: expect.any(Array)
+              })
+            ])
+          })
+        ])
+      })
+    )
+  })
+})
