d86c103bf0089792fd1f83ab39800a09a18fa135
Ben Wilson <ben.wilson@smartbear.com>
2023-02-21 12:33:10 +0000

    processor.add noop if configure has not been called

--- packages/platforms/browser/lib/processor.ts
+++ packages/platforms/browser/lib/processor.ts
@@ -3,12 +3,16 @@ import type { Processor } from '@bugsnag/js-performance-core/lib/processor'
 import delivery from './delivery'
 
 // Can we store these somewhere better?
-let endpoint: string
-let apiKey: string
-let resourceAttributesSource: () => ResourceAttributes
+let endpoint: string | undefined
+let apiKey: string | undefined
+let resourceAttributesSource: (() => ResourceAttributes) | undefined
 
 const processor: Processor = {
   add: (span) => {
+    if (!endpoint || !apiKey || !resourceAttributesSource) {
+      return // TODO: logger.error
+    }
+
     delivery(global.fetch).send(endpoint, apiKey, [span], resourceAttributesSource())
   },
   configure: (config, attributesSource) => {
