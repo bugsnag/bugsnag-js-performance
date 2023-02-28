2603047c0c4567ca4e9485057e90c3b707bb6528
Ben Wilson <ben.wilson@smartbear.com>
2023-02-28 15:27:26 +0000

    remove js extension

--- packages/core/lib/processor.ts
+++ packages/core/lib/processor.ts
@@ -1,4 +1,4 @@
-import type { SpanInternal } from './span.js'
+import type { SpanInternal } from './span'
 
 // processor.add is called by a Span when 'Span.end' is called
 // it can then add to a queue or send immediately
