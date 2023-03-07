0cc0cb7f5d7f68713fd14f8ac345ea31e7c994c5
Ben Wilson <ben.wilson@smartbear.com>
2023-03-07 16:48:40 +0000

    test: :white_check_mark: remove useless test

--- test/browser/features/fixtures/packages/test/src/index.js
+++
@@ -1,6 +0,0 @@
-import BugsnagPerformance from '@bugsnag/js-performance-browser'
-
-const apiKey = decodeURIComponent(window.location.search.match(/API_KEY=([^&]+)/)[1])
-const endpoint = decodeURIComponent(window.location.search.match(/ENDPOINT=([^&]+)/)[1])
-
-BugsnagPerformance.start({ apiKey, endpoint })
--- test/browser/features/test.feature
+++
@@ -1,5 +0,0 @@
-Feature: e2e testing
-
-  Scenario: It works
-    When I navigate to the test URL "/test"
-    # Then I should receive no errors
