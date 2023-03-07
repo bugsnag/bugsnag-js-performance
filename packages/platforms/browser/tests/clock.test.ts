1df6a44efa95f62dddd4f5c275f52038e9c5ac10
Joe Haines <hello@joehaines.co.uk>
2023-03-07 11:30:53 +0000

    Fallback to 'navigationStart' on old Safaris
    Safari <15 doesn't support 'performance.timeOrigin', so we fallback to
    'performance.timing.navigationStart'
    
    This isn't actually browser-specific, but all of our other supported
    browsers supported it much earlier

--- packages/platforms/browser/tests/clock.test.ts
+++ packages/platforms/browser/tests/clock.test.ts
@@ -38,15 +38,33 @@ describe('Browser Clock', () => {
     })
 
     it('returns the difference between provided Date and performance.timeOrigin in nanoseconds', () => {
-      const performance: Performance = {
-        ...window.performance,
-        timeOrigin: new Date('2023-01-02T00:00:00.000Z').getTime()
+      const performance = {
+        now: () => 1234,
+        timeOrigin: new Date('2023-01-02T00:00:00.000Z').getTime(),
+        timing: {
+          // this is different to the above date, so we know which is being used
+          navigationStart: new Date('2022-01-01T00:00:00.000Z').getTime()
+        }
       }
 
       const clock = createClock(performance)
       const time = new Date('2023-01-02T00:00:00.002Z')
 
-      expect(clock.convert(time)).toEqual(2_000_000) // 2ms difference = 2million nanos
+      expect(clock.convert(time)).toEqual(2_000_000) // 2ms difference = 2 million nanos
+    })
+
+    it('works when performance.timeOrigin is not defined', () => {
+      const performance = {
+        now: () => 1234,
+        timing: {
+          navigationStart: new Date('2023-01-01T00:00:00.000Z').getTime()
+        }
+      }
+
+      const clock = createClock(performance)
+      const time = new Date('2023-01-01T00:00:00.015Z')
+
+      expect(clock.convert(time)).toEqual(15_000_000) // 15ms difference = 15 million nanos
     })
   })
 })
