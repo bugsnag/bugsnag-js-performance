b1940741e13b40ad80b67ca6b6967d770ff933e8
Joe Haines <hello@joehaines.co.uk>
2023-03-07 11:30:53 +0000

    Fallback to 'navigationStart' on old Safaris
    Safari <15 doesn't support 'performance.timeOrigin', so we fallback to
    'performance.timing.navigationStart'
    
    This isn't actually browser-specific, but all of our other supported
    browsers supported it much earlier

--- packages/platforms/browser/lib/clock.ts
+++ packages/platforms/browser/lib/clock.ts
@@ -1,17 +1,35 @@
 import { type Clock } from '@bugsnag/js-performance-core'
 
+// a cut-down PerformanceTiming interface, since we don't care about most of
+// its properties
+interface PerformanceTiming {
+  navigationStart: number
+}
+
+// the 'Performance' type says 'timeOrigin' is always available, but that's not
+// true on Safari <15 so we mark it as possibly 'undefined'
+interface PerformanceWithOptionalTimeOrigin {
+  now: () => number
+  timeOrigin?: number
+  timing: PerformanceTiming
+}
+
 const NANOSECONDS_IN_MILLISECONDS = 1_000_000
 
 function millisecondsToNanoseconds (milliseconds: number): number {
   return milliseconds * NANOSECONDS_IN_MILLISECONDS
 }
 
-function createClock (performance: Performance): Clock {
+function createClock (performance: PerformanceWithOptionalTimeOrigin): Clock {
+  const timeOrigin = performance.timeOrigin
+    ? performance.timeOrigin
+    : performance.timing.navigationStart
+
   return {
     now: () => performance.now(),
-    convert: (date) => millisecondsToNanoseconds(date.getTime() - performance.timeOrigin),
+    convert: (date) => millisecondsToNanoseconds(date.getTime() - timeOrigin),
     // convert nanoseconds since timeOrigin to full timestamp
-    toUnixTimestampNanoseconds: (time: number) => time + performance.timeOrigin
+    toUnixTimestampNanoseconds: (time: number) => timeOrigin + time
   }
 }
 
