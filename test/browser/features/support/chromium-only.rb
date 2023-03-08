12a19421675451e644cec2fddb948598a69afab5
Ben Wilson <ben.wilson@smartbear.com>
2023-03-08 15:24:15 +0000

    simplify chromium-only script

--- test/browser/features/support/chromium-only.rb
+++ test/browser/features/support/chromium-only.rb
@@ -1,36 +1,7 @@
-browsers = [
-    # bitbar
-    'chrome_108',
-    'chrome_107',
-    'chrome_106',
-    'chrome_105',
-    'chrome_104',
-    'chrome_103',
-    'chrome_72',
-    'chrome_43',
-    'edge_106',
-    'edge_105',
-    'edge_104',
-    'edge_103',
-    'edge_102',
-    'edge_101',
-    # browserstack
-    'chrome_latest',
-    'chrome_72',
-    'chrome_61',
-    'chrome_53',
-    'chrome_43',
-    'chrome_42',
-    'chrome_40',
-    'chrome_38',
-    'chrome_36',
-    'chrome_34',
-    'chrome_32',
-    'chrome_30',
-    # local testing (chromedriver)
-    'chrome'
-]
+CHROMIUM_BROWSERS = Set['chrome', 'edge'] 
 
-Before('@chromium_only') do |_scenario|    
-    skip_this_scenario unless browsers.include? Maze.config.browser
+Before('@chromium_only') do 
+    unless CHROMIUM_BROWSERS.any? { |browser| Maze.config.browser.start_with?(browser) } 
+        skip_this_scenario 
+    end 
 end
