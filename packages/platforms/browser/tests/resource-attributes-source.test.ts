ec4a9c0dc081751cfdc5a72a40aea519a685c831
Joe Haines <hello@joehaines.co.uk>
2023-03-15 09:48:48 +0000

    Consolidate valid configuration into utilities

--- packages/platforms/browser/tests/resource-attributes-source.test.ts
+++ packages/platforms/browser/tests/resource-attributes-source.test.ts
@@ -3,6 +3,7 @@
  */
 
 import createResourceAttributesSource from '../lib/resource-attributes-source'
+import { createConfiguration } from './utilities'
 
 describe('resourceAttributesSource', () => {
   it('contains expected values', () => {
@@ -13,18 +14,7 @@ describe('resourceAttributesSource', () => {
     }
 
     const resourceAttributesSource = createResourceAttributesSource(navigator)
-    const resourceAttributes = resourceAttributesSource({
-      apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      endpoint: '/traces',
-      releaseStage: 'test',
-      logger: {
-        debug: jest.fn(),
-        warn: jest.fn(),
-        info: jest.fn(),
-        error: jest.fn()
-      },
-      appVersion: '1.0.0'
-    })
+    const resourceAttributes = resourceAttributesSource(createConfiguration({ releaseStage: 'test', appVersion: '1.0.0' }))
 
     expect(resourceAttributes.toJson()).toEqual([
       { key: 'deployment.environment', value: { stringValue: 'test' } },
@@ -43,21 +33,10 @@ describe('resourceAttributesSource', () => {
     }
 
     const resourceAttributesSource = createResourceAttributesSource(navigator)
-    const resourceAttributes = resourceAttributesSource({
-      apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      endpoint: '/traces',
-      releaseStage: 'test',
-      logger: {
-        debug: jest.fn(),
-        warn: jest.fn(),
-        info: jest.fn(),
-        error: jest.fn()
-      },
-      appVersion: ''
-    })
+    const resourceAttributes = resourceAttributesSource(createConfiguration())
 
     expect(resourceAttributes.toJson()).toEqual([
-      { key: 'deployment.environment', value: { stringValue: 'test' } },
+      { key: 'deployment.environment', value: { stringValue: 'production' } },
       { key: 'telemetry.sdk.name', value: { stringValue: 'bugsnag.performance.browser' } },
       { key: 'telemetry.sdk.version', value: { stringValue: '__VERSION__' } },
       { key: 'browser.user_agent', value: { stringValue: navigator.userAgent } }
@@ -75,18 +54,7 @@ describe('resourceAttributesSource', () => {
     }
 
     const resourceAttributesSource = createResourceAttributesSource(navigator)
-    const resourceAttributes = resourceAttributesSource({
-      apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      endpoint: '/traces',
-      releaseStage: 'production',
-      logger: {
-        debug: jest.fn(),
-        warn: jest.fn(),
-        info: jest.fn(),
-        error: jest.fn()
-      },
-      appVersion: ''
-    })
+    const resourceAttributes = resourceAttributesSource(createConfiguration())
 
     expect(resourceAttributes.toJson()).toEqual([
       { key: 'deployment.environment', value: { stringValue: 'production' } },
