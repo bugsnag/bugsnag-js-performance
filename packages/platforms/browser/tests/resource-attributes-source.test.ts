ac9f10f7950bc29f3ac36a53398df858877c7241
Ben Wilson <ben.wilson@smartbear.com>
2023-03-15 11:49:51 +0000

    handle appVerison in resource-attributes

--- packages/platforms/browser/tests/resource-attributes-source.test.ts
+++ packages/platforms/browser/tests/resource-attributes-source.test.ts
@@ -23,6 +23,36 @@ describe('resourceAttributesSource', () => {
         info: jest.fn(),
         error: jest.fn()
       },
+      appVersion: '1.0.0'
+    })
+
+    expect(resourceAttributes.toJson()).toEqual([
+      { key: 'deployment.environment', value: { stringValue: 'test' } },
+      { key: 'telemetry.sdk.name', value: { stringValue: 'bugsnag.performance.browser' } },
+      { key: 'telemetry.sdk.version', value: { stringValue: '__VERSION__' } },
+      { key: 'service.version', value: { stringValue: '1.0.0' } },
+      { key: 'browser.user_agent', value: { stringValue: navigator.userAgent } }
+    ])
+  })
+
+  it('excludes service.version if appVersion is an empty string (default value)', () => {
+    const navigator = {
+      ...window.navigator,
+      userAgentData: undefined,
+      userAgent: 'a jest test, (like Gecko and WebKit and also Blink) etc...'
+    }
+
+    const resourceAttributesSource = createResourceAttributesSource(navigator)
+    const resourceAttributes = resourceAttributesSource({
+      apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      endpoint: '/traces',
+      releaseStage: 'test',
+      logger: {
+        debug: jest.fn(),
+        warn: jest.fn(),
+        info: jest.fn(),
+        error: jest.fn()
+      },
       appVersion: ''
     })
 
