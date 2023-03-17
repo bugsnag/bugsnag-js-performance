b2bcc9836268d490f37e74b151d13982bff0c393
Ben Wilson <ben.wilson@smartbear.com>
2023-03-17 11:34:57 +0000

    directly import jest matchers

--- packages/core/tests/utilities/index.ts
+++ packages/core/tests/utilities/index.ts
@@ -1,3 +1,5 @@
+import './to-have-processed-span'
+
 export { default as createTestClient } from './create-test-client'
 export { default as InMemoryProcessor } from './in-memory-processor'
 export { default as IncrementingClock } from './incrementing-clock'
