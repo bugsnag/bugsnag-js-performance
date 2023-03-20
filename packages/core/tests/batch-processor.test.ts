448150566869572349ce6507eaf893590c4a89c9
Ben Wilson <ben.wilson@smartbear.com>
2023-03-20 10:24:41 +0000

    get configuration from test utilities

--- packages/core/tests/batch-processor.test.ts
+++ packages/core/tests/batch-processor.test.ts
@@ -1,8 +1,8 @@
 import { SpanAttributes, type SpanEnded } from '@bugsnag/js-performance-core'
 import { BatchProcessor } from '../lib/batch-processor'
 import { randomUUID } from 'crypto'
-import { IncrementingClock, resourceAttributesSource, VALID_API_KEY } from './utilities'
-import { type Delivery, type InternalConfiguration, type Logger } from '../lib'
+import { IncrementingClock, resourceAttributesSource, VALID_CONFIGURATION } from './utilities'
+import { type Delivery } from '../lib'
 
 jest.useFakeTimers()
 
@@ -24,24 +24,7 @@ describe('BatchProcessor', () => {
   it('delivers after reaching the specified span limit', () => {
     const clock = new IncrementingClock('1970-01-01T00:00:00Z')
     const delivery: Delivery = { send: jest.fn() }
-    const logger: Logger = {
-      info: jest.fn(),
-      warn: jest.fn(),
-      error: jest.fn(),
-      debug: jest.fn()
-    }
-    const config: InternalConfiguration = {
-      apiKey: VALID_API_KEY,
-      releaseStage: 'test',
-      enabledReleaseStages: ['test'],
-      appVersion: '1.0.0',
-      batchInactivityTimeoutMs: 30 * 1000,
-      maximumBatchSize: 100,
-      endpoint: '/traces',
-      logger
-    }
-
-    const batchProcessor = new BatchProcessor(delivery, config, resourceAttributesSource, clock)
+    const batchProcessor = new BatchProcessor(delivery, VALID_CONFIGURATION, resourceAttributesSource, clock)
 
     // add 99 spans
     for (let i = 0; i < 99; i++) {
@@ -58,23 +41,7 @@ describe('BatchProcessor', () => {
   it('delivers after the specified time limit', () => {
     const clock = new IncrementingClock('1970-01-01T00:00:00Z')
     const delivery: Delivery = { send: jest.fn() }
-    const logger: Logger = {
-      info: jest.fn(),
-      warn: jest.fn(),
-      error: jest.fn(),
-      debug: jest.fn()
-    }
-    const config: InternalConfiguration = {
-      apiKey: VALID_API_KEY,
-      releaseStage: 'test',
-      enabledReleaseStages: ['test'],
-      appVersion: '1.0.0',
-      batchInactivityTimeoutMs: 30 * 1000,
-      maximumBatchSize: 100,
-      endpoint: '/traces',
-      logger
-    }
-    const batchProcessor = new BatchProcessor(delivery, config, resourceAttributesSource, clock)
+    const batchProcessor = new BatchProcessor(delivery, VALID_CONFIGURATION, resourceAttributesSource, clock)
     batchProcessor.add(generateSpan())
     expect(delivery.send).not.toHaveBeenCalled()
 
