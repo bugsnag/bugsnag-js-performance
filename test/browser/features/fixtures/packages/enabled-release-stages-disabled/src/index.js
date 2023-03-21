262135abd8ad118e834ef3300b54ee74294cdb7b
Ben Wilson <ben.wilson@smartbear.com>
2023-03-21 16:28:15 +0000

    test: :white_check_mark: add test for positive scenario

--- test/browser/features/fixtures/packages/enabled-release-stages-disabled/src/index.js
+++ test/browser/features/fixtures/packages/enabled-release-stages-disabled/src/index.js
@@ -3,6 +3,6 @@ import BugsnagPerformance from '@bugsnag/js-performance-browser'
 const apiKey = decodeURIComponent(window.location.search.match(/API_KEY=([^&]+)/)[1])
 const endpoint = decodeURIComponent(window.location.search.match(/ENDPOINT=([^&]+)/)[1])
 
-BugsnagPerformance.start({ apiKey, endpoint, enabledReleaseStages: ['test'], releaseStage: 'test', maximumBatchSize: 1 })
+BugsnagPerformance.start({ apiKey, endpoint, releaseStage: 'test', enabledReleaseStages: ['production'], maximumBatchSize: 1 })
 
-BugsnagPerformance.startSpan('Custom/Should send').end()
+BugsnagPerformance.startSpan('Custom/Should not send').end()
