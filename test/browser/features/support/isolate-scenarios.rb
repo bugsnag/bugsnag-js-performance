7933235e5548d93715dce4995fe2ebb6f848c962
Ben Wilson <ben.wilson@smartbear.com>
2023-03-29 12:29:17 +0100

    isolate scenarios to prevent trace bleed

---
+++ test/browser/features/support/isolate-scenarios.rb
@@ -0,0 +1,14 @@
+Maze.hooks.after do
+  path = $browser.url_for("/")
+  
+  begin
+    $logger.debug "Navigating to: #{path}"
+    Maze.driver.navigate.to path
+  rescue => exception
+    $logger.error("#{exception.class} occurred during navigation attempt with message: #{exception.message}")
+    $logger.error("Restarting driver and retrying navigation to: #{path}")
+    Maze.driver.restart_driver
+    Maze.driver.navigate.to path
+    # If a further error occurs it will get thrown as normal
+  end
+end
