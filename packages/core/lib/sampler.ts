5f747beb0f094113ebc04a843f62612e7e9e5b07
Joe Haines <hello@joehaines.co.uk>
2023-04-06 12:12:25 +0100

    Add Sampler class

---
+++ packages/core/lib/sampler.ts
@@ -0,0 +1,40 @@
+// sampling rates are stored as a number between 0 and 0xffffffff (i.e. they are
+// u32s) so we need to scale the probability value to match this range as they
+// are stored as values between 0 and 1
+function scaleProbabilityToMatchSamplingRate (probability: number): number {
+  return Math.floor(probability * 0xffffffff)
+}
+
+class Sampler {
+  private _probability: number
+
+  /**
+   * The current probability scaled to match sampling rate
+   *
+   * @see scaleProbabilityToMatchSamplingRate
+   */
+  private scaledProbability: number
+
+  constructor (initialProbability: number) {
+    // we could just do 'this.probability = initialProbability' but TypeScript
+    // doesn't like that as it doesn't directly initialise these properties in
+    // the constructor
+    this._probability = initialProbability
+    this.scaledProbability = scaleProbabilityToMatchSamplingRate(initialProbability)
+  }
+
+  get probability (): number {
+    return this._probability
+  }
+
+  set probability (probability: number) {
+    this._probability = probability
+    this.scaledProbability = scaleProbabilityToMatchSamplingRate(probability)
+  }
+
+  sample (samplingRate: number): boolean {
+    return samplingRate <= this.scaledProbability
+  }
+}
+
+export default Sampler
---
+++ packages/core/tests/sampler.test.ts
@@ -0,0 +1,118 @@
+import Sampler from '../lib/sampler'
+
+describe('Sampler', () => {
+  it('uses the initial probability', () => {
+    const sampler = new Sampler(1.0)
+
+    expect(sampler.probability).toBe(1.0)
+  })
+
+  it('uses the probability when set', () => {
+    const sampler = new Sampler(1.0)
+    sampler.probability = 0.25
+
+    expect(sampler.probability).toBe(0.25)
+  })
+
+  describe('sample', () => {
+    it('returns true when the samplingRate should be sampled with initial probability', () => {
+      const sampler = new Sampler(0.75)
+
+      expect(sampler.sample(Math.floor(0.5 * 0xffffffff))).toBe(true)
+    })
+
+    it('returns false when the samplingRate should not be sampled with initial probability', () => {
+      const sampler = new Sampler(0.25)
+
+      expect(sampler.sample(Math.floor(0.5 * 0xffffffff))).toBe(false)
+    })
+
+    it('returns true when the samplingRate should be sampled with a set probability', () => {
+      const sampler = new Sampler(1.0)
+      sampler.probability = 0.75
+
+      expect(sampler.sample(Math.floor(0.5 * 0xffffffff))).toBe(true)
+    })
+
+    it('returns false when the samplingRate should not be sampled with a set probability', () => {
+      const sampler = new Sampler(1.0)
+      sampler.probability = 0.25
+
+      expect(sampler.sample(Math.floor(0.5 * 0xffffffff))).toBe(false)
+    })
+
+    it.each([
+      { probability: 1.0, rate: 1.0, expected: true },
+      { probability: 1.0, rate: 0.9, expected: true },
+      { probability: 1.0, rate: 0.8, expected: true },
+      { probability: 1.0, rate: 0.7, expected: true },
+      { probability: 1.0, rate: 0.6, expected: true },
+      { probability: 1.0, rate: 0.5, expected: true },
+      { probability: 1.0, rate: 0.4, expected: true },
+      { probability: 1.0, rate: 0.3, expected: true },
+      { probability: 1.0, rate: 0.2, expected: true },
+      { probability: 1.0, rate: 0.1, expected: true },
+      { probability: 1.0, rate: 0.0, expected: true },
+
+      { probability: 0.9, rate: 1.0, expected: false },
+      { probability: 0.8, rate: 1.0, expected: false },
+      { probability: 0.7, rate: 1.0, expected: false },
+      { probability: 0.6, rate: 1.0, expected: false },
+      { probability: 0.5, rate: 1.0, expected: false },
+      { probability: 0.4, rate: 1.0, expected: false },
+      { probability: 0.3, rate: 1.0, expected: false },
+      { probability: 0.2, rate: 1.0, expected: false },
+      { probability: 0.1, rate: 1.0, expected: false },
+      { probability: 0.0, rate: 1.0, expected: false },
+
+      { probability: 0.9, rate: 0.75, expected: true },
+      { probability: 0.8, rate: 0.75, expected: true },
+      { probability: 0.7, rate: 0.75, expected: false },
+      { probability: 0.6, rate: 0.75, expected: false },
+      { probability: 0.5, rate: 0.75, expected: false },
+      { probability: 0.4, rate: 0.75, expected: false },
+      { probability: 0.3, rate: 0.75, expected: false },
+      { probability: 0.2, rate: 0.75, expected: false },
+      { probability: 0.1, rate: 0.75, expected: false },
+      { probability: 0.0, rate: 0.75, expected: false },
+
+      { probability: 0.9, rate: 0.5, expected: true },
+      { probability: 0.8, rate: 0.5, expected: true },
+      { probability: 0.7, rate: 0.5, expected: true },
+      { probability: 0.6, rate: 0.5, expected: true },
+      { probability: 0.5, rate: 0.5, expected: true },
+      { probability: 0.4, rate: 0.5, expected: false },
+      { probability: 0.3, rate: 0.5, expected: false },
+      { probability: 0.2, rate: 0.5, expected: false },
+      { probability: 0.1, rate: 0.5, expected: false },
+      { probability: 0.0, rate: 0.5, expected: false },
+
+      { probability: 0.9, rate: 0.25, expected: true },
+      { probability: 0.8, rate: 0.25, expected: true },
+      { probability: 0.7, rate: 0.25, expected: true },
+      { probability: 0.6, rate: 0.25, expected: true },
+      { probability: 0.5, rate: 0.25, expected: true },
+      { probability: 0.4, rate: 0.25, expected: true },
+      { probability: 0.3, rate: 0.25, expected: true },
+      { probability: 0.2, rate: 0.25, expected: false },
+      { probability: 0.1, rate: 0.25, expected: false },
+      { probability: 0.0, rate: 0.25, expected: false },
+
+      { probability: 0.9, rate: 0.0, expected: true },
+      { probability: 0.8, rate: 0.0, expected: true },
+      { probability: 0.7, rate: 0.0, expected: true },
+      { probability: 0.6, rate: 0.0, expected: true },
+      { probability: 0.5, rate: 0.0, expected: true },
+      { probability: 0.4, rate: 0.0, expected: true },
+      { probability: 0.3, rate: 0.0, expected: true },
+      { probability: 0.2, rate: 0.0, expected: true },
+      { probability: 0.1, rate: 0.0, expected: true },
+      { probability: 0.0, rate: 0.0, expected: true }
+    ])('returns $expected with { probability: $probability, rate: $rate }', ({ probability, rate, expected }) => {
+      const sampler = new Sampler(1.0)
+      sampler.probability = probability
+
+      expect(sampler.sample(Math.floor(rate * 0xffffffff))).toBe(expected)
+    })
+  })
+})
