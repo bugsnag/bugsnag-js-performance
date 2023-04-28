c5b9f43f5c6c44b71e964df5af6b18cd366252d9
Joe Haines <hello@joehaines.co.uk>
2023-04-28 10:57:17 +0100

    Make Settler an abstract class
    Every Settler has ended up with exact copy/pastes of the same code,
    which clearly implies it should be shared by all Settlers
    
    The Settler interface is now an abstract class and it handles all of the
    callback logic internally — concrete classes just need to call 'settle'
    at the appropriate time

--- packages/platforms/browser/lib/on-settle/settler.ts
+++ packages/platforms/browser/lib/on-settle/settler.ts
@@ -1,3 +1,22 @@
-export interface Settler {
-  subscribe: (callback: () => void) => void
+export abstract class Settler {
+  protected settled: boolean = false
+
+  private readonly callbacks: Array<() => void> = []
+
+  subscribe (callback: () => void): void {
+    this.callbacks.push(callback)
+
+    // if we're already settled, call the callback immediately
+    if (this.settled) {
+      callback()
+    }
+  }
+
+  protected settle (): void {
+    this.settled = true
+
+    for (const callback of this.callbacks) {
+      callback()
+    }
+  }
 }
