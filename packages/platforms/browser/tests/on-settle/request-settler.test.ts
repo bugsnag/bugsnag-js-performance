2f0b28ea2305dc7c950b2a52d225ccd5bbe949dc
Joe Haines <hello@joehaines.co.uk>
2023-04-28 10:35:34 +0100

    Add RequestSettler
    This Settler settles when there have been no outstanding requests for
    100 milliseconds

---
+++ packages/platforms/browser/tests/on-settle/request-settler.test.ts
@@ -0,0 +1,138 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import RequestSettler from '../../lib/on-settle/request-settler'
+import {
+  type RequestStartContext,
+  type RequestEndContext,
+  RequestTracker
+} from '../../lib/request-tracker/request-tracker'
+
+const START_CONTEXT: RequestStartContext = {
+  url: 'https://www.bugsnag.com',
+  method: 'GET',
+  startTime: 1234
+}
+
+const END_CONTEXT: RequestEndContext = {
+  endTime: 5678,
+  status: 200
+}
+
+describe('RequestSettler', () => {
+  beforeEach(() => {
+    jest.useFakeTimers()
+  })
+
+  it('is settled by default when there are no outstanding requests', () => {
+    const settleCallback = jest.fn()
+    const tracker = new RequestTracker()
+    const settler = new RequestSettler(tracker)
+
+    settler.subscribe(settleCallback)
+    expect(settleCallback).toHaveBeenCalled()
+  })
+
+  it.failing('is not settled by default when there are outstanding requests', () => {
+    const settleCallback = jest.fn()
+    const tracker = new RequestTracker()
+
+    tracker.start(START_CONTEXT)
+
+    const settler = new RequestSettler(tracker)
+
+    settler.subscribe(settleCallback)
+    expect(settleCallback).not.toHaveBeenCalled()
+  })
+
+  it('settles 100ms after a request finishes', async () => {
+    const settleCallback = jest.fn()
+    const tracker = new RequestTracker()
+    const settler = new RequestSettler(tracker)
+
+    const end = tracker.start(START_CONTEXT)
+
+    settler.subscribe(settleCallback)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    end(END_CONTEXT)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    // after 99ms (1ms short of the timeout) we should not settle
+    await jest.advanceTimersByTimeAsync(99)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    // it should settle after another 1ms
+    await jest.advanceTimersByTimeAsync(1)
+    expect(settleCallback).toHaveBeenCalled()
+  })
+
+  it.failing('settles 100ms after a request created before construction finishes', async () => {
+    const settleCallback = jest.fn()
+    const tracker = new RequestTracker()
+
+    const end = tracker.start(START_CONTEXT)
+
+    const settler = new RequestSettler(tracker)
+
+    settler.subscribe(settleCallback)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    end(END_CONTEXT)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    await jest.advanceTimersByTimeAsync(100)
+    expect(settleCallback).toHaveBeenCalled()
+  })
+
+  it('does not settle after a request finishes if there is another outstanding request', async () => {
+    const settleCallback = jest.fn()
+    const tracker = new RequestTracker()
+    const settler = new RequestSettler(tracker)
+
+    const endRequest1 = tracker.start(START_CONTEXT)
+
+    settler.subscribe(settleCallback)
+
+    const endRequest2 = tracker.start(START_CONTEXT)
+
+    endRequest1(END_CONTEXT)
+
+    await jest.advanceTimersByTimeAsync(100)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    endRequest2(END_CONTEXT)
+    expect(settleCallback).not.toHaveBeenCalled()
+
+    await jest.advanceTimersByTimeAsync(100)
+    expect(settleCallback).toHaveBeenCalled()
+  })
+
+  it('can handle multiple callbacks', async () => {
+    const settleCallbacks = [jest.fn(), jest.fn(), jest.fn(), jest.fn()]
+    const tracker = new RequestTracker()
+    const settler = new RequestSettler(tracker)
+
+    const end = tracker.start(START_CONTEXT)
+
+    for (const settleCallback of settleCallbacks) {
+      settler.subscribe(settleCallback)
+      expect(settleCallback).not.toHaveBeenCalled()
+    }
+
+    end(END_CONTEXT)
+
+    for (const settleCallback of settleCallbacks) {
+      settler.subscribe(settleCallback)
+      expect(settleCallback).not.toHaveBeenCalled()
+    }
+
+    await jest.advanceTimersByTimeAsync(100)
+
+    for (const settleCallback of settleCallbacks) {
+      settler.subscribe(settleCallback)
+      expect(settleCallback).toHaveBeenCalled()
+    }
+  })
+})
