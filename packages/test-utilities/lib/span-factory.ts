0982339aecc6e2cd4afba07de535909e6c3fece0
Ben Wilson <ben.wilson@smartbear.com>
2023-05-04 12:54:30 +0100

    remove unused test tool
    remove unused exports

--- packages/test-utilities/lib/span-factory.ts
+++
@@ -1,8 +0,0 @@
-import { Sampler, SpanFactory } from '@bugsnag/js-performance-core'
-import InMemoryProcessor from './in-memory-processor'
-import spanAttributesSource from './span-attributes-source'
-import StableIdGenerator from './stable-id-generator'
-
-const sampler = new Sampler(0.5)
-
-export const spanFactory = new SpanFactory(new InMemoryProcessor(), sampler, new StableIdGenerator(), spanAttributesSource)
