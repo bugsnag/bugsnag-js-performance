6b051fe3ac37e7a75f24d602ed76241ae8c874f4
Benedict Wilson <ben.wilson@smartbear.com>
2023-05-04 12:52:20 +0100

    Apply suggestions from code review
    Co-authored-by: Joe Haines <hello@joehaines.co.uk>

--- packages/platforms/browser/tests/span-attributes-source.test.ts
+++ packages/platforms/browser/tests/span-attributes-source.test.ts
@@ -6,11 +6,14 @@ import createSpanAttributesSource from '../lib/span-attributes-source'
 
 describe('spanAttributesSource', () => {
   it('includes common span attributes', () => {
-    const spanAttributesSource = createSpanAttributesSource(document.title, window.location.href)
+    const spanAttributesSource = createSpanAttributesSource(
+      'the page title',
+      'https://www.bugsnag.com'
+    )
     const spanAttributes = spanAttributesSource()
     expect(spanAttributes.get('bugsnag.span.category')).toBe('custom')
     expect(spanAttributes.get('bugsnag.span.first_class')).toBe(true)
-    expect(spanAttributes.get('bugsnag.browser.page.title')).toBe('')
-    expect(spanAttributes.get('bugsnag.browser.page.url')).toMatch(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}(\.[a-z]{2,4})?\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)
+    expect(spanAttributes.get('bugsnag.browser.page.title')).toBe('the page title')
+    expect(spanAttributes.get('bugsnag.browser.page.url')).toBe('https://www.bugsnag.com')
   })
 })
