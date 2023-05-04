37e63b11527864b231f2b9add58a1afcf8b920d9
Ben Wilson <ben.wilson@smartbear.com>
2023-05-04 11:26:51 +0100

    add common span attributes

--- packages/platforms/browser/lib/span-attributes-source.ts
+++ packages/platforms/browser/lib/span-attributes-source.ts
@@ -3,8 +3,11 @@ import type { SpanAttribute, SpanAttributesSource } from '@bugsnag/js-performanc
 const createSpanAttributesSource = (title: string, url: string): SpanAttributesSource => {
   return () => {
     const spanAttributes = new Map<string, SpanAttribute>()
+    spanAttributes.set('bugsnag.span.category', 'custom')
+    spanAttributes.set('bugsnag.span.first_class', true)
     spanAttributes.set('bugsnag.browser.page.url', url)
     spanAttributes.set('bugsnag.browser.page.title', title)
+
     return spanAttributes
   }
 }
--- packages/platforms/browser/tests/span-attributes-source.test.ts
+++ packages/platforms/browser/tests/span-attributes-source.test.ts
@@ -8,9 +8,9 @@ describe('spanAttributesSource', () => {
   it('includes common span attributes', () => {
     const spanAttributesSource = createSpanAttributesSource(document.title, window.location.href)
     const spanAttributes = spanAttributesSource()
-    const url = spanAttributes.get('bugsnag.browser.page.url')
-    expect(url).toMatch(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}(\.[a-z]{2,4})?\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)
-    const title = spanAttributes.get('bugsnag.browser.page.title')
-    expect(title).toBe('')
+    expect(spanAttributes.get('bugsnag.span.category')).toBe('custom')
+    expect(spanAttributes.get('bugsnag.span.first_class')).toBe(true)
+    expect(spanAttributes.get('bugsnag.browser.page.title')).toBe('')
+    expect(spanAttributes.get('bugsnag.browser.page.url')).toMatch(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}(\.[a-z]{2,4})?\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g)
   })
 })
