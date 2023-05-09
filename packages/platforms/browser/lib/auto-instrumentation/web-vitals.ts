18d0fc0b92df3694eddb630c47b57d5f1797ce98
Ben Wilson <ben.wilson@smartbear.com>
2023-05-09 15:23:41 +0100

    refactor observer

--- packages/platforms/browser/lib/auto-instrumentation/web-vitals.ts
+++ packages/platforms/browser/lib/auto-instrumentation/web-vitals.ts
@@ -1,17 +1,21 @@
+function isPerformanceNavigationTiming (entry: PerformanceEntry): entry is PerformanceNavigationTiming {
+  return entry.entryType === 'navigation'
+}
+
 export class WebVitalsManager {
   ttfb = 0
 
   constructor (PerformanceObserverClass: typeof PerformanceObserver) {
     const observer = new PerformanceObserverClass((entryList) => {
-      const [pageNav] = entryList.getEntriesByType('navigation')
-
-      // @ts-expect-error TypeScript unaware of responseStart on pageNav
-      this.ttfb = pageNav.responseStart
+      for (const entry of entryList.getEntries()) {
+        if (isPerformanceNavigationTiming(entry) && entry.responseStart > 0) {
+          this.ttfb = entry.responseStart
+          observer.disconnect()
+          break
+        }
+      }
     })
 
-    observer?.observe?.({
-      type: 'navigation',
-      buffered: true
-    })
+    observer.observe({ type: 'navigation', buffered: true })
   }
 }
