b9b2695306818fe8ba71723f665a7b5150ea48ff
Ben Wilson <ben.wilson@smartbear.com>
2023-05-09 13:52:50 +0100

    guard observe

--- packages/platforms/browser/lib/auto-instrumentation/web-vitals.ts
+++ packages/platforms/browser/lib/auto-instrumentation/web-vitals.ts
@@ -2,12 +2,14 @@ export class WebVitalsManager {
   ttfb = 0
 
   constructor (PerformanceObserverClass: typeof PerformanceObserver) {
-    new PerformanceObserverClass((entryList) => {
+    const observer = new PerformanceObserverClass((entryList) => {
       const [pageNav] = entryList.getEntriesByType('navigation')
 
       // @ts-expect-error TypeScript unaware of responseStart on pageNav
       this.ttfb = pageNav.responseStart
-    }).observe({
+    })
+
+    observer?.observe?.({
       type: 'navigation',
       buffered: true
     })
