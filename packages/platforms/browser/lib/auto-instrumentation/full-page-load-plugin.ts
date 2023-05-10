55583ec91918a575936816d069bb9cdb8a322667
Ben Wilson <ben.wilson@smartbear.com>
2023-05-10 11:26:49 +0100

    fix merge issues

--- packages/platforms/browser/lib/auto-instrumentation/full-page-load-plugin.ts
+++ packages/platforms/browser/lib/auto-instrumentation/full-page-load-plugin.ts
@@ -1,22 +1,26 @@
 import { type InternalConfiguration, type Plugin, type SpanFactory } from '@bugsnag/js-performance-core'
 import { type BrowserConfiguration } from '../config'
 import { type OnSettle } from '../on-settle'
+import { type WebVitalsTracker } from './web-vitals'
 
 export class FullPageLoadPlugin implements Plugin<BrowserConfiguration> {
   private spanFactory: SpanFactory
   private document: Document
   private location: Location
   private onSettle: OnSettle
+  private webVitalsTracker: WebVitalsTracker
 
   constructor (
     document: Document,
     location: Location,
     spanFactory: SpanFactory,
+    webVitalsTracker: WebVitalsTracker,
     onSettle: OnSettle
   ) {
     this.document = document
     this.location = location
     this.spanFactory = spanFactory
+    this.webVitalsTracker = webVitalsTracker
     this.onSettle = onSettle
   }
 
@@ -34,7 +38,9 @@ export class FullPageLoadPlugin implements Plugin<BrowserConfiguration> {
     span.setAttribute('bugsnag.browser.page.route', route)
 
     this.onSettle((endTime: number) => {
-      // TODO: Attach web vitals
+      // Attach web vitals
+      span.addEvent('ttfb', this.webVitalsTracker.ttfb)
+
       this.spanFactory.endSpan(span, endTime)
     })
   }
--- packages/platforms/browser/lib/browser.ts
+++ packages/platforms/browser/lib/browser.ts
@@ -11,6 +11,7 @@ import createSpanAttributesSource from './span-attributes-source'
 import createFetchRequestTracker from './request-tracker/request-tracker-fetch'
 import createXmlHttpRequestTracker from './request-tracker/request-tracker-xhr'
 import { NetworkRequestPlugin } from './auto-instrumentation/network-request-plugin'
+import { WebVitalsTracker } from './auto-instrumentation/web-vitals'
 
 const clock = createClock(performance)
 const spanAttributesSource = createSpanAttributesSource(document.title, window.location.href)
@@ -18,6 +19,7 @@ const resourceAttributesSource = createResourceAttributesSource(navigator)
 const backgroundingListener = createBrowserBackgroundingListener(document)
 const fetchRequestTracker = createFetchRequestTracker(window, clock)
 const xhrRequestTracker = createXmlHttpRequestTracker(window, clock)
+const webVitalsTracker = new WebVitalsTracker(PerformanceObserver)
 const onSettle = createOnSettle(
   clock,
   document,
@@ -37,7 +39,7 @@ const BugsnagPerformance = createClient({
   schema: createSchema(window.location.hostname),
   plugins: (spanFactory) => [
     onSettle,
-    new FullPageLoadPlugin(document, window.location, spanFactory, onSettle),
+    new FullPageLoadPlugin(document, window.location, spanFactory, webVitalsTracker, onSettle),
     new NetworkRequestPlugin(spanFactory, fetchRequestTracker, xhrRequestTracker)
   ]
 })
