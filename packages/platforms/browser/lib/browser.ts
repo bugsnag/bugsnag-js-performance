101a5480edc6f87178c8075244e83affb253275d
Ben Wilson <ben.wilson@smartbear.com>
2023-05-10 11:26:49 +0100

    fix merge issues

--- packages/platforms/browser/lib/browser.ts
+++ packages/platforms/browser/lib/browser.ts
@@ -10,6 +10,7 @@ import createResourceAttributesSource from './resource-attributes-source'
 import createSpanAttributesSource from './span-attributes-source'
 import createFetchRequestTracker from './request-tracker/request-tracker-fetch'
 import createXmlHttpRequestTracker from './request-tracker/request-tracker-xhr'
+import { WebVitalsTracker } from './auto-instrumentation/web-vitals'
 
 const clock = createClock(performance)
 const spanAttributesSource = createSpanAttributesSource(document.title, window.location.href)
@@ -17,6 +18,7 @@ const resourceAttributesSource = createResourceAttributesSource(navigator)
 const backgroundingListener = createBrowserBackgroundingListener(document)
 const fetchRequestTracker = createFetchRequestTracker(window, clock)
 const xhrRequestTracker = createXmlHttpRequestTracker(window, clock)
+const webVitalsTracker = new WebVitalsTracker(PerformanceObserver)
 const onSettle = createOnSettle(
   clock,
   document,
@@ -36,7 +38,7 @@ const BugsnagPerformance = createClient({
   schema: createSchema(window.location.hostname),
   plugins: (spanFactory) => [
     onSettle,
-    new FullPageLoadPlugin(document, window.location, spanFactory, onSettle)
+    new FullPageLoadPlugin(document, window.location, spanFactory, webVitalsTracker, onSettle)
   ]
 })
 
