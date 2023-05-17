9c847377abfd9346f71211c067aa9e955cef94a0
Joe Haines <hello@joehaines.co.uk>
2023-05-17 15:46:25 +0100

    Add 'PerformanceEntryListFake' abstract class
    We need the getEntries & friends to exist on the fake performance object
    as well as for PerformanceObserver

---
+++ packages/test-utilities/lib/performance-observer/performance-entry-list.ts
@@ -0,0 +1,24 @@
+import { type PerformanceEntryFake, type PerformanceEntryType } from './performance-entry'
+
+export default abstract class PerformanceEntryListFake {
+  protected readonly entries: PerformanceEntryFake[]
+
+  constructor (entries: PerformanceEntryFake[]) {
+    this.entries = entries
+  }
+
+  getEntries (): PerformanceEntryFake[] {
+    return this.entries
+  }
+
+  getEntriesByType (entryType: PerformanceEntryType): PerformanceEntryFake[] {
+    return this.entries.filter(entry => entry.entryType === entryType)
+  }
+
+  getEntriesByName (name: string, entryType?: PerformanceEntryType): PerformanceEntryFake[] {
+    return this.entries
+      .filter(entry => entry.name === name && (
+        entryType === undefined || entry.entryType === entryType
+      ))
+  }
+}
--- packages/test-utilities/lib/performance-observer/performance-observer-entry-list.ts
+++ packages/test-utilities/lib/performance-observer/performance-observer-entry-list.ts
@@ -1,25 +1,4 @@
-import { type PerformanceEntryFake, type PerformanceEntryType } from './performance-entry'
+import PerformanceEntryListFake from './performance-entry-list'
 
 // https://w3c.github.io/performance-timeline/#performanceobserverentrylist-interface
-export default class PerformanceObserverEntryListFake {
-  private entries: PerformanceEntryFake[]
-
-  constructor (entries: PerformanceEntryFake[]) {
-    this.entries = entries
-  }
-
-  getEntries (): PerformanceEntryFake[] {
-    // duplicate the array so our internal copy can't be mutated
-    return Array.from(this.entries)
-  }
-
-  getEntriesByName (name: string, type?: PerformanceEntryType): PerformanceEntryFake[] {
-    return this.entries
-      .filter(entry => entry.name === name)
-      .filter(entry => type === undefined || entry.entryType === type)
-  }
-
-  getEntriesByType (type: PerformanceEntryType): PerformanceEntryFake[] {
-    return this.entries.filter(entry => entry.entryType === type)
-  }
-}
+export default class PerformanceObserverEntryListFake extends PerformanceEntryListFake {}
