f9c4da0e149ef9e8596ef5d59639bb68f75cc604
Joe Haines <hello@joehaines.co.uk>
2023-05-17 14:49:35 +0100

    Add PerformanceEventTimingFake

--- packages/test-utilities/lib/performance-observer/index.ts
+++ packages/test-utilities/lib/performance-observer/index.ts
@@ -1,6 +1,7 @@
 export {
   createPerformanceNavigationTimingFake,
   createPerformancePaintTimingFake,
-  createLargestContentfulPaintFake
+  createLargestContentfulPaintFake,
+  createPerformanceEventTimingFake
 } from './performance-entry'
 export { PerformanceObserverManager } from './performance-observer-manager'
