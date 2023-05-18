ccf93d958204c5d2b0ad850c02dc21e5753e1984
Ben Wilson <ben.wilson@smartbear.com>
2023-05-18 10:48:01 +0100

    remove workaround for supports_performance_paint_timing and update assertions

--- test/browser/features/lib/browser.rb
+++ test/browser/features/lib/browser.rb
@@ -72,11 +72,11 @@ class Browser
   def chrome_supported_vitals version
     case version
     when (77..)
-      ["ttfb"] # also ["fcp", "fid", "lcp", "cls"]
+      ["ttfb", "fcp"] # also ["fid", "lcp", "cls"]
     when (76..)
-      ["ttfb"] # also ["fid", "fcp"]
+      ["ttfb", "fcp"] # also ["fid"]
     when (64..)
-      ["ttfb"] # also ["fcp"]
+      ["ttfb", "fcp"]
     else 
       ["ttfb"]
     end
@@ -85,7 +85,7 @@ class Browser
   def edge_supported_vitals version
     case version
     when (79..)
-      ["ttfb"] # also ["fcp", "fid", "lcp", "cls"]
+      ["ttfb", "fcp"] # also ["fid", "lcp", "cls"]
     else 
       ["ttfb"]
     end
@@ -94,7 +94,7 @@ class Browser
   def firefox_supported_vitals version
     case version
     when (84..)
-      ["ttfb"] # also ["fcp", "fid", "lcp", "cls"]
+      ["ttfb", "fcp"] # also ["fid", "lcp", "cls"]
     else
       ["ttfb"]
     end
@@ -103,7 +103,7 @@ class Browser
   def safari_supported_vitals version
     case version
     when (14..) # technically 14.1, but our test fixtures never use 14.0
-      ["ttfb"] # also ["fcp", "fid", "lcp", "cls"]
+      ["ttfb", "fcp"] # also ["fid", "lcp", "cls"]
     else
       ["ttfb"]
     end
