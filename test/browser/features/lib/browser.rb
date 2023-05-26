1e429dc39e32e9d302ee386e7927aa87683623e9
Alex Moinet <alex@bugsnag.com>
2023-05-26 15:01:06 +0100

    Comment out tests which need more work later [PLAT-10176]

--- test/browser/features/lib/browser.rb
+++ test/browser/features/lib/browser.rb
@@ -1,6 +1,10 @@
 require 'yaml'
 
 class Browser
+
+  # Whether the Browser is running on a mobile device
+  attr_reader :is_device?
+
   def initialize(browser_spec, maze_uri, fixtures_uri)
     @maze_uri = maze_uri
     @fixtures_uri = fixtures_uri
@@ -8,12 +12,12 @@ class Browser
     # e.g. "chrome_61", "edge_latest", "chrome"
     @name, version = browser_spec.split("_")
 
+    @is_device? = @name.eql?('android') || @name.eql?('iphone')
+
     # assume we're running the latest version if there is no version present
     # this should only happen locally where the browser will auto-update
     @version =
-      if version.nil? || version == "latest"
-        Float::INFINITY
-      elsif @name.eql?('android') || @name.eql?('iphone')
+      if @is_device? || version.nil? || version == "latest"
         Float::INFINITY
       else
         Integer(version)
