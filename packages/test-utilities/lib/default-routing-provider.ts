0e5d4f525265117fb168f7b758b49995c6033243
Ben Wilson <ben.wilson@smartbear.com>
2023-05-31 14:32:51 +0100

    add browser-performance as a dependency

--- packages/test-utilities/lib/default-routing-provider.ts
+++ packages/test-utilities/lib/default-routing-provider.ts
@@ -1,6 +1,4 @@
 import { type RoutingProvider } from '@bugsnag/browser-performance'
-import { type StartRouteChangeSpan } from '@bugsnag/browser-performance/dist/types/auto-instrumentation'
-import { type OnSettle } from '@bugsnag/browser-performance/dist/types/on-settle'
 
 class DefaultRoutingProvider implements RoutingProvider {
   getInitialRoute () {
@@ -11,7 +9,7 @@ class DefaultRoutingProvider implements RoutingProvider {
     return url.pathname
   }
 
-  configure (startRouteChangeSpan: StartRouteChangeSpan, onSettle: OnSettle) {
+  configure () {
 
   }
 }
