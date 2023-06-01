36617eab5de95b6f713a4ec0d3b2fc20c5b7eabd
Ben Wilson <ben.wilson@smartbear.com>
2023-06-01 13:07:46 +0100

    remove unnecessary noop client

--- packages/platforms/js/types.d.ts
+++ packages/platforms/js/types.d.ts
@@ -1,7 +1,2 @@
-import { type BrowserConfiguration } from '@bugsnag/browser-performance'
-import { type BugsnagPerformance, type Configuration } from '@bugsnag/core-performance'
-
-declare const Bugsnag: BugsnagPerformance<BrowserConfiguration | Configuration>
-
-export default Bugsnag
 export * from '@bugsnag/browser-performance'
+export { default } from '@bugsnag/browser-performance'
