f00f8123ef1208ac64eb506b848ecac2ed00f3cf
Joe Haines <hello@joehaines.co.uk>
2023-06-01 12:32:19 +0100

    Add concept of persistence to core
    This is unused until a future PR
    
    Core implements an "InMemoryPersistence" that persists items in memory
    i.e. in a Map
    
    This can be used by platforms as a fallback if persistence isn't
    available for whatever reason (e.g. on a browser that doesn't support
    localStorage)

---
+++ packages/core/lib/persistence.ts
@@ -0,0 +1,28 @@
+export interface PersistedProbability {
+  value: number
+  time: number
+}
+
+export interface PersistencePayloadMap {
+  'bugsnag-sampling-probability': PersistedProbability
+}
+
+export type PersistenceKey = keyof PersistencePayloadMap
+export type PersistencePayload = PersistencePayloadMap[PersistenceKey]
+
+export interface Persistence {
+  load: <K extends PersistenceKey>(key: K) => Promise<PersistencePayloadMap[K] | undefined>
+  save: <K extends PersistenceKey>(key: K, value: PersistencePayloadMap[K]) => Promise<void>
+}
+
+export class InMemoryPersistence implements Persistence {
+  private readonly persistedItems = new Map<PersistenceKey, PersistencePayload>()
+
+  async load<K extends PersistenceKey> (key: K): Promise<PersistencePayloadMap[K] | undefined> {
+    return this.persistedItems.get(key)
+  }
+
+  async save<K extends PersistenceKey> (key: K, value: PersistencePayloadMap[K]): Promise<void> {
+    this.persistedItems.set(key, value)
+  }
+}
