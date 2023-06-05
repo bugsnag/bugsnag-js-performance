203067616327b47cfe668b7fc8ccaf4cae6b40a3
Steve Kirkland-Walton <steve@bugsnag.com>
2023-06-05 18:20:21 +0100

    Use public addresses

--- test/browser/features/support/maze-config.rb
+++ test/browser/features/support/maze-config.rb
@@ -6,12 +6,19 @@ require_relative '../lib/browser'
 
 Maze.config.enforce_bugsnag_integrity = false
 
-Maze.config.bind_address = ENV.fetch('API_HOST', 'localhost')
-Maze.config.document_server_bind_address = ENV.fetch('HOST', 'localhost')
+if Maze.config.aws_public_ip
+  maze_address = Maze.public_address
+  document_address = Maze.public_document_server_address
+else
+  host = ENV.fetch('HOST', 'localhost')
+  maze_address = "#{host}:#{Maze.config.port}"
+  document_address = "#{host}:#{Maze.config.document_server_port}"
+end
+
 Maze.config.document_server_root = File.realpath("#{__dir__}/../fixtures/packages")
 
 $browser = Browser.new(
   Maze.config.browser,
-  URI("http://#{Maze.public_address}"),
-  URI("http://#{Maze.document_server_address}")
+  URI("http://#{maze_address}"),
+  URI("http://#{document_address}")
 )
