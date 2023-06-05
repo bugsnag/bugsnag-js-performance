9a02c027684e2ee36d0e6b05e95ac73198ffe393
Steve Kirkland-Walton <steve@bugsnag.com>
2023-06-05 19:34:15 +0100

    Wrap in BeforeAll

--- test/browser/features/support/maze-config.rb
+++ test/browser/features/support/maze-config.rb
@@ -1,24 +1,21 @@
 require_relative '../lib/browser'
 
-# this can't be in a 'before all' hook because we need to set
-# 'Maze.config.document_server_root' before Maze Runner's before all hook runs,
-# otherwise the document server won't be started automatically
+Maze.hooks.before_all do
+  Maze.config.document_server_root = File.realpath("#{__dir__}/../fixtures/packages")
+  Maze.config.enforce_bugsnag_integrity = false
 
-Maze.config.enforce_bugsnag_integrity = false
+  if Maze.config.aws_public_ip
+    maze_address = Maze.public_address
+    document_address = Maze.public_document_server_address
+  else
+    host = ENV.fetch('HOST', 'localhost')
+    maze_address = "#{host}:#{Maze.config.port}"
+    document_address = "#{host}:#{Maze.config.document_server_port}"
+  end
 
-if Maze.config.aws_public_ip
-  maze_address = Maze.public_address
-  document_address = Maze.public_document_server_address
-else
-  host = ENV.fetch('HOST', 'localhost')
-  maze_address = "#{host}:#{Maze.config.port}"
-  document_address = "#{host}:#{Maze.config.document_server_port}"
+  $browser = Browser.new(
+    Maze.config.browser,
+    URI("http://#{maze_address}"),
+    URI("http://#{document_address}")
+  )
 end
-
-Maze.config.document_server_root = File.realpath("#{__dir__}/../fixtures/packages")
-
-$browser = Browser.new(
-  Maze.config.browser,
-  URI("http://#{maze_address}"),
-  URI("http://#{document_address}")
-)
