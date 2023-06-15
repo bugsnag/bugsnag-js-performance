bf1c7d4c9d5e15c8254828bf7404fed2c4a7f9ce
Yousif Ahmed <yousif@bugsnag.com>
2023-06-15 09:44:37 +0100

    add  `SpanContextStorage` interface and default implementation

--- packages/core/lib/span-context.ts
+++ packages/core/lib/span-context.ts
@@ -16,3 +16,74 @@ export function spanContextEquals (span1?: SpanContext, span2?: SpanContext) {
   return false
 }
 
+export interface SpanContextStorage extends Iterable<SpanContext> {
+  push: (context: SpanContext) => void
+  pop: (context: SpanContext) => void
+  readonly current: SpanContext | undefined
+}
+
+class SpanContextIterator implements Iterator<SpanContext> {
+  private readonly contexts: SpanContext[]
+  private position: number
+
+  constructor (contexts: SpanContext[]) {
+    this.contexts = contexts
+    this.position = this.contexts.length
+  }
+
+  next (): IteratorResult<SpanContext> {
+    if (this.position > 0) {
+      return {
+        done: false,
+        value: this.contexts[this.position--]
+      }
+    } else {
+      return {
+        done: true,
+        value: undefined
+      }
+    }
+  }
+}
+
+export class DefaultSpanContextStorage implements SpanContextStorage {
+  private readonly contextStack: SpanContext[]
+
+  constructor (contextStack: SpanContext[] = []) {
+    this.contextStack = contextStack
+  }
+
+  [Symbol.iterator] (): SpanContextIterator {
+    return new SpanContextIterator(this.contextStack)
+  }
+
+  push (context: SpanContext) {
+    if (context.isValid()) {
+      this.contextStack.push(context)
+    }
+  }
+
+  pop (context: SpanContext) {
+    if (spanContextEquals(context, this.current)) {
+      this.contextStack.pop()
+    }
+
+    this.removeClosedContexts()
+  }
+
+  get current () {
+    this.removeClosedContexts()
+    return this.contextStack.length > 0
+      ? this.contextStack[this.contextStack.length - 1]
+      : undefined
+  }
+
+  private removeClosedContexts () {
+    while (
+      this.contextStack.length > 0 &&
+      this.contextStack[this.contextStack.length - 1].isValid() === false
+    ) {
+      this.contextStack.pop()
+    }
+  }
+}
