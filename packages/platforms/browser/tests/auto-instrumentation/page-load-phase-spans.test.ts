66dd4f266323ac24601f4d2c562d7685afef8be4
Ben Wilson <ben.wilson@smartbear.com>
2023-06-16 17:27:50 +0100

    test: :white_check_mark: test page load phase sub spans are created and use expected times

---
+++ packages/platforms/browser/tests/auto-instrumentation/page-load-phase-spans.test.ts
@@ -0,0 +1,95 @@
+import { MockSpanFactory } from '@bugsnag/js-performance-test-utilities'
+import { pageLoadPhaseSpans } from '../../lib/auto-instrumentation/page-load-phase-spans'
+import { PerformanceFake, createPerformanceNavigationTimingFake } from '../utilities'
+
+describe('PageLoadPhase Spans', () => {
+  it('automatically creates expected spans', () => {
+    const spanFactory = new MockSpanFactory()
+    const performance = new PerformanceFake()
+    performance.addEntries(createPerformanceNavigationTimingFake({
+      startTime: 1,
+      unloadEventStart: 2,
+      unloadEventEnd: 3,
+      redirectStart: 4,
+      redirectEnd: 5,
+      workerStart: 6,
+      fetchStart: 7,
+      domainLookupStart: 8,
+      domainLookupEnd: 9,
+      connectStart: 10,
+      secureConnectionStart: 11,
+      connectEnd: 12,
+      requestStart: 13,
+      responseStart: 14,
+      responseEnd: 15,
+      domInteractive: 16,
+      domContentLoadedEventStart: 17,
+      domContentLoadedEventEnd: 18,
+      domComplete: 19,
+      loadEventStart: 20,
+      loadEventEnd: 21
+    }))
+
+    pageLoadPhaseSpans(spanFactory, '/page-load-phase-spans', performance as unknown as Performance)
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/Unload]/page-load-phase-spans',
+      startTime: 2,
+      endTime: 3
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/Redirect]/page-load-phase-spans',
+      startTime: 4,
+      endTime: 5
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/LoadFromCache]/page-load-phase-spans',
+      startTime: 7,
+      endTime: 8
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/DNSLookup]/page-load-phase-spans',
+      startTime: 8,
+      endTime: 9
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/TCPHandshake]/page-load-phase-spans',
+      startTime: 10,
+      endTime: 11
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/TLS]/page-load-phase-spans',
+      startTime: 11,
+      endTime: 12
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/HTTPRequest]/page-load-phase-spans',
+      startTime: 13,
+      endTime: 14
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/HTTPResponse]/page-load-phase-spans',
+      startTime: 14,
+      endTime: 15
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/DomContentLoadedEvent]/page-load-phase-spans',
+      startTime: 17,
+      endTime: 18
+    }))
+
+    expect(spanFactory.createdSpans).toContainEqual(expect.objectContaining({
+      name: '[PageLoadPhase/LoadEvent]/page-load-phase-spans',
+      startTime: 20,
+      endTime: 21
+    }))
+  })
+})
