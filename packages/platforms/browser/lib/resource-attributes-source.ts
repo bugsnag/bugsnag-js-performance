9ad1ba6ea727e59efe395761d41c1b27ce9b8897
Joe Haines <hello@joehaines.co.uk>
2023-06-19 10:51:42 +0100

    Ensure only one anonymous ID is generated/loaded

--- packages/platforms/browser/lib/resource-attributes-source.ts
+++ packages/platforms/browser/lib/resource-attributes-source.ts
@@ -6,6 +6,9 @@ function createResourceAttributesSource (
   navigator: Navigator,
   persistence: Persistence
 ): ResourceAttributeSource<BrowserConfiguration> {
+  let getDeviceId: Promise<string> | undefined
+  let deviceId: string | undefined
+
   return function resourceAttributesSource (config) {
     const attributes = new ResourceAttributes(
       config.releaseStage,
@@ -23,17 +26,36 @@ function createResourceAttributesSource (
     }
 
     if (config.generateAnonymousId) {
-      persistence.load('bugsnag-anonymous-id').then(maybeDeviceId => {
-        // use the persisted value or generate a new ID
-        const deviceId = maybeDeviceId || cuid()
+      // ensure we only load/generate the anonymous ID once no matter how many
+      // times we're called, otherwise we could generate different IDs on
+      // different calls as cuids are partly time based
+      if (!getDeviceId) {
+        getDeviceId = persistence.load('bugsnag-anonymous-id')
+          .then(maybeAnonymousId => {
+            // use the persisted value or generate a new ID
+            const anonymousId = maybeAnonymousId || cuid()
 
-        // if there was no persisted value, save the newly generated ID
-        if (!maybeDeviceId) {
-          persistence.save('bugsnag-anonymous-id', deviceId)
-        }
+            // if there was no persisted value, save the newly generated ID
+            if (!maybeAnonymousId) {
+              persistence.save('bugsnag-anonymous-id', anonymousId)
+            }
 
+            // store the device ID so we can set it synchronously in future
+            deviceId = anonymousId
+
+            return deviceId
+          })
+      }
+
+      if (deviceId) {
+        // set device ID synchronously if it's already available
         attributes.set('device.id', deviceId)
-      })
+      } else {
+        // otherwise add it when the promise resolves
+        getDeviceId.then(deviceId => {
+          attributes.set('device.id', deviceId)
+        })
+      }
     }
 
     return attributes
--- packages/platforms/browser/tests/resource-attributes-source.test.ts
+++ packages/platforms/browser/tests/resource-attributes-source.test.ts
@@ -17,15 +17,17 @@ describe('resourceAttributesSource', () => {
     }
 
     await persistence.save('bugsnag-anonymous-id', 'c1234567890abcdefghijklmnop')
+    expect(await persistence.load('bugsnag-anonymous-id')).toBe('c1234567890abcdefghijklmnop')
 
     const resourceAttributesSource = createResourceAttributesSource(navigator, persistence)
     const resourceAttributes = resourceAttributesSource(
       createConfiguration<BrowserConfiguration>({ releaseStage: 'test', appVersion: '1.0.0' })
     )
 
-    // wait for the anyonmous ID to be available (we don't actually need to load
-    // it, just wait for the next iteration of the event loop)
-    await persistence.load('bugsnag-anonymous-id')
+    await new Promise<void>(resolve => { resolve() })
+
+    // ensure the anyonmous ID hasn't changed
+    expect(await persistence.load('bugsnag-anonymous-id')).toBe('c1234567890abcdefghijklmnop')
 
     expect(resourceAttributes.toJson()).toEqual([
       { key: 'deployment.environment', value: { stringValue: 'test' } },
@@ -103,6 +105,47 @@ describe('resourceAttributesSource', () => {
       { key: 'browser.user_agent', value: { stringValue: navigator.userAgent } },
       { key: 'device.id', value: { stringValue: deviceId } }
     ])
+  })
+
+  it('only generates a single device.id across multiple calls', async () => {
+    const persistence = new InMemoryPersistence()
+    const navigator = {
+      ...window.navigator,
+      userAgentData: undefined,
+      userAgent: 'a jest test, (like Gecko and WebKit and also Blink) etc...'
+    }
+
+    // ensure no device ID is persisted
+    expect(await persistence.load('bugsnag-anonymous-id')).toBeUndefined()
+
+    const resourceAttributesSource = createResourceAttributesSource(navigator, persistence)
+    const configuration = createConfiguration<BrowserConfiguration>()
+
+    const resourceAttributes1 = resourceAttributesSource(configuration)
+    const resourceAttributes2 = resourceAttributesSource(configuration)
+    const resourceAttributes3 = resourceAttributesSource(configuration)
+
+    await new Promise<void>(resolve => { resolve() })
+    const deviceId = await persistence.load('bugsnag-anonymous-id')
+
+    expect(resourceAttributes1.toJson()).toStrictEqual([
+      { key: 'deployment.environment', value: { stringValue: 'production' } },
+      { key: 'telemetry.sdk.name', value: { stringValue: 'bugsnag.performance.browser' } },
+      { key: 'telemetry.sdk.version', value: { stringValue: '__VERSION__' } },
+      { key: 'browser.user_agent', value: { stringValue: navigator.userAgent } },
+      { key: 'device.id', value: { stringValue: deviceId } }
+    ])
+
+    expect(resourceAttributes1.toJson()).toStrictEqual(resourceAttributes2.toJson())
+    expect(resourceAttributes2.toJson()).toStrictEqual(resourceAttributes3.toJson())
+
+    // device ID is now available synchronously so we don't need to wait for it
+    // to be available on this call
+    const resourceAttributes4 = resourceAttributesSource(configuration)
+    expect(resourceAttributes3.toJson()).toStrictEqual(resourceAttributes4.toJson())
+
+    // device ID shouldn't have changed
+    expect(await persistence.load('bugsnag-anonymous-id')).toBe(deviceId)
   })
 
   it('does not generate a device.id if generateAnonymousId is false', async () => {
