6364790db0726ddf22f55426542433053cc754ab
Joe Haines <hello@joehaines.co.uk>
2023-06-19 09:44:47 +0100

    Add persisted device ID as a resource attribute

--- packages/platforms/browser/lib/resource-attributes-source.ts
+++ packages/platforms/browser/lib/resource-attributes-source.ts
@@ -1,7 +1,12 @@
-import { ResourceAttributes, type ResourceAttributeSource } from '@bugsnag/core-performance'
+import { ResourceAttributes, type Persistence, type ResourceAttributeSource } from '@bugsnag/core-performance'
 import { type BrowserConfiguration } from './config'
 
-function createResourceAttributesSource (navigator: Navigator): ResourceAttributeSource<BrowserConfiguration> {
+function createResourceAttributesSource (
+  navigator: Navigator,
+  persistence: Persistence
+): ResourceAttributeSource<BrowserConfiguration> {
+  const getDeviceId = persistence.load('bugsnag-anonymous-id')
+
   return function resourceAttributesSource (config) {
     const attributes = new ResourceAttributes(
       config.releaseStage,
@@ -17,6 +22,12 @@ function createResourceAttributesSource (navigator: Navigator): ResourceAttribut
       attributes.set('browser.platform', navigator.userAgentData.platform)
       attributes.set('browser.mobile', navigator.userAgentData.mobile)
     }
+
+    getDeviceId.then(maybeDeviceId => {
+      if (maybeDeviceId) {
+        attributes.set('device.id', maybeDeviceId)
+      }
+    })
 
     return attributes
   }
