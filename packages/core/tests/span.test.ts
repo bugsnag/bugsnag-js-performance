1a509ee1953b87ae0ee95a24a8606196afd8bea8
Joe Haines <hello@joehaines.co.uk>
2023-06-20 11:13:14 +0100

    Add isFirstClass span option

--- packages/core/tests/span.test.ts
+++ packages/core/tests/span.test.ts
@@ -96,6 +96,103 @@ describe('SpanInternal', () => {
   })
 })
 
+describe('SpanFactory', () => {
+  describe('startSpan', () => {
+    it('omits first class span attribute by default', () => {
+      const clock = new IncrementingClock('1970-01-01T00:00:00.000Z')
+      const sampler = new Sampler(0.5)
+      const delivery = { send: jest.fn() }
+      const processor = { add: (span: SpanEnded) => delivery.send(spanToJson(span, clock)) }
+      const spanFactory = new SpanFactory(
+        processor,
+        sampler,
+        new StableIdGenerator(),
+        spanAttributesSource,
+        new IncrementingClock(),
+        new ControllableBackgroundingListener(),
+        jestLogger
+      )
+
+      const span = spanFactory.startSpan('name')
+
+      // @ts-expect-error 'attributes' is private but very awkward to test otherwise
+      expect(span.attributes.attributes.has('bugsnag.span.first_class')).toBe(false)
+    })
+
+    it('creates first class spans when isFirstClass is true', () => {
+      const clock = new IncrementingClock('1970-01-01T00:00:00.000Z')
+      const sampler = new Sampler(0.5)
+      const delivery = { send: jest.fn() }
+      const processor = { add: (span: SpanEnded) => delivery.send(spanToJson(span, clock)) }
+      const spanFactory = new SpanFactory(
+        processor,
+        sampler,
+        new StableIdGenerator(),
+        spanAttributesSource,
+        new IncrementingClock(),
+        new ControllableBackgroundingListener(),
+        jestLogger
+      )
+
+      const span = spanFactory.startSpan('name', { isFirstClass: true })
+
+      // @ts-expect-error 'attributes' is private but very awkward to test otherwise
+      expect(span.attributes.attributes.get('bugsnag.span.first_class')).toBe(true)
+    })
+
+    it('does not create first class spans when isFirstClass is false', () => {
+      const clock = new IncrementingClock('1970-01-01T00:00:00.000Z')
+      const sampler = new Sampler(0.5)
+      const delivery = { send: jest.fn() }
+      const processor = { add: (span: SpanEnded) => delivery.send(spanToJson(span, clock)) }
+      const spanFactory = new SpanFactory(
+        processor,
+        sampler,
+        new StableIdGenerator(),
+        spanAttributesSource,
+        new IncrementingClock(),
+        new ControllableBackgroundingListener(),
+        jestLogger
+      )
+
+      const span = spanFactory.startSpan('name', { isFirstClass: false })
+
+      // @ts-expect-error 'attributes' is private but very awkward to test otherwise
+      expect(span.attributes.attributes.get('bugsnag.span.first_class')).toBe(false)
+    })
+
+    it.each([
+      null,
+      undefined,
+      1,
+      0,
+      'true',
+      'false',
+      [true, false]
+    ])('omits first class attribute when isFirstClass is %s', (isFirstClass) => {
+      const clock = new IncrementingClock('1970-01-01T00:00:00.000Z')
+      const sampler = new Sampler(0.5)
+      const delivery = { send: jest.fn() }
+      const processor = { add: (span: SpanEnded) => delivery.send(spanToJson(span, clock)) }
+      const spanFactory = new SpanFactory(
+        processor,
+        sampler,
+        new StableIdGenerator(),
+        spanAttributesSource,
+        new IncrementingClock(),
+        new ControllableBackgroundingListener(),
+        jestLogger
+      )
+
+      // @ts-expect-error 'isFirstClass' is the wrong type
+      const span = spanFactory.startSpan('name', { isFirstClass })
+
+      // @ts-expect-error 'attributes' is private but very awkward to test otherwise
+      expect(span.attributes.attributes.has('bugsnag.span.first_class')).toBe(false)
+    })
+  })
+})
+
 describe('Span', () => {
   describe('client.startSpan()', () => {
     it('returns a Span', () => {
