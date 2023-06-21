740ea2c1d73436b42bb5c53f4862987e5c6d3271
Joe Haines <hello@joehaines.co.uk>
2023-06-21 09:35:33 +0100

    Make sure the $browser global is always available
    We need $browser to be available at all times so that we can use it for
    things like the 'skip' annotations. This was problematic as it was
    created in a 'before' hook, which run just before a scenario starts, so
    if a skip annotation required checking the $browser object then we'd
    crash
    
    The $browser global needed to be in a before hook because it used the
    Maze Runner URLs for generating URLs to fixtures and those were only
    available after configuration is setup. This is now handled by a
    separate $url_generator object, which means the $browser object can be
    made available at all times

---
+++ test/browser/features/lib/url-generator.rb
@@ -0,0 +1,19 @@
+class UrlGenerator
+  def initialize(maze_uri, fixtures_uri)
+    @maze_uri = maze_uri
+    @fixtures_uri = fixtures_uri
+  end
+
+  def for_path(path)
+    uri = URI.join(@fixtures_uri, path)
+    config_query_string = "endpoint=#{@maze_uri}/traces&logs=#{@maze_uri}/logs&api_key=#{$api_key}"
+
+    if uri.query
+      uri.query += "&#{config_query_string}"
+    else
+      uri.query = config_query_string
+    end
+
+    uri.to_s
+  end
+end
