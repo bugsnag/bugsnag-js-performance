97a545cfdfae86d456611dea85d1742c188ffeda
Yousif Ahmed <yousif@bugsnag.com>
2023-06-22 12:23:55 +0100

    make `PageLoadPhase` spans children of the full page load span

--- packages/platforms/browser/lib/auto-instrumentation/page-load-phase-spans.ts
+++ packages/platforms/browser/lib/auto-instrumentation/page-load-phase-spans.ts
@@ -1,4 +1,4 @@
-import type { SpanFactory } from '@bugsnag/core-performance'
+import type { SpanContext, SpanFactory } from '@bugsnag/core-performance'
 import { type PerformanceWithTiming } from '../on-settle/load-event-end-settler'
 
 function shouldOmitSpan (startTime?: number, endTime?: number): boolean {
@@ -6,31 +6,35 @@ function shouldOmitSpan (startTime?: number, endTime?: number): boolean {
   (startTime === 0 && endTime === 0)
 }
 
-export const instrumentPageLoadPhaseSpans = (spanFactory: SpanFactory, route: string, performance: PerformanceWithTiming) => {
+export const instrumentPageLoadPhaseSpans = (spanFactory: SpanFactory, performance: PerformanceWithTiming, route: string, pageLoadSpan: SpanContext) => {
   const entry = performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming
 
   if (entry) {
     if (!shouldOmitSpan(entry.unloadEventStart, entry.unloadEventEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/Unload]' + route, {
-        startTime: entry.unloadEventStart
+        startTime: entry.unloadEventStart,
+        parentContext: pageLoadSpan
       }), entry.unloadEventEnd)
     }
 
     if (!shouldOmitSpan(entry.redirectStart, entry.redirectEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/Redirect]' + route, {
-        startTime: entry.redirectStart
+        startTime: entry.redirectStart,
+        parentContext: pageLoadSpan
       }), entry.redirectEnd)
     }
 
     if (!shouldOmitSpan(entry.fetchStart, entry.domainLookupStart)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/LoadFromCache]' + route, {
-        startTime: entry.fetchStart
+        startTime: entry.fetchStart,
+        parentContext: pageLoadSpan
       }), entry.domainLookupStart)
     }
 
     if (!shouldOmitSpan(entry.domainLookupStart, entry.domainLookupEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/DNSLookup]' + route, {
-        startTime: entry.domainLookupStart
+        startTime: entry.domainLookupStart,
+        parentContext: pageLoadSpan
       }), entry.domainLookupEnd)
     }
 
@@ -39,37 +43,43 @@ export const instrumentPageLoadPhaseSpans = (spanFactory: SpanFactory, route: st
     const TCPHandshakeEnd = entry.secureConnectionStart || entry.connectEnd
     if (!shouldOmitSpan(entry.connectStart, TCPHandshakeEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/TCPHandshake]' + route, {
-        startTime: entry.connectStart
+        startTime: entry.connectStart,
+        parentContext: pageLoadSpan
       }), TCPHandshakeEnd)
     }
 
     if (!shouldOmitSpan(entry.secureConnectionStart, entry.connectEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/TLS]' + route, {
-        startTime: entry.secureConnectionStart
+        startTime: entry.secureConnectionStart,
+        parentContext: pageLoadSpan
       }), entry.connectEnd)
     }
 
     if (!shouldOmitSpan(entry.requestStart, entry.responseStart)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/HTTPRequest]' + route, {
-        startTime: entry.requestStart
+        startTime: entry.requestStart,
+        parentContext: pageLoadSpan
       }), entry.responseStart)
     }
 
     if (!shouldOmitSpan(entry.responseStart, entry.responseEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/HTTPResponse]' + route, {
-        startTime: entry.responseStart
+        startTime: entry.responseStart,
+        parentContext: pageLoadSpan
       }), entry.responseEnd)
     }
 
     if (!shouldOmitSpan(entry.domContentLoadedEventStart, entry.domContentLoadedEventEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/DomContentLoadedEvent]' + route, {
-        startTime: entry.domContentLoadedEventStart
+        startTime: entry.domContentLoadedEventStart,
+        parentContext: pageLoadSpan
       }), entry.domContentLoadedEventEnd)
     }
 
     if (!shouldOmitSpan(entry.loadEventStart, entry.loadEventEnd)) {
       spanFactory.endSpan(spanFactory.startSpan('[PageLoadPhase/LoadEvent]' + route, {
-        startTime: entry.loadEventStart
+        startTime: entry.loadEventStart,
+        parentContext: pageLoadSpan
       }), entry.loadEventEnd)
     }
   }
