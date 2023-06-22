dd0a6f7b7b1ba09e53006ed167fa6a311d706af1
Joe Haines <hello@joehaines.co.uk>
2023-06-22 14:46:10 +0100

    Don't warn when a span was discarded before ending
    It will be fairly common for a span to be discarded before being ended
    as we do this automatically when the page is backgrounded
    
    Now we will only warn if a span is explicitly ended multiple times

--- packages/core/tests/span.test.ts
+++ packages/core/tests/span.test.ts
@@ -433,22 +433,21 @@ describe('Span', () => {
       backgroundingListener.sendToBackground()
       movedToBackground.end()
 
-      expect(logger.warn).toHaveBeenCalledWith('Attempted to end a Span which has already ended or been discarded.')
-      expect(logger.warn).toHaveBeenCalledTimes(1)
+      expect(logger.warn).not.toHaveBeenCalled()
 
       // started in background and ended in foreground
       const movedToForeground = client.startSpan('moved-to-foreground')
       backgroundingListener.sendToForeground()
       movedToForeground.end()
 
-      expect(logger.warn).toHaveBeenCalledTimes(2)
+      expect(logger.warn).not.toHaveBeenCalled()
 
       // entirely in background
       backgroundingListener.sendToBackground()
       const backgroundSpan = client.startSpan('entirely-in-background')
       backgroundSpan.end()
 
-      expect(logger.warn).toHaveBeenCalledTimes(3)
+      expect(logger.warn).not.toHaveBeenCalled()
 
       // started and ended in foreground but backgrounded during span
       backgroundingListener.sendToForeground()
@@ -457,7 +456,7 @@ describe('Span', () => {
       backgroundingListener.sendToForeground()
       backgroundedDuringSpan.end()
 
-      expect(logger.warn).toHaveBeenCalledTimes(4)
+      expect(logger.warn).not.toHaveBeenCalled()
 
       // entirely in foreground (should be delivered)
       const inForeground = client.startSpan('entirely-in-foreground')
@@ -465,7 +464,7 @@ describe('Span', () => {
 
       await jest.runOnlyPendingTimersAsync()
 
-      expect(logger.warn).toHaveBeenCalledTimes(4)
+      expect(logger.warn).not.toHaveBeenCalled()
 
       expect(delivery).not.toHaveSentSpan(expect.objectContaining({
         name: 'moved-to-background'
@@ -517,7 +516,7 @@ describe('Span', () => {
 
       await jest.runOnlyPendingTimersAsync()
 
-      expect(logger.warn).toHaveBeenCalledWith('Attempted to end a Span which has already ended or been discarded.')
+      expect(logger.warn).toHaveBeenCalledWith('Attempted to end a Span which has already ended.')
       expect(delivery.requests).toHaveLength(1)
     })
   })
