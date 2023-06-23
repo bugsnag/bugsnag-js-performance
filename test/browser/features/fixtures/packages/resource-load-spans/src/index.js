17b2df7515fa75cea800d59b191cc8e8dec05a3e
Ben Wilson <ben.wilson@smartbear.com>
2023-06-23 15:55:49 +0100

    test: :white_check_mark: defer loading image to ensure span ordering

--- test/browser/features/fixtures/packages/resource-load-spans/src/index.js
+++ test/browser/features/fixtures/packages/resource-load-spans/src/index.js
@@ -9,5 +9,11 @@ const span = BugsnagPerformance.startSpan("[Custom]/resource-load-spans")
 BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 3, batchInactivityTimeoutMs: 3000, autoInstrumentFullPageLoads: false, autoInstrumentNetworkRequests: false })
 
 document.getElementById("end-span").addEventListener("click", () => {
-    span.end()
+    const node = document.getElementById("image-container")
+    const img = new Image()
+    node.appendChild(img)
+    img.src = "/favicon.png?height=100&width=100"
+    img.onload = () => {
+        span.end()
+    }
 })
