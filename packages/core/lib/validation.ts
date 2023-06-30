f10f474fbbd2c7540de3b6b611e6f54edf27ff3f
Yousif Ahmed <yousif@bugsnag.com>
2023-06-30 12:23:40 +0100

    add isTime validation

--- packages/core/lib/validation.ts
+++ packages/core/lib/validation.ts
@@ -1,6 +1,7 @@
 import { type Logger } from './config'
 import { type PersistedProbability } from './persistence'
 import { type SpanContext } from './span-context'
+import { type Time } from './time'
 
 export const isBoolean = (value: unknown): value is boolean =>
   value === true || value === false
@@ -38,3 +39,7 @@ export const isSpanContext = (value: unknown): value is SpanContext =>
     typeof value.id === 'string' &&
     typeof value.traceId === 'string' &&
     typeof value.isValid === 'function'
+
+export function isTime (value: unknown): value is Time {
+  return isNumber(value) || value instanceof Date
+}
