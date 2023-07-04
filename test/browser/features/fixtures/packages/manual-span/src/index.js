1f0cb26a71a7f0d28a2c0b399886c2ce0cd94679
Ben Wilson <ben.wilson@smartbear.com>
2023-07-04 10:52:19 +0100

    test: :white_check_mark: add every combination of valid start/end times

--- test/browser/features/fixtures/packages/manual-span/src/index.js
+++ test/browser/features/fixtures/packages/manual-span/src/index.js
@@ -4,7 +4,7 @@ const parameters = new URLSearchParams(window.location.search)
 const apiKey = parameters.get('api_key')
 const endpoint = parameters.get('endpoint')
 
-BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 1, autoInstrumentFullPageLoads: false, autoInstrumentNetworkRequests: false })
+BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 9, autoInstrumentFullPageLoads: false, autoInstrumentNetworkRequests: false })
 
 const spanOptions = {}
 
@@ -12,5 +12,29 @@ if (parameters.has('isFirstClass')) {
   spanOptions.isFirstClass = JSON.parse(parameters.get('isFirstClass'))
 }
 
-const span = BugsnagPerformance.startSpan("Custom/ManualSpanScenario", spanOptions)
-span.end()
+function getTime (type) {
+  switch (type) {
+    case 'date':
+      return new Date()
+    case 'number':
+      return performance.now()
+  }
+}
+
+const combinations = [
+  ['date', 'date'],
+  ['date', 'number'],
+  ['date', 'undefined'],
+  ['number', 'date'],
+  ['number', 'number'],
+  ['number', 'undefined'],
+  ['undefined', 'date'],
+  ['undefined', 'number'],
+  ['undefined', 'undefined'],
+]
+
+for (const combination of combinations) {
+  spanOptions.startTime = getTime(combination[0])
+  const span = BugsnagPerformance.startSpan("Custom/ManualSpanScenario", spanOptions)
+  span.end(getTime(combination[1]))
+}
