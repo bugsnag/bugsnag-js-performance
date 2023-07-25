e800571486c3fb9f43fcfa9958144b53e8728697
Ben Wilson <ben.wilson@smartbear.com>
2023-07-25 16:48:50 +0100

    test: :white_check_mark: Add end to end tests
    add end to end tests for modifying urls and preventing delivery of spans using networkRequestCallback

---
+++ test/browser/features/fixtures/packages/network-span-control/src/index.js
@@ -0,0 +1,48 @@
+import BugsnagPerformance from '@bugsnag/browser-performance'
+
+const parameters = new URLSearchParams(window.location.search)
+const apiKey = parameters.get('api_key')
+const endpoint = parameters.get('endpoint')
+const reflectEndpoint = '/reflect?status=200&delay_ms=0'
+
+const commonConfig = {
+  apiKey,
+  endpoint,
+  autoInstrumentFullPageLoads: false,
+  autoInstrumentNetworkRequests: true,
+  maximumBatchSize: 1,
+}
+
+const modifiedUrlConfig = { 
+  ...commonConfig,
+  networkRequestCallback: (networkRequestInfo) => ({ ...networkRequestInfo, url: 'not-your-ordinary-url' })
+}
+
+document.getElementById('fetch-modified-url').addEventListener('click', () => {
+  BugsnagPerformance.start(modifiedUrlConfig)
+  fetch(reflectEndpoint)
+})
+
+document.getElementById('xhr-modified-url').addEventListener('click', () => {
+  BugsnagPerformance.start(modifiedUrlConfig)
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', reflectEndpoint)
+  xhr.send()
+})
+
+const deliveryPreventedConfig = { 
+  ...commonConfig,
+  networkRequestCallback: () => null
+}
+
+document.getElementById('fetch-prevented').addEventListener('click', () => {
+  BugsnagPerformance.start(deliveryPreventedConfig)
+  fetch(reflectEndpoint)
+})
+
+document.getElementById('xhr-prevented').addEventListener('click', () => {
+  BugsnagPerformance.start(deliveryPreventedConfig)
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', reflectEndpoint)
+  xhr.send()
+})
