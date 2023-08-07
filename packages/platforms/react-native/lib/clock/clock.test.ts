9e0b4217fda53ee07e8e92e23eabed8ac32afdda
Ben Wilson <ben.wilson@smartbear.com>
2023-08-07 10:30:24 +0100

    test: :white_check_mark: add tests for clock.date()

--- packages/platforms/react-native/lib/clock/clock.test.ts
+++ packages/platforms/react-native/lib/clock/clock.test.ts
@@ -29,4 +29,33 @@ describe('React Native Clock', () => {
       }
     })
   })
+
+  describe('clock.date()', () => {
+    it('returns a Date', async () => {
+      const timeOrigin = new Date('2023-01-02T00:00:00.000Z').getTime()
+
+      const clock = createClock(timeOrigin, performance)
+
+      await jest.advanceTimersByTimeAsync(100)
+
+      const advancedDate = new Date(timeOrigin + 100)
+      expect(clock.date()).toEqual(advancedDate)
+    })
+
+    it('returns a greater date on every invocation (100 runs)', async () => {
+      const timeOrigin = new Date('2023-01-02T00:00:00.000Z')
+
+      let lastDate = timeOrigin
+      const clock = createClock(timeOrigin.getTime(), performance)
+
+      for (let i = 0; i < 100; i++) {
+        await jest.advanceTimersByTimeAsync(10)
+
+        const newDate = clock.date()
+
+        expect(newDate).toEqual(new Date(lastDate.getTime() + 10))
+        lastDate = newDate
+      }
+    })
+  })
 })
