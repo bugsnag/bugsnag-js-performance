3b42ee6ffe4d0f0da3c65709f2400cf5df835d3f
Ben Wilson <ben.wilson@smartbear.com>
2023-08-09 11:37:41 +0100

    refactor: :recycle: rename internal now function

--- packages/platforms/react-native/lib/clock/clock.ts
+++ packages/platforms/react-native/lib/clock/clock.ts
@@ -9,25 +9,24 @@ const createClock = (performance: Performance): Clock => {
   // React Native 0.68 introduced a bug where `performance.now` was no longer monotonic https://github.com/facebook/react-native/issues/33977
   // NOTE: If our new "now" is less than the previous "now" - we will use the previous value.
   let previousClock = performance.now()
-  const now = () => {
+  const currentPerfTime = () => {
     const newClock = performance.now()
-    if (newClock > previousClock) { 
+    if (newClock > previousClock) {
       previousClock = newClock
-      return newClock 
+      return newClock
     }
     return previousClock
   }
 
-  // Get timestamps at same point in time
-  const startWallTime = Date.now() // Wall time
   // Measurable "monotonic" time
   // In React Native, `performance.now` often returns some very high values, but does not expose the `timeOrigin` it uses to calculate what "now" is.
   // by storing the value of `performance.now` when the app starts, we can remove that value from any further `.now` calculations, and add it to the current "wall time" to get a useful timestamp.
-  const startPerfTime = now()
+  const startPerfTime = currentPerfTime()
+  const startWallTime = Date.now()
 
   return {
-    now,
-    date: () => new Date(now() - startPerfTime + startWallTime),
+    now: currentPerfTime,
+    date: () => new Date(currentPerfTime() - startPerfTime + startWallTime),
     convert: (date: Date) => date.getTime() - startWallTime + startPerfTime,
     // convert milliseconds since timeOrigin to full timestamp
     toUnixTimestampNanoseconds: (time: number) =>
