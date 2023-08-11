d8214e6f6cc40dfc5a8c56e2892ccf0a22828a7b
Ben Wilson <ben.wilson@smartbear.com>
2023-08-11 14:20:32 +0100

    feat: :sparkles: add react native package with resource attributes source

--- packages/platforms/react-native/lib/resource-attributes-source/resource-attributes-source.ts
+++ packages/platforms/react-native/lib/resource-attributes-source/resource-attributes-source.ts
@@ -1,4 +1,5 @@
 import { ResourceAttributes, type Configuration, type InternalConfiguration } from '@bugsnag/core-performance'
+import { Platform } from 'react-native'
 
 export function resourceAttributesSource (config: InternalConfiguration<Configuration>) {
   const attributes = new ResourceAttributes(
@@ -7,6 +8,19 @@ export function resourceAttributesSource (config: InternalConfiguration<Configur
     'bugsnag.performance.reactnative',
     '__VERSION__'
   )
+
+  attributes.set('os.type', Platform.select({ android: 'linux', ios: 'darwin', default: 'unknown' }))
+  attributes.set('os.name', Platform.OS)
+  attributes.set('os.version', Platform.Version.toString())
+  attributes.set('service.name', 'unknown')
+  attributes.set('device.id', 'unknown')
+  attributes.set('bugsnag.app.code_bundle_id', 'unknown')
+
+  // @ts-expect-error Platform.constants.Manufacturer exists on android devices
+  attributes.set('device.manufacturer', Platform.select({ ios: 'Apple', default: Platform.constants.Manufacturer }))
+
+  // @ts-expect-error Platform.constants.Serial exists on android devices
+  attributes.set('device.model.identifier', Platform.select({ android: Platform.constants.Serial, default: 'unknown' }))
 
   return Promise.resolve(attributes)
 }
