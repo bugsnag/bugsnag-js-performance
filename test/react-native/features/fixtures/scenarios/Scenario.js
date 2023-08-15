bcb3a0cfd28e1aa93f7a4a7bcfe76fa8d472422a
Yousif Ahmed <yousif@bugsnag.com>
2023-08-15 15:57:58 +0100

    Add scenarios package

---
+++ test/react-native/features/fixtures/scenarios/Scenario.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export const Scenario = () => {
+  return (
+    <View style={styles.scenario}>
+      <Text>Scenario</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  scenario: {
+    flex: 1
+  }
+})
