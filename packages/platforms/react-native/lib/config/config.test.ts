bf2e7ec6aee63606eeff1588434db9cec5f489ed
Ben Wilson <ben.wilson@smartbear.com>
2023-08-16 15:43:07 +0100

    test: :white_check_mark: update config tests

--- packages/platforms/react-native/lib/config/config.test.ts
+++ packages/platforms/react-native/lib/config/config.test.ts
@@ -1,18 +1,67 @@
 import { createSchema } from './config'
 
-describe('createSchema', () => {
-  it('includes all expected options', () => {
-    const schema = createSchema()
-    expect(Object.keys(schema)).toStrictEqual([
-      'appVersion',
-      'endpoint',
-      'apiKey',
-      'logger',
-      'releaseStage',
-      'enabledReleaseStages',
-      'samplingProbability',
-      'appName',
-      'codeBundleId'
-    ])
+describe('ReactNativeSchema', () => {
+  const schema = createSchema()
+
+  describe('appName', () => {
+    it('defaults to an empty string', () => {
+      expect(schema.appName.defaultValue).toBe('')
+    })
+
+    it.each([
+      [false, 123],
+      [false, false],
+      [false, null],
+      [false, undefined],
+      [false, ''],
+      [false, 'a'],
+      [false, /a/],
+      [false, {}],
+      [false, { a: 1, b: 2 }],
+      [false, [[]]],
+      [false, [['a']]],
+      [false, ['a', /b/, 1]],
+      [true, []],
+      [true, ['a']],
+      [true, [/a/]],
+      [true, ['a', 'b', 'c']],
+      [true, [/a/, /b/, /c/]],
+      [true, ['a', /b/, 'c']]
+    ])('returns %s from validation for the value %p', (expected, value) => {
+      const schema = createSchema()
+      const validate = schema.appName.validate
+      expect(validate(value)).toBe(expected)
+    })
+  })
+
+  describe('codeBundleId', () => {
+    it('defaults to an empty string', () => {
+      expect(schema.codeBundleId.defaultValue).toBe('')
+    })
+
+    it.each([
+      [false, 123],
+      [false, false],
+      [false, null],
+      [false, undefined],
+      [false, ''],
+      [false, 'a'],
+      [false, /a/],
+      [false, {}],
+      [false, { a: 1, b: 2 }],
+      [false, [[]]],
+      [false, [['a']]],
+      [false, ['a', /b/, 1]],
+      [true, []],
+      [true, ['a']],
+      [true, [/a/]],
+      [true, ['a', 'b', 'c']],
+      [true, [/a/, /b/, /c/]],
+      [true, ['a', /b/, 'c']]
+    ])('returns %s from validation for the value %p', (expected, value) => {
+      const schema = createSchema()
+      const validate = schema.codeBundleId.validate
+      expect(validate(value)).toBe(expected)
+    })
   })
 })
