8ae8aac72d6c51c697ca57afb3c9ae9abad06d3c
Ben Wilson <ben.wilson@smartbear.com>
2023-08-16 15:50:18 +0100

    update config validation

--- packages/platforms/react-native/lib/config/config.test.ts
+++ packages/platforms/react-native/lib/config/config.test.ts
@@ -1,5 +1,26 @@
 import { createSchema } from './config'
 
+const stringValidation = [
+  { expected: false, value: 123 },
+  { expected: false, value: false },
+  { expected: false, value: null },
+  { expected: false, value: undefined },
+  { expected: false, value: '' },
+  { expected: false, value: /a/ },
+  { expected: false, value: {} },
+  { expected: false, value: { a: 1, b: 2 } },
+  { expected: false, value: [[]] },
+  { expected: false, value: [['a']] },
+  { expected: false, value: ['a', /b/, 1] },
+  { expected: false, value: [] },
+  { expected: false, value: ['a'] },
+  { expected: false, value: [/a/] },
+  { expected: false, value: ['a', 'b', 'c'] },
+  { expected: false, value: [/a/, /b/, /c/] },
+  { expected: false, value: ['a', /b/, 'c'] },
+  { expected: true, value: 'a' }
+]
+
 describe('ReactNativeSchema', () => {
   const schema = createSchema()
 
@@ -8,26 +29,7 @@ describe('ReactNativeSchema', () => {
       expect(schema.appName.defaultValue).toBe('')
     })
 
-    it.each([
-      [false, 123],
-      [false, false],
-      [false, null],
-      [false, undefined],
-      [false, ''],
-      [false, 'a'],
-      [false, /a/],
-      [false, {}],
-      [false, { a: 1, b: 2 }],
-      [false, [[]]],
-      [false, [['a']]],
-      [false, ['a', /b/, 1]],
-      [true, []],
-      [true, ['a']],
-      [true, [/a/]],
-      [true, ['a', 'b', 'c']],
-      [true, [/a/, /b/, /c/]],
-      [true, ['a', /b/, 'c']]
-    ])('returns %s from validation for the value %p', (expected, value) => {
+    it.each(stringValidation)('returns $expected from validation for the value $value', ({ expected, value }) => {
       const schema = createSchema()
       const validate = schema.appName.validate
       expect(validate(value)).toBe(expected)
@@ -39,26 +41,7 @@ describe('ReactNativeSchema', () => {
       expect(schema.codeBundleId.defaultValue).toBe('')
     })
 
-    it.each([
-      [false, 123],
-      [false, false],
-      [false, null],
-      [false, undefined],
-      [false, ''],
-      [false, 'a'],
-      [false, /a/],
-      [false, {}],
-      [false, { a: 1, b: 2 }],
-      [false, [[]]],
-      [false, [['a']]],
-      [false, ['a', /b/, 1]],
-      [true, []],
-      [true, ['a']],
-      [true, [/a/]],
-      [true, ['a', 'b', 'c']],
-      [true, [/a/, /b/, /c/]],
-      [true, ['a', /b/, 'c']]
-    ])('returns %s from validation for the value %p', (expected, value) => {
+    it.each(stringValidation)('returns $expected from validation for the value $value', ({ expected, value }) => {
       const schema = createSchema()
       const validate = schema.codeBundleId.validate
       expect(validate(value)).toBe(expected)
