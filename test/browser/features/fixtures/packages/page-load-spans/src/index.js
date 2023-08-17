bf9ece060ef19e8127f647e48d3a04686319cae8
Ben Wilson <ben.wilson@smartbear.com>
2023-08-17 16:56:13 +0100

    test: :white_check_mark: use custom routing provider in page load span fixture

--- test/browser/features/fixtures/packages/page-load-spans/src/index.js
+++ test/browser/features/fixtures/packages/page-load-spans/src/index.js
@@ -1,7 +1,38 @@
-import BugsnagPerformance from '@bugsnag/browser-performance'
+import BugsnagPerformance, { onSettle } from '@bugsnag/browser-performance'
 
 const parameters = new URLSearchParams(window.location.search)
 const apiKey = parameters.get('api_key')
 const endpoint = parameters.get('endpoint')
 
-BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 13, batchInactivityTimeoutMs: 5000, autoInstrumentNetworkRequests: false })
+class CustomRoutingProvider {
+    resolveRoute = (url) => url.pathname || '/'
+
+    listenForRouteChanges (startRouteChangeSpan) {
+      addEventListener('popstate', (ev) => {
+        const url = new URL(window.location.href)
+        const span = startRouteChangeSpan(url, 'popstate')
+
+        onSettle((endTime) => {
+          span.end(endTime)
+        })
+      })
+
+      const originalPushState = history.pushState
+      history.pushState = function (...args) {
+        const url = args[2]
+
+        if (url) {
+          const absoluteURL = new URL(url.toString(), document.baseURI)
+          const span = startRouteChangeSpan(absoluteURL, 'pushState')
+
+          onSettle((endTime) => {
+            span.end(endTime)
+          })
+        }
+
+        originalPushState.apply(this, args)
+      }
+    }
+  }
+
+BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 13, batchInactivityTimeoutMs: 5000, autoInstrumentNetworkRequests: false, routingProvider: new CustomRoutingProvider() })
