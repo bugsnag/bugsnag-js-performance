4ddffd9720f22d93421fe383ef43bfd128a480f6
Yousif Ahmed <yousif@bugsnag.com>
2023-08-17 09:03:59 +0100

    Add debug logging to scenario

--- test/react-native/features/fixtures/scenarios/ConfigFileReader.js
+++ test/react-native/features/fixtures/scenarios/ConfigFileReader.js
@@ -1,11 +1,48 @@
 import { Platform } from 'react-native'
 import { Dirs, FileSystem } from 'react-native-file-access'
 
+const checkDirectoryExists = async () => {
+  const configFileDir = Platform.OS === 'android'
+    ? `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files/`
+    : Dirs.DocumentDir
+
+  const startTime = Date.now()
+  let directoryExists = false
+
+  while (Date.now() - startTime < 30000) {
+    directoryExists = await FileSystem.exists(configFileDir)
+    if (directoryExists) {
+      break
+    }
+  }
+
+  return directoryExists
+}
+
+const listDirectoryContents = async () => {
+  const configFileDir = Platform.OS === 'android'
+    ? `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files/`
+    : Dirs.DocumentDir
+
+  const startTime = Date.now()
+  let directoryContents = []
+
+  while (Date.now() - startTime < 30000) {
+    directoryContents = await FileSystem.ls(configFileDir)
+    if (directoryContents.length > 0) {
+      break
+    }
+  }
+
+  return JSON.stringify(directoryContents)
+}
+
 const getMazeRunnerAddress = async () => {
   const configFilePath = Platform.OS === 'android'
     ? `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files/fixture_config.json`
     : `${Dirs.DocumentDir}/fixture_config.json`
 
+  console.log(`fixture config file path: ${configFilePath}`)
   const startTime = Date.now()
   let configFileExists = false
 
@@ -26,4 +63,6 @@ const getMazeRunnerAddress = async () => {
   return config.maze_address
 }
 
+module.exports.checkDirectoryExists = checkDirectoryExists
+module.exports.listDirectoryContents = listDirectoryContents
 module.exports.getMazeRunnerAddress = getMazeRunnerAddress
--- test/react-native/features/fixtures/scenarios/Scenario.js
+++ test/react-native/features/fixtures/scenarios/Scenario.js
@@ -1,20 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { getMazeRunnerAddress } from './ConfigFileReader'
+import { getMazeRunnerAddress, checkDirectoryExists, listDirectoryContents } from './ConfigFileReader'
 
 export const Scenario = () => {
+  const [directoryExists, setDirectoryExists] = useState('Waiting for directory...')
+  const [directoryContents, setDirectoryContents] = useState('Waiting for directory...')
   const [mazeAddress, setMazeAddress] = useState('Waiting for maze address...')
 
   useEffect(() => {
+    const checkDirectory = async () => {
+      const exists = await checkDirectoryExists()
+      setDirectoryExists(`${exists}`)
+    }
+
+    const lsDirectory = async () => {
+      const contents = await listDirectoryContents()
+      setDirectoryContents(contents)
+    }
+
     const getMazeAddress = async () => {
       const mazeAddress = await getMazeRunnerAddress()
       setMazeAddress(mazeAddress)
     }
+
+    checkDirectory()
+    lsDirectory()
     getMazeAddress()
-  })
+  }, [])
 
   return (
     <View style={styles.scenario}>
+      <Text>{directoryExists}</Text>
+      <Text>{directoryContents}</Text>
       <Text accessibilityLabel='scenario'>{mazeAddress}</Text>
     </View>
   )
