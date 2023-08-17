3c775e94de37baeea230e6ef88b2f83e55fa3eef
Yousif Ahmed <yousif@bugsnag.com>
2023-08-17 17:50:21 +0100

    update config file reader for ios

--- test/react-native/features/fixtures/scenarios/ConfigFileReader.js
+++ test/react-native/features/fixtures/scenarios/ConfigFileReader.js
@@ -1,4 +1,5 @@
-import { FileSystem } from 'react-native-file-access'
+import { Platform } from 'react-native'
+import { Dirs, FileSystem } from 'react-native-file-access'
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -6,12 +7,12 @@ const getMazeRunnerAddress = async () => {
   const startTime = Date.now()
 
   // poll for the config file to exist
-  while (Date.now() - startTime < 120000) {
-    const tempDir = '/data/local/tmp'
-    const tempDirExists = await FileSystem.exists(tempDir)
-    console.log(`${tempDir} exists?: ${tempDirExists}`)
+  while (Date.now() - startTime < 60000) {
+    const configFileDir = Platform.OS === 'android' ? '/data/local/tmp' : Dirs.DocumentDir
+    const tempDirExists = await FileSystem.exists(configFileDir)
+    console.log(`${configFileDir} exists?: ${tempDirExists}`)
 
-    const configFilePath = `${tempDir}/fixture_config.json`
+    const configFilePath = `${configFileDir}/fixture_config.json`
     const configFileExists = await FileSystem.exists(configFilePath)
     console.log(`${configFilePath} exists?: ${configFileExists}`)
 
@@ -23,7 +24,7 @@ const getMazeRunnerAddress = async () => {
       return `${config.maze_address}`
     }
 
-    await delay(10000)
+    await delay(5000)
   }
 
   return 'bs-local:9339'
