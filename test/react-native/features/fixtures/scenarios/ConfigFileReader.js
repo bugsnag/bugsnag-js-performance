2526ffa95d762d74a55c023697d53e163056a753
Yousif Ahmed <yousif@bugsnag.com>
2023-08-17 12:11:20 +0100

    try to read directly from external storage

--- test/react-native/features/fixtures/scenarios/ConfigFileReader.js
+++ test/react-native/features/fixtures/scenarios/ConfigFileReader.js
@@ -8,11 +8,11 @@ const checkDirectoryExists = async () => {
   let emulatedDirExists = false
 
   while (Date.now() - startTime < 60000) {
-    const sdCardDir = `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files/`
+    const sdCardDir = `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files`
     sdCardDirExists = await FileSystem.exists(sdCardDir)
     console.log(`Directory ${sdCardDir} exists: ${sdCardDirExists}`)
 
-    const emulatedDir = '/storage/emulated/0/Android/data/com.bugsnag.fixtures.reactnative.performance/files/'
+    const emulatedDir = '/storage/emulated/0/Android/data/com.bugsnag.fixtures.reactnative.performance/files'
     emulatedDirExists = await FileSystem.exists(emulatedDir)
     console.log(`Directory ${emulatedDir} exists: ${emulatedDirExists}`)
 
@@ -42,41 +42,36 @@ const listDirectoryContents = async () => {
 }
 
 const getMazeRunnerAddress = async () => {
-  const configFilePath = `${Dirs.DocumentDir}/fixture_config.json`
   const startTime = Date.now()
 
   // poll for the config file to exist
   while (Date.now() - startTime < 60000) {
-    const emulatedExternalConfigFile = '/storage/emulated/0/Android/data/com.bugsnag.fixtures.reactnative.performance/files/fixture_config.json'
-    console.log(`Emulated storage file path: ${emulatedExternalConfigFile}`)
+    const externalConfigFile = `${Dirs.SDCardDir}/Android/data/com.bugsnag.fixtures.reactnative.performance/files/fixture_config.json`
+    const externalConfigFileExists = await FileSystem.exists(externalConfigFile)
+    console.log(`File ${externalConfigFile} exists: ${externalConfigFileExists}`)
 
+    if (externalConfigFileExists) {
+      const configFile = await FileSystem.readFile(externalConfigFile)
+      console.log(`config file raw contents: ${configFile}`)
+      const config = JSON.parse(configFile)
+      console.log(`config file JSON contents: ${JSON.stringify(config)}`)
+      return config.maze_address
+    }
+
+    const emulatedExternalConfigFile = '/storage/emulated/0/Android/data/com.bugsnag.fixtures.reactnative.performance/files/fixture_config.json'
     const emulatedExternalConfigFileExists = await FileSystem.exists(emulatedExternalConfigFile)
     console.log(`Emulated storage file '${emulatedExternalConfigFile}' exists: ${emulatedExternalConfigFileExists}`)
 
+    if (emulatedExternalConfigFileExists) {
+      const configFile = await FileSystem.readFile(emulatedExternalConfigFile)
+      console.log(`config file raw contents: ${configFile}`)
+      const config = JSON.parse(configFile)
+      console.log(`config file JSON contents: ${JSON.stringify(config)}`)
+      return config.maze_address
+    }
+
     await delay(10000)
   }
-
-  let configFileExists = false
-  try {
-    console.log('copying config file into document directory...')
-    await FileSystem.cp('/storage/emulated/0/Android/data/com.bugsnag.fixtures.reactnative.performance/files/fixture_config.json', configFilePath)
-    configFileExists = await FileSystem.exists(configFilePath)
-    console.log(`copied config file into document directory. success: ${configFileExists}`)
-  } catch (e) {
-    console.log('error copying config file: ', e)
-  }
-
-  if (!configFileExists) {
-    console.log('config file not fould, setting maze address to bs-local.com:9339')
-    return 'bs-local.com:9339'
-  }
-
-  console.log('config file exists! Attempting to read file...')
-  const configFile = await FileSystem.readFile(configFilePath)
-  console.log(`config file raw contents: ${configFile}`)
-  const config = JSON.parse(configFile)
-  console.log(`config file JSON contents: ${JSON.stringify(config)}`)
-  return config.maze_address
 }
 
 module.exports.checkDirectoryExists = checkDirectoryExists
--- test/react-native/features/fixtures/scenarios/Scenario.js
+++ test/react-native/features/fixtures/scenarios/Scenario.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import { getMazeRunnerAddress, checkDirectoryExists, listDirectoryContents } from './ConfigFileReader'
+import { getMazeRunnerAddress } from './ConfigFileReader'
 import { Dirs } from 'react-native-file-access'
 
 export const Scenario = () => {
-  const [directoryExists, setDirectoryExists] = useState('Waiting for directory...')
-  const [directoryContents, setDirectoryContents] = useState('Waiting for directory...')
+  // const [directoryExists, setDirectoryExists] = useState('Waiting for directory...')
+  // const [directoryContents, setDirectoryContents] = useState('Waiting for directory...')
   const [mazeAddress, setMazeAddress] = useState('Waiting for maze address...')
 
   useEffect(() => {
-    const checkDirectory = async () => {
-      const exists = await checkDirectoryExists()
-      setDirectoryExists(`${exists}`)
-    }
+    // const checkDirectory = async () => {
+    //   const exists = await checkDirectoryExists()
+    //   setDirectoryExists(`${exists}`)
+    // }
 
-    const lsDirectory = async () => {
-      const contents = await listDirectoryContents()
-      setDirectoryContents(contents)
-    }
+    // const lsDirectory = async () => {
+    //   const contents = await listDirectoryContents()
+    //   setDirectoryContents(contents)
+    // }
 
     const getMazeAddress = async () => {
       const mazeAddress = await getMazeRunnerAddress()
       setMazeAddress(mazeAddress)
     }
 
-    checkDirectory()
-    lsDirectory()
+    // checkDirectory()
+    // lsDirectory()
     getMazeAddress()
   }, [])
 
