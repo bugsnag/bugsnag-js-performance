d1c6deff25319acc80917aa1fb6b4a118480f88d
Yousif Ahmed <yousif@bugsnag.com>
2023-08-18 10:24:19 +0100

    Add a basic command runner

---
+++ test/react-native/features/steps/react-native-steps.rb
@@ -0,0 +1,28 @@
+def execute_command(action, scenario_name = '')
+
+  address = if Maze.config.farm == :bb
+              if Maze.config.aws_public_ip
+                Maze.public_address
+              else
+                'local:9339'
+              end
+            else
+              'bs-local.com:9339'
+            end
+
+  command = {
+    action: action,
+    scenario_name: scenario_name,
+    endpoint: "http://#{address}/traces",
+  }
+  Maze::Server.commands.add command
+
+  # Ensure fixture has read the command
+  count = 600
+  sleep 0.1 until Maze::Server.commands.remaining.empty? || (count -= 1) < 1
+  raise 'Test fixture did not GET /command' unless Maze::Server.commands.remaining.empty?
+end
+
+When('I run {string}') do |scenario_name|
+  execute_command 'run_scenario', scenario_name
+end
