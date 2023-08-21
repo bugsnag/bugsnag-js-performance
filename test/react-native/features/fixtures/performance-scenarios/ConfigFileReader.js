6278dcc233055b4a9523a60e10ae4b2a9d372a94
Yousif Ahmed <yousif@bugsnag.com>
2023-08-21 09:03:08 +0100

    simplify logging in config file reader

--- test/react-native/features/fixtures/performance-scenarios/ConfigFileReader.js
+++ test/react-native/features/fixtures/performance-scenarios/ConfigFileReader.js
@@ -4,29 +4,26 @@ import { Dirs, FileSystem } from 'react-native-file-access'
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const getMazeRunnerAddress = async () => {
+  let configFilePath
   const startTime = Date.now()
 
   // poll for the config file to exist
   while (Date.now() - startTime < 10000) {
     const configFileDir = Platform.OS === 'android' ? '/data/local/tmp' : Dirs.DocumentDir
-    const tempDirExists = await FileSystem.exists(configFileDir)
-    console.log(`${configFileDir} exists?: ${tempDirExists}`)
-
-    const configFilePath = `${configFileDir}/fixture_config.json`
+    configFilePath = `${configFileDir}/fixture_config.json`
     const configFileExists = await FileSystem.exists(configFilePath)
-    console.log(`${configFilePath} exists?: ${configFileExists}`)
 
     if (configFileExists) {
       const configFile = await FileSystem.readFile(configFilePath)
-      console.log(`config file raw contents: ${configFile}`)
+      console.log(`found config file at '${configFilePath}'. contents: ${configFile}`)
       const config = JSON.parse(configFile)
-      console.log(`config file JSON contents: ${JSON.stringify(config)}`)
       return `${config.maze_address}`
     }
 
     await delay(500)
   }
 
+  console.log(`no config file found at ${configFilePath}`)
   return 'bs-local:9339'
 }
 
