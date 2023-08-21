582f50b93032d95e87b89d6f5b8de2ae8a521270
Yousif Ahmed <yousif@bugsnag.com>
2023-08-21 17:55:43 +0100

    Rename CommandRunner.getCommand to getCurrentCommand for clarity

--- test/react-native/features/fixtures/performance-scenarios/ScenarioLauncher.js
+++ test/react-native/features/fixtures/performance-scenarios/ScenarioLauncher.js
@@ -1,9 +1,9 @@
 import { AppRegistry } from 'react-native'
-import { getCommand } from './CommandRunner'
+import { getCurrentCommand } from './CommandRunner'
 import * as Scenarios from './scenarios/Scenarios'
 
 export const launchScenario = async (rootTag) => {
-  const command = await getCommand()
+  const command = await getCurrentCommand()
   const scenario = Scenarios[command.scenario_name]
   AppRegistry.registerComponent('scenario', () => scenario(command.endpoint))
   AppRegistry.runApplication('scenario', { initialProps: {}, rootTag })
