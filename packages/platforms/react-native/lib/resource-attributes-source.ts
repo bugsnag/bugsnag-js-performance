4143227d24246823d569c941fc2364aa26757c92
Ben Wilson <ben.wilson@smartbear.com>
2023-08-22 15:13:04 +0100

    add persistence to react-native and get device id

--- packages/platforms/react-native/lib/resource-attributes-source.ts
+++ packages/platforms/react-native/lib/resource-attributes-source.ts
@@ -1,8 +1,15 @@
 import { ResourceAttributes, type InternalConfiguration } from '@bugsnag/core-performance'
 import { type ReactNativeConfiguration } from './config'
 import { Platform } from 'react-native'
+import { getReactNativePersistence } from './persistence'
+import cuid from '@bugsnag/cuid'
+
+const persistence = getReactNativePersistence()
 
 function resourceAttributesSource (config: InternalConfiguration<ReactNativeConfiguration>) {
+  let getDeviceId: Promise<string> | undefined
+  let deviceId: string | undefined
+
   const attributes = new ResourceAttributes(
     config.releaseStage,
     config.appVersion,
@@ -14,7 +21,6 @@ function resourceAttributesSource (config: InternalConfiguration<ReactNativeConf
   attributes.set('os.name', Platform.OS)
   attributes.set('os.version', Platform.Version.toString())
   attributes.set('service.name', config.appName)
-  attributes.set('device.id', 'unknown')
 
   if (config.codeBundleId) {
     attributes.set('bugsnag.app.code_bundle_id', config.codeBundleId)
@@ -25,6 +31,42 @@ function resourceAttributesSource (config: InternalConfiguration<ReactNativeConf
 
   // @ts-expect-error Platform.constants.Model exists on android devices
   attributes.set('device.model.identifier', Platform.select({ android: Platform.constants.Model, ios: 'unknown', default: 'unknown' }))
+
+  if (config.generateAnonymousId) {
+    // ensure we only load/generate the anonymous ID once no matter how many
+    // times we're called, otherwise we could generate different IDs on
+    // different calls as cuids are partly time based
+    if (!getDeviceId) {
+      getDeviceId = persistence.load('bugsnag-anonymous-id')
+        .then(maybeAnonymousId => {
+          // use the persisted value or generate a new ID
+          const anonymousId = maybeAnonymousId || cuid()
+
+          // if there was no persisted value, save the newly generated ID
+          if (!maybeAnonymousId) {
+            persistence.save('bugsnag-anonymous-id', anonymousId)
+          }
+
+          // store the device ID so we can set it synchronously in future
+          deviceId = anonymousId
+
+          return deviceId
+        })
+    }
+
+    if (deviceId) {
+      // set device ID synchronously if it's already available
+      attributes.set('device.id', deviceId)
+    } else {
+      // otherwise add it when the promise resolves
+      return getDeviceId
+        .then(deviceId => {
+          attributes.set('device.id', deviceId)
+
+          return attributes
+        })
+    }
+  }
 
   return Promise.resolve(attributes)
 }
