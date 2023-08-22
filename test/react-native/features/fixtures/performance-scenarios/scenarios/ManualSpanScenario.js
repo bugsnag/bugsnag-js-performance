57f777b4f68002180368af43cd19b7d30ee1d5e3
Yousif Ahmed <yousif@bugsnag.com>
2023-08-22 08:03:16 +0100

    Set maximumBatchSize in test config

--- test/react-native/features/fixtures/performance-scenarios/scenarios/ManualSpanScenario.js
+++ test/react-native/features/fixtures/performance-scenarios/scenarios/ManualSpanScenario.js
@@ -5,7 +5,7 @@ import BugsnagPerformance from '@bugsnag/react-native-performance'
 export const ManualSpanScenario = (endpoint, apiKey) => {
   const App = () => {
     useEffect(() => {
-      BugsnagPerformance.start({ apiKey, endpoint })
+      BugsnagPerformance.start({ apiKey, endpoint, maximumBatchSize: 1 })
       BugsnagPerformance.startSpan('ManualSpanScenario').end()
     }, [])
 
