8ec911e164553651adb9531fc0f58fe839ef81de
Yousif Ahmed <yousif@bugsnag.com>
2023-08-23 17:25:19 +0100

    Add app start instrumentation plugin

---
+++ packages/platforms/react-native/lib/auto-instrumentation/app-start-plugin.tsx
@@ -0,0 +1,40 @@
+import {
+  type InternalConfiguration,
+  type Plugin,
+  type SpanFactory,
+  type Clock
+} from '@bugsnag/core-performance'
+import { type ReactNativeConfiguration } from '../config'
+import { useEffect } from 'react'
+import { type WrapperComponentProvider, AppRegistry } from 'react-native'
+
+export class AppStartPlugin implements Plugin<ReactNativeConfiguration> {
+  constructor (
+    private readonly spanFactory: SpanFactory<ReactNativeConfiguration>,
+    private readonly clock: Clock
+  ) {}
+
+  configure (configuration: InternalConfiguration<ReactNativeConfiguration>) {
+    if (!configuration.autoInstrumentAppStarts) return
+
+    const appStartSpan = this.spanFactory.startSpan('[AppStart/ReactNativeInit]', { parentContext: null })
+    appStartSpan.setAttribute('bugsnag.span.category', 'app_start')
+    appStartSpan.setAttribute('bugsnag.app_start.type', 'ReactNativeInit')
+
+    const instrumentedComponentProvider: WrapperComponentProvider = (appProps) => {
+      const endSpan = () => {
+        this.spanFactory.endSpan(appStartSpan, this.clock.now())
+      }
+
+      return ({children}) => {
+        useEffect(() => {
+          endSpan()
+        }, [])
+
+        return <>{children}</>
+      }
+    }
+
+    AppRegistry.setWrapperComponentProvider(instrumentedComponentProvider)
+  }
+}
