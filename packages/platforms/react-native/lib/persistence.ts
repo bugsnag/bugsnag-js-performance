61a42d63c4f186562b6926b2df2047532e2bebad
Ben Wilson <ben.wilson@smartbear.com>
2023-08-23 17:53:18 +0100

    update asyncstorage check

--- packages/platforms/react-native/lib/persistence.ts
+++ packages/platforms/react-native/lib/persistence.ts
@@ -1,16 +1,23 @@
 import {
   InMemoryPersistence,
+  isObject,
   toPersistedPayload,
   type Persistence,
   type PersistenceKey,
   type PersistencePayloadMap
 } from '@bugsnag/core-performance'
-import AsyncStorage, { type AsyncStorageStatic } from '@react-native-async-storage/async-storage'
+import { type AsyncStorageStatic } from '@react-native-async-storage/async-storage'
+
+const isAsyncStorage = (value: unknown): value is AsyncStorageStatic => isObject(value) && typeof value.getItem === 'function' && typeof value.setItem === 'function'
 
 export function getReactNativePersistence (): Persistence {
   // use @react-native-async-storage/async-storage if it's installed
   try {
-    if (AsyncStorage) return new ReactNativePersistence(AsyncStorage)
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const asyncStorage = require('@react-native-async-storage/async-storage').default
+    if (isAsyncStorage(asyncStorage)) {
+      return new ReactNativePersistence(asyncStorage)
+    }
   } catch {}
 
   // store items in memory if @react-native-async-storage/async-storage isn't available
