1c529eeda90d827787fd5401b3365508d8593655
Yousif Ahmed <yousif@bugsnag.com>
2023-08-23 15:08:49 +0100

    fix RN Android e2e tests when running locally on BrowserStack

--- test/react-native/features/support/env.rb
+++ test/react-native/features/support/env.rb
@@ -1,4 +1,6 @@
 BeforeAll do
-  Maze.config.android_app_files_directory = '/data/local/tmp'
+  if Maze.config.farm == :bb
+    Maze.config.android_app_files_directory = '/data/local/tmp'
+  end
   Maze.config.enforce_bugsnag_integrity = false
 end
