1996bac31aaeaa1e908b82255e110ff63770afe6
Yousif Ahmed <yousif@bugsnag.com>
2023-08-25 12:14:58 +0100

    Fix rollup config

--- packages/platforms/react-native/rollup.config.mjs
+++ packages/platforms/react-native/rollup.config.mjs
@@ -2,6 +2,6 @@ import createRollupConfig from '../../../.rollup/index.mjs'
 import jsx from 'acorn-jsx'
 
 export default createRollupConfig({
-  external: ['@bugsnag/delivery-fetch-performance', 'react-native', 'react' '@bugsnag/cuid', '@react-native-async-storage/async-storage'],
+  external: ['@bugsnag/delivery-fetch-performance', 'react-native', 'react', '@bugsnag/cuid', '@react-native-async-storage/async-storage'],
   acornInjectPlugins: [jsx()],
 })
