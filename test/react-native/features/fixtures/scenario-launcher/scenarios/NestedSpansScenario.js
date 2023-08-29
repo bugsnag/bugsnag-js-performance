6b5c9d7a5e666a80291fb62f01731d109215e838
Ben Wilson <ben.wilson@smartbear.com>
2023-08-29 16:40:15 +0100

    test: :white_check_mark: test spans become nested as expected

---
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/NestedSpansScenario.js
@@ -0,0 +1,34 @@
+import BugsnagPerformance from '@bugsnag/react-native-performance'
+import React, { useEffect } from 'react'
+import { Text, View } from 'react-native'
+
+export const config = {
+    maximumBatchSize: 4,
+    autoInstrumentAppStarts: false,
+}
+
+const createAndEndSpan = (name) => new Promise((resolve) => {
+    const span = BugsnagPerformance.startSpan(name)
+    setTimeout(() => {
+        span.end()
+        resolve()
+    }, 100)
+})
+
+export const App = () => {
+    useEffect(() => {
+        (async () => {
+            const parentSpan = BugsnagPerformance.startSpan('Parent Span')
+            await createAndEndSpan('Nested Span 1')
+            await createAndEndSpan('Nested Span 2')
+            await createAndEndSpan('Nested Span 3')
+            parentSpan.end()
+        })()
+    }, [])
+
+    return (
+        <View>
+            <Text>NestedSpansScenario</Text>
+        </View>
+    )
+}
--- test/react-native/features/fixtures/scenario-launcher/scenarios/index.js
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/index.js
@@ -1,3 +1,5 @@
-export * as ManualSpanScenario from './ManualSpanScenario'
 export * as AppStartScenario from './AppStartScenario'
+export * as ManualSpanScenario from './ManualSpanScenario'
+export * as NestedSpansScenario from './NestedSpansScenario'
 export * as WrapperComponentProviderScenario from './WrapperComponentProviderScenario'
+
