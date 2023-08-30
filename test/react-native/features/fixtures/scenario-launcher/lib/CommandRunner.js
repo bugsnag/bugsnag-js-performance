57cc5e4b6ef85e8be5be27253642e8f577bfa34e
Ben Wilson <ben.wilson@smartbear.com>
2023-08-30 11:19:17 +0100

    add retry to getCurrentCommand

--- test/react-native/features/fixtures/scenario-launcher/lib/CommandRunner.js
+++ test/react-native/features/fixtures/scenario-launcher/lib/CommandRunner.js
@@ -1,27 +1,47 @@
 import { getMazeRunnerAddress } from './ConfigFileReader'
 
+const RETRY_COUNT = 3
+const INTERVAL = 100
+
 let mazeAddress
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const fetchCommand = (url) => new Promise(async (resolve, reject) => {
+  let retries = 0
+  let finalError
+
+  while (retries < RETRY_COUNT) {
+    try {
+      await delay(INTERVAL)
+      const response = await fetch(url)
+  
+      if (!response.ok) {
+        throw new Error(`Received ${response.status} response from maze runner`)
+      }
+  
+      const command = await response.json()
+      console.log(`Received command from maze runner: ${JSON.stringify(command)}`)
+  
+      resolve(command)
+    } catch (err) {
+      console.error(`Error fetching command from maze runner: ${err.message}`, err)
+      finalError = err
+      retries++
+    }
+  }
+
+  reject(finalError)
+})
 
 const getCurrentCommand = async () => {
   if (!mazeAddress) {
     mazeAddress = await getMazeRunnerAddress()
   }
 
-  const commandUrl = `http://${mazeAddress}/command`
+  const command = await fetchCommand(`http://${mazeAddress}/command`)
 
-  try {
-    const response = await fetch(commandUrl)
-    if (!response.ok) {
-      throw new Error(`Received ${response.status} response from maze runner`)
-    }
-
-    const command = await response.json()
-    console.log(`Received command from maze runner: ${JSON.stringify(command)}`)
-    return command
-  } catch (err) {
-    console.error(`Error fetching command from maze runner: ${err.message}`, err)
-    throw err
-  }
+  return command
 }
 
 module.exports.getCurrentCommand = getCurrentCommand
