98dc62e78cf7443f3530a39826cef6fc27e35073
Ben Wilson <ben.wilson@smartbear.com>
2023-09-05 11:16:45 +0100

    fix linting issues

--- packages/platforms/react-native/lib/resource-attributes-source.ts
+++ packages/platforms/react-native/lib/resource-attributes-source.ts
@@ -1,4 +1,4 @@
-import { Persistence, ResourceAttributes, type InternalConfiguration } from '@bugsnag/core-performance'
+import { type Persistence, ResourceAttributes, type InternalConfiguration } from '@bugsnag/core-performance'
 import cuid from '@bugsnag/cuid'
 import { Platform } from 'react-native'
 import { type ReactNativeConfiguration } from './config'
@@ -7,29 +7,29 @@ export function createResourceAttributesSource (persistence: Persistence) {
   return function resourceAttributesSource (config: InternalConfiguration<ReactNativeConfiguration>) {
     let getDeviceId: Promise<string> | undefined
     let deviceId: string | undefined
-  
+
     const attributes = new ResourceAttributes(
       config.releaseStage,
       config.appVersion,
       'bugsnag.performance.reactnative',
       '__VERSION__'
     )
-  
+
     attributes.set('os.type', Platform.select({ android: 'linux', ios: 'darwin', default: 'unknown' }))
     attributes.set('os.name', Platform.OS)
     attributes.set('os.version', Platform.Version.toString())
     attributes.set('service.name', config.appName)
-  
+
     if (config.codeBundleId) {
       attributes.set('bugsnag.app.code_bundle_id', config.codeBundleId)
     }
-  
+
     // @ts-expect-error Platform.constants.Manufacturer exists on android devices
     attributes.set('device.manufacturer', Platform.select({ android: Platform.constants.Manufacturer, ios: 'Apple', default: 'unknown' }))
-  
+
     // @ts-expect-error Platform.constants.Model exists on android devices
     attributes.set('device.model.identifier', Platform.select({ android: Platform.constants.Model, ios: 'unknown', default: 'unknown' }))
-  
+
     if (config.generateAnonymousId) {
       // ensure we only load/generate the anonymous ID once no matter how many
       // times we're called, otherwise we could generate different IDs on
@@ -39,19 +39,19 @@ export function createResourceAttributesSource (persistence: Persistence) {
           .then(maybeAnonymousId => {
             // use the persisted value or generate a new ID
             const anonymousId = maybeAnonymousId || cuid()
-  
+
             // if there was no persisted value, save the newly generated ID
             if (!maybeAnonymousId) {
               persistence.save('bugsnag-anonymous-id', anonymousId)
             }
-  
+
             // store the device ID so we can set it synchronously in future
             deviceId = anonymousId
-  
+
             return deviceId
           })
       }
-  
+
       if (deviceId) {
         // set device ID synchronously if it's already available
         attributes.set('device.id', deviceId)
@@ -60,12 +60,12 @@ export function createResourceAttributesSource (persistence: Persistence) {
         return getDeviceId
           .then(deviceId => {
             attributes.set('device.id', deviceId)
-  
+
             return attributes
           })
       }
     }
-  
+
     return Promise.resolve(attributes)
   }
 }
