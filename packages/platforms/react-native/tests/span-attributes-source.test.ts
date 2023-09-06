f63075c8baab09e35f1bc17c96838f3b84cac4b1
Ben Wilson <ben.wilson@smartbear.com>
2023-09-06 16:25:50 +0100

    test: :white_check_mark: unit test bugsnag.app.in_foreground attribute

---
+++ packages/platforms/react-native/tests/span-attributes-source.test.ts
@@ -0,0 +1,41 @@
+import { InMemoryDelivery, VALID_API_KEY, createTestClient } from '@bugsnag/js-performance-test-utilities'
+import { createSpanAttributesSource } from '../lib/span-attributes-source'
+import { type AppStateStatus } from 'react-native/Libraries/AppState/AppState'
+
+jest.useFakeTimers()
+
+describe('spanAttributesSource', () => {
+  describe('.requestAttributes()', () => {
+    it('sets the bugsnag.app.in_foreground attribute', async () => {
+      const mockAppState = { currentState: 'active' as AppStateStatus, isAvailable: true, addEventListener: jest.fn() }
+      const spanAttributesSource = createSpanAttributesSource(mockAppState)
+      const delivery = new InMemoryDelivery()
+      const testClient = createTestClient({ spanAttributesSource, deliveryFactory: () => delivery })
+
+      testClient.start({ apiKey: VALID_API_KEY, appName: 'test' })
+      testClient.startSpan('foreground span').end()
+
+      await jest.runOnlyPendingTimersAsync()
+
+      expect(delivery).toHaveSentSpan(expect.objectContaining({
+        name: 'foreground span',
+        attributes: expect.arrayContaining([
+          { key: 'bugsnag.app.in_foreground', value: { boolValue: true } }
+        ])
+      }))
+
+      mockAppState.currentState = 'background'
+
+      testClient.startSpan('background span').end()
+
+      await jest.runOnlyPendingTimersAsync()
+
+      expect(delivery).toHaveSentSpan(expect.objectContaining({
+        name: 'background span',
+        attributes: expect.arrayContaining([
+          { key: 'bugsnag.app.in_foreground', value: { boolValue: false } }
+        ])
+      }))
+    })
+  })
+})
