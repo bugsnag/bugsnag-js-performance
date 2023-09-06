b0de8480f63cec2c8d0c46b05630a49eca1403ca
Ben Wilson <ben.wilson@smartbear.com>
2023-09-06 12:39:19 +0100

    re-implement persistence

--- packages/platforms/react-native/lib/client.ts
+++ packages/platforms/react-native/lib/client.ts
@@ -1,25 +1,28 @@
-import { InMemoryPersistence, createClient } from '@bugsnag/core-performance'
+import { createClient } from '@bugsnag/core-performance'
 import createFetchDeliveryFactory from '@bugsnag/delivery-fetch-performance'
 import { AppRegistry, AppState } from 'react-native'
 import { AppStartPlugin } from './auto-instrumentation/app-start-plugin'
 import createClock from './clock'
 import createSchema from './config'
 import idGenerator from './id-generator'
+import { getReactNativePersistence } from './persistence'
 import { platformExtensions } from './platform-extensions'
-import resourceAttributesSource from './resource-attributes-source'
+import { createResourceAttributesSource } from './resource-attributes-source'
 import { createSpanAttributesSource } from './span-attributes-source'
 
 const clock = createClock(performance)
 const appStartTime = clock.now()
 const deliveryFactory = createFetchDeliveryFactory(fetch, clock)
 const spanAttributesSource = createSpanAttributesSource(AppState)
+const persistence = getReactNativePersistence()
+const resourceAttributesSource = createResourceAttributesSource(persistence)
 
 const BugsnagPerformance = createClient({
   backgroundingListener: { onStateChange: () => {} },
   clock,
   deliveryFactory,
   idGenerator,
-  persistence: new InMemoryPersistence(),
+  persistence,
   plugins: (spanFactory, spanContextStorage) => [new AppStartPlugin(appStartTime, spanFactory, clock, AppRegistry)],
   resourceAttributesSource,
   schema: createSchema(),
