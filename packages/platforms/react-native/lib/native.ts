32004e881a91a267398eb9d28ea38acdbd7593f5
Yousif Ahmed <yousif@bugsnag.com>
2023-09-08 13:10:43 +0100

    Import the native module in JS

---
+++ packages/platforms/react-native/lib/native.ts
@@ -0,0 +1,15 @@
+import { type TurboModule, TurboModuleRegistry } from 'react-native'
+
+interface DeviceInfo {
+  arch: string
+  versionCode: string
+  bundleVersion: string
+}
+
+interface BugsnagReactNativePerformance extends TurboModule {
+  getDeviceInfo: () => DeviceInfo
+}
+
+const NativeBugsnagPerformance = TurboModuleRegistry.get('BugsnagReactNativePerformance') || undefined
+
+export default NativeBugsnagPerformance as unknown as BugsnagReactNativePerformance
