b9a0e06e16bf80ee1cfa1ae7d78085c65104db2b
Yousif Ahmed <yousif@bugsnag.com>
2023-09-08 13:08:55 +0100

    Add ios native module and podspec

---
+++ packages/platforms/react-native/ios/BugsnagReactNativePerformance.mm
@@ -0,0 +1,35 @@
+#import "BugsnagReactNativePerformanceSpec.h"
+#import "BugsnagReactNativePerformance.h"
+
+@implementation BugsnagReactNativePerformance
+
+RCT_EXPORT_MODULE()
+
+static NSString *hostArch() noexcept {
+#if TARGET_CPU_ARM
+    return @"arm32";
+#elif TARGET_CPU_ARM64
+    return @"arm64";
+#elif TARGET_CPU_X86
+    return @"x86";
+#elif TARGET_CPU_X86_64
+    return @"amd64";
+#endif
+}
+
+- (NSDictionary *)getDeviceInfo {
+    NSMutableDictionary *info = [NSMutableDictionary new];
+    auto infoDictionary = NSBundle.mainBundle.infoDictionary;
+    info[@"arch"] = hostArch();
+    info[@"bundleVersion"] = infoDictionary[@"CFBundleVersion"] ?: @"";
+    info[@"versionCode"] = @"";
+    return info;
+}
+
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeBugsnagPerformanceSpecJSI>(params);
+}
+
+@end
