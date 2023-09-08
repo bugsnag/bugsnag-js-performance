dd383336f43d4924aa6ce3ebd1f3c9e944d0714c
Yousif Ahmed <yousif@bugsnag.com>
2023-09-08 13:08:55 +0100

    Add ios native module and podspec

---
+++ packages/platforms/react-native/ios/BugsnagReactNativePerformance.h
@@ -0,0 +1,9 @@
+#import <BugsnagReactNativePerformanceSpec/BugsnagReactNativePerformanceSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BugsnagReactNativePerformance : NSObject <NativeBugsnagPerformanceSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
