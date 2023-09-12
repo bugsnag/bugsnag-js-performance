050eeb6809139115604019fe898f39cd34500330
Joe Haines <hello@joehaines.co.uk>
2023-09-12 11:29:16 +0100

    Refactor fetchCommand to avoid reusing 'retries'

--- test/react-native/features/fixtures/scenario-launcher/lib/CommandRunner.js
+++ test/react-native/features/fixtures/scenario-launcher/lib/CommandRunner.js
@@ -4,44 +4,33 @@ const RETRY_COUNT = 20
 const INTERVAL = 500
 
 let mazeAddress
-let retries = 0
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const fetchCommand = async (url) => {
-  // poll periodically for the command - if we don't get one after 10 seconds, give up
-  try {
-    const response = await fetch(url)
-    const command = await response.json()
-    
-    // keep polling until a scenario command is received
-    if (command.action !== 'noop') {
-      console.error(`[BugsnagPerformance] Received command from maze runner: ${JSON.stringify(command)}`)
-      return command
-    } 
-    else if (retries < RETRY_COUNT) {
-      retries++
-      console.error(`[BugsnagPerformance] Received a noop command from maze runner, ${RETRY_COUNT - retries} retries remaining...`)
-      
-      await delay(INTERVAL)
-      return fetchCommand(url)
-    }
-    
-    throw new Error('Retry limit exceeded, giving up...')
-    
-  } catch (err) {
-    console.error(`[BugsnagPerformance] Error fetching command from maze runner: ${err.message}`, err)
+  let retries = 0
 
-    if(retries < RETRY_COUNT) {
-      retries++
-      console.error(`[BugsnagPerformance] ${RETRY_COUNT - retries} retries remaining...`)
+  while (retries++ < RETRY_COUNT) {
+    try {
+      const response = await fetch(url)
+      const command = await response.json()
 
-      await delay(INTERVAL)
-      return fetchCommand(url)
+      // keep polling until a scenario command is received
+      if (command.action !== 'noop') {
+        console.error(`[BugsnagPerformance] Received command from maze runner: ${JSON.stringify(command)}`)
+
+        return command
+      }
+    } catch (err) {
+      console.error(`[BugsnagPerformance] Error fetching command from maze runner: ${err.message}`, err)
     }
 
-    throw err
+    console.error(`[BugsnagPerformance] ${RETRY_COUNT - retries} retries remaining...`)
+
+    await delay(INTERVAL)
   }
+
+  throw new Error('Retry limit exceeded, giving up...')
 }
 
 const getCurrentCommand = async () => {
