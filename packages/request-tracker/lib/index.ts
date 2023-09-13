ba0af2b7bce9fe52384c4d78efc5939d3b8b42ea
Yousif Ahmed <yousif@bugsnag.com>
2023-09-13 14:24:42 +0100

    Add new `@bugsnag/request-tracker-performance` package

---
+++ packages/request-tracker/lib/index.ts
@@ -0,0 +1,5 @@
+import getAbsoluteUrl from './url-helpers'
+import createFetchRequestTracker from './request-tracker-fetch'
+import createXmlHttpRequestTracker from './request-tracker-xhr'
+
+export { getAbsoluteUrl, createFetchRequestTracker, createXmlHttpRequestTracker }
---
+++ packages/request-tracker/lib/request-tracker-fetch.ts
@@ -0,0 +1,41 @@
+import { type Clock } from '@bugsnag/core-performance'
+import { type RequestStartContext, RequestTracker } from './request-tracker'
+import getAbsoluteUrl from './url-helpers'
+
+interface WindowWithFetch {
+  fetch: typeof fetch
+  document: Document
+}
+
+function createStartContext (baseUrl: string, startTime: number, input: unknown, init?: unknown): RequestStartContext {
+  const inputIsRequest = isRequest(input)
+  const url = inputIsRequest ? input.url : String(input)
+  const method = (!!init && (init as RequestInit).method) || (inputIsRequest && input.method) || 'GET'
+  return { url: getAbsoluteUrl(url, baseUrl), method, startTime, type: 'fetch' }
+}
+
+function isRequest (input: unknown): input is Request {
+  return !!input && typeof input === 'object' && !(input instanceof URL)
+}
+
+function createFetchRequestTracker (window: WindowWithFetch, clock: Clock) {
+  const requestTracker = new RequestTracker()
+  const originalFetch = window.fetch
+
+  window.fetch = function fetch (input?: unknown, init?: unknown) {
+    const startContext = createStartContext(window.document.baseURI, clock.now(), input, init)
+    const onRequestEnd = requestTracker.start(startContext)
+
+    return originalFetch.call(this, input as RequestInfo, init as RequestInit).then(response => {
+      onRequestEnd({ status: response.status, endTime: clock.now(), state: 'success' })
+      return response
+    }).catch(error => {
+      onRequestEnd({ error, endTime: clock.now(), state: 'error' })
+      throw error
+    })
+  }
+
+  return requestTracker
+}
+
+export default createFetchRequestTracker
