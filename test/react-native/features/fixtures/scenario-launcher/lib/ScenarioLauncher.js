9b45b735b8a9e02a6515a1d10b4ec4a7f1c026d5
Joe Haines <hello@joehaines.co.uk>
2023-09-25 09:05:38 +0100

    Delete persisted data before launching scenarios

--- test/react-native/features/fixtures/scenario-launcher/lib/ScenarioLauncher.js
+++ test/react-native/features/fixtures/scenario-launcher/lib/ScenarioLauncher.js
@@ -3,8 +3,12 @@ import { getCurrentCommand } from './CommandRunner'
 import * as Scenarios from '../scenarios'
 import { REACT_APP_SCENARIO_NAME, REACT_APP_ENDPOINT, REACT_APP_API_KEY } from '@env'
 import BugsnagPerformance from '@bugsnag/react-native-performance'
+import { FileSystem, Dirs } from 'react-native-file-access'
 
 const isTurboModuleEnabled = () => global.__turboModuleProxy != null
+
+const PERSISTED_STATE_VERSION = 1
+const PERSISTED_STATE_PATH = `${Dirs.CacheDir}/bugsnag-performance-react-native/v${PERSISTED_STATE_VERSION}/persisted-state.json`
 
 export const launchScenario = async (rootTag) => {
   let command
@@ -13,6 +17,11 @@ export const launchScenario = async (rootTag) => {
     command = { scenario_name: REACT_APP_SCENARIO_NAME, api_key: REACT_APP_API_KEY, endpoint: REACT_APP_ENDPOINT }
   } else {
     command = await getCurrentCommand()
+  }
+
+  if (await FileSystem.exists(PERSISTED_STATE_PATH)) {
+    console.error(`[BugsnagPerformance] Clearing persisted data at path: ${PERSISTED_STATE_PATH}`)
+    await FileSystem.unlink(PERSISTED_STATE_PATH)
   }
 
   console.error(`[BugsnagPerformance] Launching scenario: ${command.scenario_name}`)
