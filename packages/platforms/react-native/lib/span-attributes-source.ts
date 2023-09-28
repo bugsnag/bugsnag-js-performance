1f767b872f5dfc95488fdec38397a125dc95a0d2
Ben Wilson <ben.wilson@smartbear.com>
2023-09-28 15:13:12 +0100

    experiment: implement connection listener in span attributes source

--- packages/platforms/react-native/lib/span-attributes-source.ts
+++ packages/platforms/react-native/lib/span-attributes-source.ts
@@ -1,14 +1,27 @@
 import { type SpanAttributesSource } from '@bugsnag/core-performance'
-import { type ReactNativeConfiguration } from './config'
+import NetInfo from '@react-native-community/netinfo'
 import { type AppStateStatic } from 'react-native'
+import { type ReactNativeConfiguration } from './config'
+
+// type State = 'unknown' | 'none' | 'cellular' | 'wifi' | 'bluetooth' | 'ethernet' | 'wimax' | 'vpn' | 'other'
 
 export function createSpanAttributesSource (appState: AppStateStatic) {
+  let connectionType = 'unknown' // TODO: do we need to set an initial value or will the event listener fire in the first instance?
+
+  // Subscribe to network changes
+  NetInfo.addEventListener(state => {
+    connectionType = state.type
+    console.log(`[BusgnagPerformance] connection state: ${JSON.stringify(state)}`)
+  })
+
   const spanAttributesSource: SpanAttributesSource<ReactNativeConfiguration> = {
     configure (configuration) {
 
     },
     requestAttributes (span) {
       span.setAttribute('bugsnag.app.in_foreground', appState.currentState === 'active')
+      span.setAttribute('net.host.connection.type', connectionType)
+      // span.setAttribute('net.host.connection.subtype', connectionType) // TODO: What exactly should this be!?
     }
   }
 
