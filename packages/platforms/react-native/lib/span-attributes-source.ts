a1cc6854c6de63d1c3c1599f7473935e683e3fbc
Ben Wilson <ben.wilson@smartbear.com>
2023-10-18 13:16:04 +0100

    feat: :sparkles: add network subtype attribute when available and network type is cellular

--- packages/platforms/react-native/lib/span-attributes-source.ts
+++ packages/platforms/react-native/lib/span-attributes-source.ts
@@ -2,14 +2,17 @@ import { type SpanAttributesSource } from '@bugsnag/core-performance'
 import NetInfo from '@react-native-community/netinfo'
 import { type AppStateStatic } from 'react-native'
 import { type ReactNativeConfiguration } from './config'
-import getNetworkConnectionType, { type NetworkConnectionType } from './get-network-connection-type'
+import getNetworkConnectionType, { type CellularGeneration, type NetworkConnectionType } from './get-network-connection-type'
 
 export function createSpanAttributesSource (appState: AppStateStatic) {
   let connectionType: NetworkConnectionType = 'unknown'
+  let cellularGeneration: CellularGeneration
 
   // Subscribe to network changes
   NetInfo.addEventListener(state => {
     connectionType = getNetworkConnectionType(state.type)
+    // @ts-expect-error cellularGeneration does not exist on type
+    cellularGeneration = state.details?.cellularGeneration
   })
 
   const spanAttributesSource: SpanAttributesSource<ReactNativeConfiguration> = {
@@ -19,6 +22,10 @@ export function createSpanAttributesSource (appState: AppStateStatic) {
     requestAttributes (span) {
       span.setAttribute('bugsnag.app.in_foreground', appState.currentState === 'active')
       span.setAttribute('net.host.connection.type', connectionType)
+
+      if (cellularGeneration && connectionType === 'cell') {
+        span.setAttribute('net.host.connection.subtype', cellularGeneration)
+      }
     }
   }
 
