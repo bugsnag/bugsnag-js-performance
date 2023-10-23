78b714d8bf090d08284a38ede595e1d1ad7031b7
Joe Haines <hello@joehaines.co.uk>
2023-10-23 11:20:54 +0100

    Flush the retry queue when the app state changes

--- packages/platforms/react-native/tests/retry-queue/index.test.ts
+++ packages/platforms/react-native/tests/retry-queue/index.test.ts
@@ -2,6 +2,7 @@ import createRetryQueueFactory from '../../lib/retry-queue'
 import FileBasedRetryQueue from '../../lib/retry-queue/file-based'
 import FileSystemFake from '../utilities/file-system-fake'
 import { InMemoryDelivery, makePayloadCreator } from '@bugsnag/js-performance-test-utilities'
+import { AppState } from 'react-native'
 
 const EXPECTED_PATH = '/mock/CacheDir/bugsnag-performance-react-native/v1/retry-queue'
 const flushPromises = () => new Promise(process.nextTick)
@@ -31,6 +32,30 @@ describe('File based retry queue factory', () => {
     expect(JSON.parse(file)).toStrictEqual(payload)
 
     await queue.flush()
+
+    expect(delivery.requests[0]).toStrictEqual(payload.body)
+    expect(delivery.requests).toHaveLength(1)
+  })
+
+  it.each(
+    ['active', 'inactive', 'background']
+  )('flushes the retry queue when the app state status changes to "%s"', async (status) => {
+    const fileSystem = new FileSystemFake()
+    const retryQueueFactory = createRetryQueueFactory(fileSystem)
+
+    const delivery = new InMemoryDelivery()
+    const queue = retryQueueFactory(delivery, 20)
+
+    const validEndTime = BigInt(Date.now()) * BigInt(1_000_000)
+    const payload = createPayload({ spanId: 'abcd', endTimeUnixNano: validEndTime.toString() })
+    await queue.add(payload, 0)
+
+    expect(delivery.requests).toHaveLength(0)
+
+    // @ts-expect-error we add this in our RN mock
+    AppState.bugsnagChangeAppStateStatus(status)
+
+    await flushPromises()
 
     expect(delivery.requests[0]).toStrictEqual(payload.body)
     expect(delivery.requests).toHaveLength(1)
