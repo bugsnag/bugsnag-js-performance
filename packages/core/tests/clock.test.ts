acb8c2821cc27b32251297b0380240e5418ecf55
Yousif Ahmed <yousif@bugsnag.com>
2023-10-26 10:35:11 +0100

    Ensure timestamps are rounded to the nearest integer

---
+++ packages/core/tests/clock.test.ts
@@ -0,0 +1,8 @@
+import { millisecondsToNanoseconds } from '../lib/clock'
+
+describe('millisecondsToNanoseconds', () => {
+  it('rounds to the nearest integer', () => {
+    expect(millisecondsToNanoseconds(123456789.1122345)).toEqual(123456789_112235)
+    expect(millisecondsToNanoseconds(123456789.1122344)).toEqual(123456789_112234)
+  })
+})
