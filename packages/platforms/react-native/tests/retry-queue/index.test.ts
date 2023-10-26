05ad14166a68b90ed40959edb1ded37e22706115
Joe Haines <hello@joehaines.co.uk>
2023-10-26 10:14:28 +0100

    Flush the retry queue on new network connectivity

--- packages/platforms/react-native/tests/retry-queue/index.test.ts
+++ packages/platforms/react-native/tests/retry-queue/index.test.ts
@@ -4,6 +4,15 @@ import FileSystemFake from '../utilities/file-system-fake'
 import { InMemoryDelivery, makePayloadCreator } from '@bugsnag/js-performance-test-utilities'
 import { AppState } from 'react-native'
 
+// eslint-disable-next-line jest/no-mocks-import
+import {
+  NetInfoStateType,
+  notifyNetworkStateChange,
+  resetEventListeners
+} from '../../__mocks__/@react-native-community/netinfo'
+
+afterEach(resetEventListeners)
+
 const EXPECTED_PATH = '/mock/CacheDir/bugsnag-performance-react-native/v1/retry-queue'
 const flushPromises = () => new Promise(process.nextTick)
 const createPayload = makePayloadCreator()
@@ -75,6 +84,55 @@ describe('File based retry queue factory', () => {
 
     // @ts-expect-error we add this in our RN mock
     AppState.bugsnagChangeAppStateStatus(status)
+
+    await flushPromises()
+
+    expect(delivery.requests[0]).toStrictEqual(payload.body)
+    expect(delivery.requests).toHaveLength(1)
+  })
+
+  it('flushes the retry queue when the app gains network connectivity', async () => {
+    const fileSystem = new FileSystemFake()
+    const retryQueueFactory = createRetryQueueFactory(fileSystem)
+
+    const delivery = new InMemoryDelivery()
+    const queue = retryQueueFactory(delivery, 20)
+
+    const validEndTime = BigInt(Date.now()) * BigInt(1_000_000)
+    const payload = createPayload({ spanId: 'abcd', endTimeUnixNano: validEndTime.toString() })
+    await queue.add(payload, 0)
+
+    expect(delivery.requests).toHaveLength(0)
+
+    notifyNetworkStateChange({
+      isConnected: false,
+      isInternetReachable: false,
+      type: NetInfoStateType.none,
+      details: null
+    })
+
+    // we just disconnected so there should be no requests
+    await flushPromises()
+    expect(delivery.requests).toHaveLength(0)
+
+    notifyNetworkStateChange({
+      isConnected: false,
+      isInternetReachable: false,
+      type: NetInfoStateType.none,
+      details: null
+    })
+
+    // another disconnection should still not send a request
+    await flushPromises()
+    expect(delivery.requests).toHaveLength(0)
+
+    // connecting should result in a flush
+    notifyNetworkStateChange({
+      isConnected: true,
+      isInternetReachable: true,
+      type: NetInfoStateType.ethernet,
+      details: { ipAddress: '1234', subnet: '5678', isConnectionExpensive: false }
+    })
 
     await flushPromises()
 
