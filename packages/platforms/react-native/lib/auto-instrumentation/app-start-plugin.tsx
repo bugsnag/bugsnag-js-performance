4b7eef16e2a23c6daa8ab1596b5e6b2d5490d12a
Yousif Ahmed <yousif@bugsnag.com>
2023-11-02 13:00:49 +0000

    Add diagnostic sub-spans to AppStartPlugin

--- packages/platforms/react-native/lib/auto-instrumentation/app-start-plugin.tsx
+++ packages/platforms/react-native/lib/auto-instrumentation/app-start-plugin.tsx
@@ -5,15 +5,35 @@ import {
   type Clock
 } from '@bugsnag/core-performance'
 import { type ReactNativeConfiguration } from '../config'
-import { type ReactNode, useEffect } from 'react'
+import React, { type ReactNode, type PropsWithChildren, useEffect, useState, useRef } from 'react'
 import { type WrapperComponentProvider, type AppRegistry } from 'react-native'
 
-type WrapperProps = {
-  children: ReactNode
-}
+type DiagnosticWrapperProps = PropsWithChildren<{
+  spanFactory: SpanFactory<ReactNativeConfiguration>,
+  clock: Clock
+}>
 
 export const isWrapperComponentProvider = (value: unknown): value is WrapperComponentProvider | null => 
   value === null || typeof value === 'function'
+
+class DiagnosticWrapper extends React.Component<DiagnosticWrapperProps> {
+  private readonly span: ReturnType<SpanFactory<ReactNativeConfiguration>['startSpan']>
+
+  constructor (props: DiagnosticWrapperProps) {
+    super(props)
+    this.span = props.spanFactory.startSpan('DiagnosticWrapper (CC) rendered', { startTime: props.clock.now() })
+  }
+
+  componentDidMount(): void {
+    if (this.span) {
+      this.props.spanFactory.endSpan(this.span, this.props.clock.now())
+    }
+  }
+
+  render () {
+    return this.props.children
+  }
+}
 
 export class AppStartPlugin implements Plugin<ReactNativeConfiguration> {
   private readonly appStartTime: number
@@ -40,28 +60,49 @@ export class AppStartPlugin implements Plugin<ReactNativeConfiguration> {
     appStartSpan.setAttribute('bugsnag.span.category', 'app_start')
     appStartSpan.setAttribute('bugsnag.app_start.type', 'ReactNativeInit')
 
-    const AppStartWrapper = ({ children }: WrapperProps) => {
+    const clientStartedSpan = this.spanFactory.startSpan('BugSnag client started', { startTime: this.clock.now(), parentContext: appStartSpan })
+
+    const AppStartWrapper = ({ children }: PropsWithChildren) => {
+      const [appStarted, setAppStarted] = useState(false)
+      const wrapperComponentSpan = useRef<any>(null)
+      
+      if (!appStarted) {
+        wrapperComponentSpan.current = this.spanFactory.startSpan('AppStartWrapper (FC) rendered', { startTime: this.clock.now(), parentContext: appStartSpan })
+      }
+
       useEffect(() => {
+        setAppStarted(true)
+        this.spanFactory.endSpan(wrapperComponentSpan.current, this.clock.now())
         this.spanFactory.endSpan(appStartSpan, this.clock.now())
       }, [])
 
       return <>{children}</>
     }
 
+    const diagnosticWrapperProps = {
+      spanFactory: this.spanFactory,
+      clock: this.clock
+    }
+
     const instrumentedComponentProvider: WrapperComponentProvider = (appParams) => ({ children }) => {
       if (configuration.wrapperComponentProvider) {
-        const WrapperComponent = configuration.wrapperComponentProvider(appParams)
+        const OriginalWrapper = configuration.wrapperComponentProvider(appParams)
 
         return (
           <AppStartWrapper>
-            <WrapperComponent>{children}</WrapperComponent>
+            <DiagnosticWrapper {...diagnosticWrapperProps}>
+              <OriginalWrapper>{children}</OriginalWrapper>
+            </DiagnosticWrapper>  
           </AppStartWrapper>
         )
       }
 
-      return <AppStartWrapper>{children}</AppStartWrapper>
+      return <AppStartWrapper>
+              <DiagnosticWrapper {...diagnosticWrapperProps}>{children}</DiagnosticWrapper>
+            </AppStartWrapper>
     }
 
+    this.spanFactory.endSpan(clientStartedSpan, this.clock.now())
     this.appRegistry.setWrapperComponentProvider(instrumentedComponentProvider)
   }
 }
