4f812dc89ae6d0384f390690b697c836021cffa5
Yousif Ahmed <yousif@bugsnag.com>
2023-11-23 12:54:47 +0000

    Add basic unit test for setWrapperComponentProvider monkey patch

--- packages/platforms/react-native/tests/auto-instrumentation/app-start-plugin.test.ts
+++ packages/platforms/react-native/tests/auto-instrumentation/app-start-plugin.test.ts
@@ -4,6 +4,7 @@ import createClock from '../../lib/clock'
 import { AppStartPlugin } from '../../lib/auto-instrumentation/app-start-plugin'
 import { type ReactNativeConfiguration } from '../../lib/config'
 import { type AppRegistry } from 'react-native'
+import { type PropsWithChildren } from 'react'
 
 describe('app start plugin', () => {
   let spanFactory: MockSpanFactory<ReactNativeConfiguration>
@@ -42,5 +43,37 @@ describe('app start plugin', () => {
 
     expect(spanFactory.startSpan).not.toHaveBeenCalled()
     expect(originalSetWrapperComponentProvider).not.toHaveBeenCalled()
+  })
+
+  it('monkey patches setWrapperComponentProvider', () => {
+    const appStartTime = 1234
+    const plugin = new AppStartPlugin(appStartTime, spanFactory, clock, appRegistry)
+
+    const mockSetWrapperComponentProvider = appRegistry.setWrapperComponentProvider as jest.MockedFunction<typeof appRegistry.setWrapperComponentProvider>
+
+    plugin.configure(createConfiguration<ReactNativeConfiguration>({ autoInstrumentAppStarts: true }))
+
+    expect(spanFactory.startSpan).toHaveBeenCalledWith('[AppStart/ReactNativeInit]',
+      expect.objectContaining({
+        startTime: appStartTime,
+        parentContext: null
+      }))
+
+    expect(appRegistry.setWrapperComponentProvider).not.toBe(mockSetWrapperComponentProvider)
+
+    // mock function should have been called prior to monkey-patching
+    expect(mockSetWrapperComponentProvider).toHaveBeenCalledTimes(1)
+    expect(mockSetWrapperComponentProvider).toHaveBeenCalledWith(expect.any(Function))
+
+    // call the monkey-patched setWrapperComponentProvider
+    const mockProvider = jest.fn((appParams) => ({ children }: PropsWithChildren) => children)
+    appRegistry.setWrapperComponentProvider(mockProvider)
+
+    // a diagnostic span should have been started
+    expect(spanFactory.startSpan).toHaveBeenCalledWith('AppRegistry.setWrapperComponentProvider called',
+      expect.objectContaining({ startTime: expect.any(Number) }))
+
+    // the original setWrapperComponentProvider should have been called
+    expect(mockSetWrapperComponentProvider).toHaveBeenCalledTimes(2)
   })
 })
