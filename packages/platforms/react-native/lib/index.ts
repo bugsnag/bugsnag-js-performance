03d9aaafa3343ba2e7f793ee99c2ade0cbd608c1
Ben Wilson <ben.wilson@smartbear.com>
2023-12-07 17:56:23 +0000

    feat: :label: export config and platform extension types from react-native

--- packages/platforms/react-native/lib/index.ts
+++ packages/platforms/react-native/lib/index.ts
@@ -1,3 +1,6 @@
 import BugsnagPerformance from './client'
 
+export type { ReactNativeConfiguration } from './config'
+export type { PlatformExtensions } from './platform-extensions'
+
 export default BugsnagPerformance
