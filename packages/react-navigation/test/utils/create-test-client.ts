4564bb9608b3f3cc375ae33e36a97bbd56003e70
Ben Wilson <ben.wilson@smartbear.com>
2024-01-09 16:04:37 +0000

    test: :white_check_mark: update test utils

--- packages/react-navigation/test/utils/create-test-client.ts
+++
@@ -1,9 +0,0 @@
-import { type Span } from '@bugsnag/core-performance'
-
-export const createTestClient = (testSpan: Span) => ({
-  start: jest.fn(),
-  startSpan: jest.fn(),
-  startNavigationSpan: jest.fn(() => testSpan),
-  currentSpanContext: testSpan,
-  getPlugin: jest.fn()
-})
--- packages/react-navigation/test/utils/create-test-span.ts
+++
@@ -1,6 +0,0 @@
-export const createTestSpan = () => ({
-  id: 'test-id',
-  traceId: 'test-trace-id',
-  isValid: () => true,
-  end: jest.fn()
-})
