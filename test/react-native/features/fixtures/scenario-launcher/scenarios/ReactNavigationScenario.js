ebf35608ca712c880012262f7bc3911f929db983
Benedict Wilson <ben.wilson@smartbear.com>
2024-01-31 17:35:36 +0000

    [PLAT-11474] React navigation end to end tests (#395)
    * test: :test_tube: create end to end test fixture for react-navigation/native
    
    * test: :white_check_mark: navigate to details page on load
    
    * fix: :bug: fix createNavigationContainer return type
    
    * test: :white_check_mark: add assertions for new navigation attributes
    
    * test: :white_check_mark: skip react-navigation tests on new architecture
    
    * replace MainActivity file with predefined one for react/navigation support
    
    * install react navigation dependencies on old architecture

---
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/ReactNavigationScenario.js
@@ -0,0 +1,67 @@
+import BugsnagPerformance from '@bugsnag/react-native-performance'
+import { ReactNavigationNativePlugin } from '@bugsnag/react-navigation-performance'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React, { useEffect } from 'react'
+import { SafeAreaView, Text, Button } from 'react-native'
+
+export const config = {
+    maximumBatchSize: 1,
+    autoInstrumentAppStarts: false,
+    appVersion: '1.2.3',
+    plugins: [new ReactNavigationNativePlugin()]
+}
+
+const Stack = createNativeStackNavigator()
+
+export function App() {
+
+    // These methods must be called after Bugsnag has been started
+    const plugin = BugsnagPerformance.getPlugin(ReactNavigationNativePlugin)
+    const BugsnagNavigationContainer = plugin.createNavigationContainer()
+
+    return (
+        <BugsnagNavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name='Home' component={HomeScreen} />
+                <Stack.Screen name='Loading' component={LoadingScreen} />
+                <Stack.Screen name='Details' component={DetailsScreen} />
+            </Stack.Navigator>
+        </BugsnagNavigationContainer>
+    );
+}
+
+function HomeScreen({ navigation }) {
+    useEffect(() => {
+        navigation.navigate('Details')
+    }, [])
+
+    return (
+        <SafeAreaView>
+            <Text>HomeScreen</Text>
+            <Button title='Go to details screen' onPress={() => { navigation.navigate('Details') }} />
+            <Button title='Go to loading screen' onPress={() => { navigation.navigate('Loading') }} />
+        </SafeAreaView>
+    )
+}
+
+function LoadingScreen({ navigation }) {
+    return (
+        <SafeAreaView>
+            <Text>LoadingScreen</Text>
+            <Button title='Go back' onPress={() => { navigation.goBack() }} />
+            <Button title='Go to home screen' onPress={() => { navigation.navigate('Home') }} />
+            <Button title='Go to details screen' onPress={() => { navigation.navigate('Details') }} />
+        </SafeAreaView>
+    )
+}
+
+function DetailsScreen({ navigation }) {
+    return (
+        <SafeAreaView>
+            <Text>DetailsScreen</Text>
+            <Button title='Go back' onPress={() => { navigation.goBack() }} />
+            <Button title='Go to home screen' onPress={() => { navigation.navigate('Home') }} />
+            <Button title='Go to loading screen' onPress={() => { navigation.navigate('Loading') }} />
+        </SafeAreaView>
+    )
+}
--- test/react-native/features/react-navigation.feature
+++ test/react-native/features/react-navigation.feature
@@ -1,7 +1,8 @@
-Feature: Navigation spans
+@skip_new_arch
+Feature: Navigation spans with React Navigation
 
-  Scenario: Manual Navigation Spans can be logged
-    When I run 'NavigationSpanScenario'
+  Scenario: Navigation Spans are automatically instrumented
+    When I run 'ReactNavigationScenario'
     And I wait to receive a sampling request
     And I wait for 1 span
 
@@ -9,12 +10,14 @@ Feature: Navigation spans
     Then the sampling request "Bugsnag-Span-Sampling" header equals "1.0:0"
 
     And the trace "Bugsnag-Span-Sampling" header equals "1:1"
-    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.name" equals "[Navigation]NavigationSpanScenario"
+    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.name" equals "[Navigation]Details"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.kind" equals 3
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.spanId" matches the regex "^[A-Fa-f0-9]{16}$"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.traceId" matches the regex "^[A-Fa-f0-9]{32}$"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.startTimeUnixNano" matches the regex "^[0-9]+$"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0.endTimeUnixNano" matches the regex "^[0-9]+$"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" string attribute "bugsnag.span.category" equals "navigation"
-    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" string attribute "bugsnag.navigation.route" equals "NavigationSpanScenario"
+    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" string attribute "bugsnag.navigation.route" equals "Details"
+    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" string attribute "bugsnag.navigation.triggered_by" equals "@bugsnag/react-navigation-performance"
+    And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" string attribute "bugsnag.navigation.ended_by" equals "immediate"
     And the trace payload field "resourceSpans.0.scopeSpans.0.spans.0" bool attribute "bugsnag.app.in_foreground" is true
