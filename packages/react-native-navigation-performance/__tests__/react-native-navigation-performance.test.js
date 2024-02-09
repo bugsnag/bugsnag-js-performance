99f5effdf7a9573829503a0266b0810d0cd7e7fc
Ben Wilson <ben.wilson@smartbear.com>
2024-02-09 16:54:13 +0000

    feat: :sparkles: generate new package

---
+++ packages/react-native-navigation-performance/__tests__/react-native-navigation-performance.test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const reactNativeNavigationPerformance = require('..')
+const assert = require('assert').strict
+
+assert.strictEqual(reactNativeNavigationPerformance(), 'Hello from reactNativeNavigationPerformance')
+console.info('reactNativeNavigationPerformance tests passed')
