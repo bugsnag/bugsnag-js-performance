149b945d4b9c1cae7be1da07c760b037503560eb
Ben Wilson <ben.wilson@smartbear.com>
2024-02-13 10:10:26 +0000

    add Navigation parameter to constuctor

--- packages/react-native-navigation/__tests__/react-native-navigation-plugin.test.ts
+++ packages/react-native-navigation/__tests__/react-native-navigation-plugin.test.ts
@@ -1,13 +1,14 @@
 import { MockSpanFactory, createConfiguration } from '@bugsnag/js-performance-test-utilities'
 import { type ReactNativeConfiguration } from '@bugsnag/react-native-performance'
 import ReactNativeNavigationPlugin from '../lib/react-native-navigation-plugin'
+import { Navigation } from 'react-native-navigation'
 
 describe('ReactNativeNavigationPlugin', () => {
-  it('has a configure method', () => {
+  it('is configurable', () => {
+    const plugin = new ReactNativeNavigationPlugin(Navigation)
+    const configuration = createConfiguration<ReactNativeConfiguration>()
+    const spanFactory = new MockSpanFactory()
     expect(() => {
-      const plugin = new ReactNativeNavigationPlugin()
-      const configuration = createConfiguration<ReactNativeConfiguration>()
-      const spanFactory = new MockSpanFactory()
       plugin.configure(configuration, spanFactory)
     }).not.toThrow()
   })
--- packages/react-native-navigation/lib/react-native-navigation-plugin.ts
+++ packages/react-native-navigation/lib/react-native-navigation-plugin.ts
@@ -1,9 +1,12 @@
 import { type InternalConfiguration, type Plugin, type SpanFactory } from '@bugsnag/core-performance'
 import { type ReactNativeConfiguration } from '@bugsnag/react-native-performance'
+import { type NavigationDelegate } from 'react-native-navigation/lib/dist/src/NavigationDelegate'
 
 class ReactNativeNavigationPlugin implements Plugin<ReactNativeConfiguration> {
-  configure (configuration: InternalConfiguration<ReactNativeConfiguration>, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+  constructor (private Navigation: NavigationDelegate) {}
 
+  configure (configuration: InternalConfiguration<ReactNativeConfiguration>, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+    // TODO: Set up navigation listeners
   }
 }
 
