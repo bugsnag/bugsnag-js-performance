5b2ec85d73db17a7d65e8fee960ac55487a6229d
Ben Wilson <ben.wilson@smartbear.com>
2024-02-15 16:36:03 +0000

    fix whitespace issues

--- test/react-native/features/fixtures/app/android/MainActivity.0.72.java
+++ test/react-native/features/fixtures/app/android/MainActivity.0.72.java
@@ -34,9 +34,10 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+      this,
+      getMainComponentName(),
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      DefaultNewArchitectureEntryPoint.getFabricEnabled()
+    );
   }
 }
