e52c52dbaa9de1e89f557b06bae2776e51932d1b
Ben Wilson <ben.wilson@smartbear.com>
2024-02-15 16:35:41 +0000

    create dedicated scenario launcher script

---
+++ test/react-native/features/fixtures/scenario-launcher/lib/ReactNativeNavigationScenarioLauncher.js
@@ -0,0 +1,88 @@
+import BugsnagPerformance from '@bugsnag/react-native-performance'
+import { REACT_APP_API_KEY, REACT_APP_ENDPOINT, REACT_APP_SCENARIO_NAME } from '@env'
+import { Navigation } from 'react-native-navigation'
+import * as Scenarios from '../scenarios'
+import { getCurrentCommand } from './CommandRunner'
+import { clearPersistedState, setDeviceId, setSamplingProbability } from './Persistence'
+
+// const isTurboModuleEnabled = () => global.__turboModuleProxy != null
+
+async function runScenario (rootTag, scenarioName, apiKey, endpoint) {
+  console.error(`[BugsnagPerformance] Launching scenario: ${scenarioName}`)
+  const scenario = Scenarios[scenarioName]
+
+  BugsnagPerformance.start({
+    apiKey,
+    endpoint,
+    networkRequestCallback (requestInfo) {
+      // ignore requests to the maze runner command endpoint
+      if (requestInfo.url.endsWith('/command')) {
+        console.error(`[BugsnagPerformance] ignoring request to ${requestInfo.url}`)
+        return null
+      }
+
+      return requestInfo
+    },
+    ...scenario.config
+  })
+
+  Navigation.setRoot(scenario.getRoot())
+}
+
+export async function launchScenario (rootTag, clearPersistedData = true) {
+  if (clearPersistedData) {
+    await clearPersistedState()
+  }
+
+  let command
+
+  if (REACT_APP_SCENARIO_NAME && REACT_APP_API_KEY) {
+    command = {
+      action: 'run-scenario',
+      scenario_name: REACT_APP_SCENARIO_NAME,
+      api_key: REACT_APP_API_KEY,
+      endpoint: REACT_APP_ENDPOINT
+    }
+  } else {
+    command = await getCurrentCommand()
+  }
+
+  switch (command.action) {
+    case 'run-scenario':
+      return await runScenario(
+        rootTag,
+        command.scenario_name,
+        command.api_key,
+        command.endpoint
+      )
+
+    case 'set-sampling-probability-to-0':
+      await setSamplingProbability(0)
+
+      return await launchScenario(rootTag, false)
+
+    case 'set-device-id':
+      await setDeviceId('c1234567890abcdefghijklmnop')
+
+      return await launchScenario(rootTag, false)
+
+    case 'set-invalid-sampling-probability':
+      await setSamplingProbability('this is not a valid sampling probability')
+
+      return await launchScenario(rootTag, false)
+
+    case 'set-expired-sampling-probability':
+      // Friday February 13th 2009 23:31:30
+      await setSamplingProbability(0, 1234567890)
+
+      return await launchScenario(rootTag, false)
+
+    case 'set-invalid-device-id':
+      await setDeviceId('this is not a valid device ID')
+
+      return await launchScenario(rootTag, false)
+
+    default:
+      throw new Error(`Unknown action '${command.action}'`)
+  }
+}
