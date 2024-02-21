a2e12e32ab0d41a534910422e8f16c0bbfdb414f
Ben Wilson <ben.wilson@smartbear.com>
2024-02-21 16:36:41 +0000

    feat: :sparkles: add event listeners to instrument navigation spans when changing route

--- packages/react-native-navigation/lib/react-native-navigation-plugin.ts
+++ packages/react-native-navigation/lib/react-native-navigation-plugin.ts
@@ -1,12 +1,40 @@
-import { type InternalConfiguration, type Plugin, type SpanFactory } from '@bugsnag/core-performance'
+import { type InternalConfiguration, type Plugin, type SpanFactory, type SpanInternal } from '@bugsnag/core-performance'
 import { type ReactNativeConfiguration } from '@bugsnag/react-native-performance'
 import { type NavigationDelegate } from 'react-native-navigation/lib/dist/src/NavigationDelegate'
 
 class ReactNativeNavigationPlugin implements Plugin<ReactNativeConfiguration> {
+  navigationSpan?: SpanInternal
+  startTime?: number
+
   constructor (private Navigation: NavigationDelegate) {}
 
+  private clearActiveSpan () {
+    this.startTime = undefined
+    this.navigationSpan = undefined
+  }
+
   configure (configuration: InternalConfiguration<ReactNativeConfiguration>, spanFactory: SpanFactory<ReactNativeConfiguration>) {
-    // TODO: Set up navigation listeners
+    this.Navigation.events().registerCommandListener((name, params) => {
+      this.startTime = performance.now()
+    })
+
+    this.Navigation.events().registerComponentDidAppearListener(event => {
+      const routeName = event.componentName
+
+      if (this.startTime) {
+        this.navigationSpan = spanFactory.startSpan('[Navigation]' + routeName, {
+          startTime: this.startTime
+        })
+      }
+    })
+
+    this.Navigation.events().registerCommandCompletedListener(event => {
+      const endTime = event.completionTime
+      if (this.navigationSpan) {
+        spanFactory.endSpan(this.navigationSpan, endTime)
+        this.clearActiveSpan()
+      }
+    })
   }
 }
 
