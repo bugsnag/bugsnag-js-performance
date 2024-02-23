92c4347086204913ba41ca4611fdfa45aaa4816d
Ben Wilson <ben.wilson@smartbear.com>
2024-02-23 12:31:45 +0000

    update navigation handling

--- packages/react-native-navigation/lib/react-native-navigation-plugin.ts
+++ packages/react-native-navigation/lib/react-native-navigation-plugin.ts
@@ -3,36 +3,52 @@ import { type ReactNativeConfiguration } from '@bugsnag/react-native-performance
 import { type NavigationDelegate } from 'react-native-navigation/lib/dist/src/NavigationDelegate'
 
 class ReactNativeNavigationPlugin implements Plugin<ReactNativeConfiguration> {
-  private navigationSpan?: SpanInternal
   private startTime?: number
+  private startTimeout?: NodeJS.Timeout
+  private endTimeout?: NodeJS.Timeout
+  private navigationSpan?: SpanInternal
 
   constructor (private Navigation: NavigationDelegate) {}
 
   private clearActiveSpan () {
+    clearTimeout(this.startTimeout)
+    clearTimeout(this.endTimeout)
     this.startTime = undefined
     this.navigationSpan = undefined
   }
 
   configure (configuration: InternalConfiguration<ReactNativeConfiguration>, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+    const triggerNavigationEnd = () => {
+      const endTime = performance.now()
+      this.endTimeout = setTimeout(() => {
+        if (this.navigationSpan) {
+          spanFactory.endSpan(this.navigationSpan, endTime)
+          this.clearActiveSpan()
+        }
+      }, 100)
+    }
+
+    // Potential for a navigation to occur
     this.Navigation.events().registerCommandListener((name, params) => {
+      if (this.startTimeout) {
+        clearTimeout(this.startTimeout)
+      }
+
       this.startTime = performance.now()
+      this.startTimeout = setTimeout(() => {
+        this.clearActiveSpan()
+      }, 1000)
     })
 
+    // Navigation has occurred
     this.Navigation.events().registerComponentDidAppearListener(event => {
-      const routeName = event.componentName
-
       if (this.startTime) {
+        const routeName = event.componentName
         this.navigationSpan = spanFactory.startSpan('[Navigation]' + routeName, {
           startTime: this.startTime
         })
-      }
-    })
 
-    this.Navigation.events().registerCommandCompletedListener(event => {
-      const endTime = event.completionTime
-      if (this.navigationSpan) {
-        spanFactory.endSpan(this.navigationSpan, endTime)
-        this.clearActiveSpan()
+        triggerNavigationEnd()
       }
     })
   }
