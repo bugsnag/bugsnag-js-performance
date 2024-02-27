4a804583d6bc185b2b4020ebfed71cc8143569e9
Ben Wilson <ben.wilson@smartbear.com>
2024-02-27 10:55:34 +0000

    perf: :zap: implement getter pattern for plugin reference

--- packages/react-native-navigation/lib/CompleteNavigation.tsx
+++ packages/react-native-navigation/lib/CompleteNavigation.tsx
@@ -6,34 +6,47 @@ interface Props extends PropsWithChildren {
   on: 'mount' | 'unmount' | boolean
 }
 
-// @ts-expect-error throwing error for now
-const getPlugin = () => BugsnagPerformance.getPlugin(ReactNativeNavigationPlugin)
-
 /** End the current navigation span when the component is mounted, unmounted or the `on` prop is `true` */
 export class CompleteNavigation extends React.Component<Props> {
+  private _plugin?: ReactNativeNavigationPlugin
+
+  private get plugin () {
+    if (this._plugin) return this._plugin
+
+    // @ts-expect-error signature does not match
+    this._plugin = BugsnagPerformance.getPlugin(ReactNativeNavigationPlugin)
+
+    return this._plugin
+  }
+
   constructor (props: Props) {
     super(props)
-    getPlugin()?.blockNavigationEnd()
+
+    if (this.plugin) {
+      this.plugin.blockNavigationEnd()
+    }
   }
 
   componentDidMount () {
     if (this.props.on === 'mount') {
       setTimeout(() => {
-        getPlugin()?.unblockNavigationEnd('mount')
+        if (this.plugin) {
+          this.plugin.unblockNavigationEnd('mount')
+        }
       })
     }
   }
 
   componentWillUnmount () {
-    if (this.props.on === 'unmount') {
-      getPlugin()?.unblockNavigationEnd('unmount')
+    if (this.props.on === 'unmount' && this.plugin) {
+      this.plugin.unblockNavigationEnd('unmount')
     }
   }
 
   componentDidUpdate (prevProps: Readonly<Props>) {
     if (typeof this.props.on === 'boolean') {
-      if (this.props.on && !prevProps.on) {
-        getPlugin()?.unblockNavigationEnd('condition')
+      if (this.props.on && !prevProps.on && this.plugin) {
+        this.plugin.unblockNavigationEnd('condition')
       }
     }
   }
