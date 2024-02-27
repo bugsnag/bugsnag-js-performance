f925b4db3493a18294993bed862d325514338277
Ben Wilson <ben.wilson@smartbear.com>
2024-02-27 10:48:03 +0000

    refactor: :recycle: update methods for react-native-navigation plugin

--- packages/react-native-navigation/lib/react-native-navigation-plugin.ts
+++ packages/react-native-navigation/lib/react-native-navigation-plugin.ts
@@ -1,58 +1,92 @@
-import { type InternalConfiguration, type Plugin, type SpanFactory, type SpanInternal } from '@bugsnag/core-performance'
-import { type ReactNativeConfiguration } from '@bugsnag/react-native-performance'
-import { type NavigationDelegate } from 'react-native-navigation/lib/dist/src/NavigationDelegate'
+import type { Plugin, SpanFactory, SpanInternal } from '@bugsnag/core-performance'
+import type { ReactNativeConfiguration } from '@bugsnag/react-native-performance'
+import type { NavigationDelegate } from 'react-native-navigation/lib/dist/src/NavigationDelegate'
+
+type Reason = 'immediate' | 'mount' | 'unmount' | 'condition'
+
+const NAVIGATION_START_TIMEOUT = 1000
+const NAVIGATION_COMPLETE_TIMEOUT = 100
 
 class ReactNativeNavigationPlugin implements Plugin<ReactNativeConfiguration> {
+  private Navigation: NavigationDelegate
+  private currentNavigationSpan?: SpanInternal
   private startTime?: number
   private startTimeout?: NodeJS.Timeout
   private endTimeout?: NodeJS.Timeout
-  private navigationSpan?: SpanInternal
+  private componentsWaiting = 0
+  private spanFactory?: SpanFactory<ReactNativeConfiguration>
 
-  constructor (private Navigation: NavigationDelegate) {}
+  constructor (Navigation: NavigationDelegate) {
+    this.Navigation = Navigation
+  }
 
   private clearActiveSpan () {
     clearTimeout(this.startTimeout)
     clearTimeout(this.endTimeout)
     this.startTime = undefined
-    this.navigationSpan = undefined
+    this.currentNavigationSpan = undefined
   }
 
   /** Trigger the end of the current navigation span after 100ms */
-  private triggerNavigationEnd = (spanFactory: SpanFactory<ReactNativeConfiguration>) => {
-    const endTime = performance.now()
+  private triggerNavigationEnd = (spanFactory: SpanFactory<ReactNativeConfiguration>, endedBy: Reason, endTime = performance.now()) => {
+    clearTimeout(this.endTimeout)
+
     this.endTimeout = setTimeout(() => {
-      if (this.navigationSpan) {
-        spanFactory.endSpan(this.navigationSpan, endTime)
+      if (this.componentsWaiting === 0 && this.currentNavigationSpan) {
+        this.currentNavigationSpan.setAttribute('bugsnag.navigation.ended_by', endedBy)
+        spanFactory.endSpan(this.currentNavigationSpan, endTime)
         this.clearActiveSpan()
       }
-    }, 100)
+    }, NAVIGATION_COMPLETE_TIMEOUT)
   }
 
-  configure (configuration: InternalConfiguration<ReactNativeConfiguration>, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+  /**
+   * Blocks the current navigation by incrementing the count of components waiting
+   */
+  blockNavigationEnd = () => {
+    this.componentsWaiting += 1
+  }
+
+  /**
+   * Unblocks the current navigation by decrementing the count of components waiting and setting the reason
+   */
+  unblockNavigationEnd = (endedBy: Reason) => {
+    const renderTime = performance.now()
+
+    this.componentsWaiting = Math.max(this.componentsWaiting - 1, 0)
+
+    if (this.spanFactory) {
+      this.triggerNavigationEnd(this.spanFactory, endedBy, renderTime)
+    }
+  }
+
+  configure (configuration: ReactNativeConfiguration, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+    this.spanFactory = spanFactory
+
     // Potential for a navigation to occur
     this.Navigation.events().registerCommandListener((name, params) => {
       this.clearActiveSpan()
       this.startTime = performance.now()
       this.startTimeout = setTimeout(() => {
         this.clearActiveSpan()
-      }, 1000)
+      }, NAVIGATION_START_TIMEOUT)
     })
 
     // Navigation has occurred
     this.Navigation.events().registerComponentDidAppearListener(event => {
       if (typeof this.startTime === 'number') {
         const routeName = event.componentName
-        this.navigationSpan = spanFactory.startSpan('[Navigation]' + routeName, {
+        this.currentNavigationSpan = spanFactory.startSpan('[Navigation]' + routeName, {
           startTime: this.startTime,
           isFirstClass: false,
           parentContext: null
         })
 
-        this.navigationSpan.setAttribute('bugsnag.span.category', 'navigation')
-        this.navigationSpan.setAttribute('bugsnag.navigation.route', routeName)
-        this.navigationSpan.setAttribute('bugsnag.navigation.triggered_by', '@bugsnag/react-native-navigation-performance')
+        this.currentNavigationSpan.setAttribute('bugsnag.span.category', 'navigation')
+        this.currentNavigationSpan.setAttribute('bugsnag.navigation.route', routeName)
+        this.currentNavigationSpan.setAttribute('bugsnag.navigation.triggered_by', '@bugsnag/react-native-navigation-performance')
 
-        this.triggerNavigationEnd(spanFactory)
+        this.triggerNavigationEnd(spanFactory, 'immediate')
       }
     })
   }
