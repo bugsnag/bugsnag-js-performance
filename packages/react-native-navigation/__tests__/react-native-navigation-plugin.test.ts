f8c548c4a733575d77cb8ee385599aa5a15d53c9
Ben Wilson <ben.wilson@smartbear.com>
2024-03-07 10:51:38 +0000

    test: :white_check_mark: test that existing navigation span is discarded on route change

--- packages/react-native-navigation/__tests__/react-native-navigation-plugin.test.ts
+++ packages/react-native-navigation/__tests__/react-native-navigation-plugin.test.ts
@@ -94,4 +94,51 @@ describe('ReactNativeNavigationPlugin', () => {
     expect(span).toHaveAttribute('bugsnag.navigation.triggered_by', '@bugsnag/react-native-navigation-performance')
     expect(span).toHaveAttribute('bugsnag.navigation.ended_by', 'condition')
   })
+
+  it('discards the active navigation span when the route changes', () => {
+    const plugin = new ReactNativeNavigationPlugin(Navigation)
+    const configuration = createConfiguration<ReactNativeConfiguration>()
+    const spanFactory = new MockSpanFactory()
+    plugin.configure(configuration, spanFactory)
+
+    // Simulate a route change
+    jest.mocked(Navigation.events().registerCommandListener).mock.calls[2][0]('push', {
+      commandId: '1',
+      componentId: '1'
+    })
+
+    jest.mocked(Navigation.events().registerComponentDidAppearListener).mock.calls[2][0]({
+      componentId: '1',
+      componentName: 'FirstScreen',
+      componentType: 'Component'
+    })
+
+    jest.advanceTimersByTime(50)
+
+    // Simulate a second route change
+    jest.mocked(Navigation.events().registerCommandListener).mock.calls[2][0]('push', {
+      commandId: '2',
+      componentId: '2'
+    })
+
+    jest.mocked(Navigation.events().registerComponentDidAppearListener).mock.calls[2][0]({
+      componentId: '2',
+      componentName: 'SecondScreen',
+      componentType: 'Component'
+    })
+
+    jest.advanceTimersByTime(100)
+
+    expect(spanFactory.startSpan).toHaveBeenCalledTimes(2)
+    expect(spanFactory.endSpan).toHaveBeenCalledTimes(1)
+    expect(spanFactory.createdSpans).toHaveLength(1)
+
+    const span = spanFactory.createdSpans[0]
+    expect(span.name).toEqual('[Navigation]SecondScreen')
+    expect(span).toHaveAttribute('bugsnag.span.category', 'navigation')
+    expect(span).toHaveAttribute('bugsnag.span.first_class', true)
+    expect(span).toHaveAttribute('bugsnag.navigation.route', 'SecondScreen')
+    expect(span).toHaveAttribute('bugsnag.navigation.triggered_by', '@bugsnag/react-native-navigation-performance')
+    expect(span).toHaveAttribute('bugsnag.navigation.ended_by', 'immediate')
+  })
 })
