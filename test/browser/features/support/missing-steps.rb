c4456958cb54566380ecfcba34d9bb6e5055f445
Ben Wilson <ben.wilson@smartbear.com>
2024-04-12 09:16:04 +0100

    move trace validation steps to browser-steps.rb

--- test/browser/features/support/missing-steps.rb
+++
@@ -1,3 +0,0 @@
-Then('the trace payload field {string} double attribute {string} equals {int}') do |field, attribute, expected|
-    check_attribute_equal field, attribute, 'doubleValue', expected
-end
