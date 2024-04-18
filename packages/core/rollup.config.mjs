e5b28a98262a49b7ea01b68d4d226e69f3b9d02a
Ben Wilson <ben.wilson@smartbear.com>
2024-04-18 14:20:11 +0100

    hotfix: fix rollup config

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -2,6 +2,7 @@ import fs from 'fs'
 import createRollupConfig from '../../.rollup/index.mjs'
 
 export default createRollupConfig({
+  external: ['@bugsnag/cuid'],
   additionalReplacements: {
     __ENABLE_BUGSNAG_TEST_CONFIGURATION__: !!process.env.ENABLE_TEST_CONFIGURATION
   }
