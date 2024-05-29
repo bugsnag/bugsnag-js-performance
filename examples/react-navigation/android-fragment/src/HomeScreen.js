72cb24b52ed86bfa5f5a28439031546d60c210b2
Ben Wilson <ben.wilson@smartbear.com>
2024-05-29 12:31:08 +0100

    add CompleteNavigation component to HomeScreen

--- examples/react-navigation/android-fragment/src/HomeScreen.js
+++ examples/react-navigation/android-fragment/src/HomeScreen.js
@@ -1,15 +1,18 @@
+import { CompleteNavigation } from '@bugsnag/plugin-react-navigation-performance';
 import * as React from 'react';
 import { Button, Text, View } from "react-native";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="See more"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <CompleteNavigation on='mount'>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="See more"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
+    </CompleteNavigation>
   );
 }
 
