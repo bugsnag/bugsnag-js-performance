01be81601153b078531afc91eabacb0f3661143d
Ben Wilson <ben.wilson@smartbear.com>
2024-06-20 11:43:19 +0100

    test: :white_check_mark: add react-native error correlation end to end test

---
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/ErrorCorrelationScenario.js
@@ -0,0 +1,50 @@
+import BugsnagPerformance from '@bugsnag/react-native-performance'
+import Bugsnag from '@bugsnag/react-native'
+import React, { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+import { getCurrentCommand } from '../lib/CommandRunner'
+
+export const config = {
+  appVersion: '1.2.3',
+  maximumBatchSize: 1,
+  autoInstrumentAppStarts: false
+}
+
+export function App () {
+  const [id, setId] = useState(1)
+  const span = useRef()
+
+  useEffect(() => {
+    (async () => {
+      console.error(`[BugsnagPerformance] ErrorCorrelationScenario waiting for command...`)
+
+      const command = await getCurrentCommand(Infinity)
+
+      console.error(`[BugsnagPerformance] ErrorCorrelationScenario got command: ${JSON.stringify(command)}`)
+      console.error(`[BugsnagPerformance] ErrorCorrelationScenario has id: ${id}`)
+
+      switch (command.action) {
+        case 'start-span':
+            span.current = BugsnagPerformance.startSpan(`ErrorCorrelationScenario ${id}`)
+            setId(id => id + 1)
+            break
+        case 'end-span':
+            span.current?.end()
+            setId(id => id + 1)
+            break
+        case 'send-error':
+            Bugsnag.notify(new Error(`ErrorCorrelationScenario`))
+            break
+        default:
+            throw new Error(`Unknown command: ${JSON.stringify(command)}`)
+      }
+    })()
+  }, [id])
+
+  return (
+    <View>
+      <Text>ErrorCorrelationScenario</Text>
+      <Text>ID: {id}</Text>
+    </View>
+  )
+}
