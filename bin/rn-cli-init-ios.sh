81790a872764100733db3c58aa83958904e035eb
Ben Wilson <ben.wilson@smartbear.com>
2024-07-26 10:45:10 +0100

    update notifier version to 8.0.0-alpha.11

--- bin/rn-cli-init-ios.sh
+++ bin/rn-cli-init-ios.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
 
 set timeout -1
-set notifierVersion "next";
+set notifierVersion "8.0.0-alpha.11"
 set notifierApiKey "1234567890abcdef1234567890abcdef"
-set fixtureDirectory [lindex $argv 0];
-set rnVersion [lindex $argv 1];
+set fixtureDirectory [lindex $argv 0]
+set rnVersion [lindex $argv 1]
 set rnVersionInt ""
 set substringToTrim ".expo.ejected"
 
