38abd8f77f670c28a3bc33d1c52bcf006848e12f
Joe Haines <hello@joehaines.co.uk>
2024-07-30 11:59:17 +0100

    Remove 24 hour probability fetch timeout

--- packages/core/tests/probability-manager.test.ts
+++ packages/core/tests/probability-manager.test.ts
@@ -7,7 +7,7 @@ import { InMemoryDelivery } from '@bugsnag/js-performance-test-utilities'
 jest.useFakeTimers()
 
 describe('ProbabilityManager', () => {
-  it('uses the configured probability and fetches a new one immediately if there is no persisted value', async () => {
+  it('uses the configured probability if there is no persisted value', async () => {
     const persistence = new InMemoryPersistence()
     const delivery = new InMemoryDelivery()
     delivery.setNextSamplingProbability(0.5)
@@ -26,29 +26,13 @@ describe('ProbabilityManager', () => {
     // the configured probability should not be persisted
     expect(await persistence.load('bugsnag-sampling-probability')).toBeUndefined()
 
-    // a request will only be made after a macro task
+    await jest.advanceTimersByTimeAsync(1)
     expect(delivery.samplingRequests).toHaveLength(0)
 
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(1)
-
-    // the value fetched from the server should be persisted
-    expect(await persistence.load('bugsnag-sampling-probability')).toStrictEqual({
-      value: 0.5,
-      // the time will be 1ms ago as jest's timer has advanced 1ms after the
-      // persistence happened
-      time: Date.now() - 1
-    })
-
-    // after 1 day, another request should be made
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 2)
-    expect(delivery.samplingRequests).toHaveLength(1)
-
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(2)
+    expect(await persistence.load('bugsnag-sampling-probability')).toBeUndefined()
   })
 
-  it('uses the persisted probability and fetches a new one immediately if the persisted value is too old', async () => {
+  it('uses the persisted probability if the persisted value is too old', async () => {
     const persistence = new InMemoryPersistence()
     persistence.save('bugsnag-sampling-probability', {
       value: 0.25,
@@ -69,29 +53,11 @@ describe('ProbabilityManager', () => {
 
     expect(sampler.probability).toBe(0.25)
 
-    // a request will only be made after a macro task
+    await jest.runOnlyPendingTimersAsync()
     expect(delivery.samplingRequests).toHaveLength(0)
-
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(1)
-
-    // the value fetched from the server should be persisted
-    expect(await persistence.load('bugsnag-sampling-probability')).toStrictEqual({
-      value: 0.5,
-      // the time will be 1ms ago as jest's timer has advanced 1ms after the
-      // persistence happened
-      time: Date.now() - 1
-    })
-
-    // after 1 day, another request should be made
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 2)
-    expect(delivery.samplingRequests).toHaveLength(1)
-
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(2)
   })
 
-  it('uses the persisted probability and fetches a new one when it expires if the persisted value is recent', async () => {
+  it('uses the persisted probability if the persisted value is recent', async () => {
     const persistence = new InMemoryPersistence()
     persistence.save('bugsnag-sampling-probability', {
       value: 0.25,
@@ -112,25 +78,11 @@ describe('ProbabilityManager', () => {
 
     expect(sampler.probability).toBe(0.25)
 
-    // a request should not be made until the probability expires
-    await jest.advanceTimersByTimeAsync(1)
+    await jest.runOnlyPendingTimersAsync()
     expect(delivery.samplingRequests).toHaveLength(0)
-
-    // after 1 day - 30 seconds, another request should be made
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 30_000 - 2)
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(1)
-
-    // the value fetched from the server should be persisted
-    expect(await persistence.load('bugsnag-sampling-probability')).toStrictEqual({
-      value: 0.5,
-      time: Date.now()
-    })
   })
 
-  it('persists new probability values and fetches a new one after they expire', async () => {
+  it('persists new probability values', async () => {
     const persistence = new InMemoryPersistence()
     const delivery = new InMemoryDelivery()
     delivery.setNextSamplingProbability(0.5)
@@ -152,62 +104,7 @@ describe('ProbabilityManager', () => {
       time: Date.now()
     })
 
+    await jest.runOnlyPendingTimersAsync()
     expect(delivery.samplingRequests).toHaveLength(0)
-
-    // after 1 day a request for a new probability should be made
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 1)
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(1)
-  })
-
-  it('refreshes the expiration timer when a new probability is set', async () => {
-    const persistence = new InMemoryPersistence()
-    const delivery = new InMemoryDelivery()
-    delivery.setNextSamplingProbability(0.5)
-
-    const sampler = new Sampler(0.75)
-    const fetcher = new ProbabilityFetcher(delivery, 'api key')
-
-    const manager = await ProbabilityManager.create(
-      persistence,
-      sampler,
-      fetcher
-    )
-
-    await manager.setProbability(0.25)
-
-    // the new probability should be persisted
-    expect(await persistence.load('bugsnag-sampling-probability')).toStrictEqual({
-      value: 0.25,
-      time: Date.now()
-    })
-
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    // wait just less than 1 day
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 1)
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    await manager.setProbability(0.8)
-
-    expect(await persistence.load('bugsnag-sampling-probability')).toStrictEqual({
-      value: 0.8,
-      time: Date.now()
-    })
-
-    // as we just set a new probability, a request shouldn't be made
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    // wait just less than 1 day again (we've already just waited 1 ms above)
-    await jest.advanceTimersByTimeAsync((24 * 60 * 60 * 1000) - 2)
-    expect(delivery.samplingRequests).toHaveLength(0)
-
-    // now the probability we just set has expired, so we should refresh it from
-    // the server
-    await jest.advanceTimersByTimeAsync(1)
-    expect(delivery.samplingRequests).toHaveLength(1)
   })
 })
