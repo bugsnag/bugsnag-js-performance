d3dceed9ef136a0e527a87b674c10a709270bac7
Yousif Ahmed <yousif@bugsnag.com>
2024-08-01 12:11:25 +0100

    test(react-native): generate 0.64 fixture

---
+++ test/react-native/features/fixtures/app/android/MainActivity.0.64.java
@@ -0,0 +1,25 @@
+package com.bugsnag.fixtures.reactnative.performance;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+/**
+   * Required for react-navigation/native implementation
+   * https://reactnavigation.org/docs/getting-started/#installing-dependencies-into-a-bare-react-native-project
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "reactnative";
+  }
+}
