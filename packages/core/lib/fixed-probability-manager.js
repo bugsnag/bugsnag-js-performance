235e9c9e1b9d5b3b443114cab5843791b2ffb540
Dan Skinner <dan.skinner@smartbear.com>
2024-08-06 11:15:39 +0100

    add a fixed sampling probability configuration option

--- packages/core/lib/fixed-probability-manager.js
+++ packages/core/lib/fixed-probability-manager.js
@@ -1,17 +1,20 @@
 class FixedProbabilityManager {
-    static async create(sampler, samplingProbability) {
-        sampler.probability = samplingProbability;
-        return new FixedProbabilityManager(sampler, samplingProbability);
-    }
-    constructor(sampler, samplingProbability) {
-        this.sampler = sampler;
-        this.samplingProbability = samplingProbability;
-    }
-    setProbability(newProbability) {
-        return Promise.resolve();
-    }
-    ensureFreshProbability() {
-        return Promise.resolve();
-    }
+  static async create (sampler, samplingProbability) {
+    sampler.probability = samplingProbability
+    return new FixedProbabilityManager(sampler, samplingProbability)
+  }
+
+  constructor (sampler, samplingProbability) {
+    this.sampler = sampler
+    this.samplingProbability = samplingProbability
+  }
+
+  setProbability (newProbability) {
+    return Promise.resolve()
+  }
+
+  ensureFreshProbability () {
+    return Promise.resolve()
+  }
 }
-export default FixedProbabilityManager;
+export default FixedProbabilityManager
