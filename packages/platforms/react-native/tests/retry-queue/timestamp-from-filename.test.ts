06c1cfb360fcbfb7606f0d1f58c4ec34f601ef5b
Yousif Ahmed <yousif@bugsnag.com>
2024-08-06 10:44:25 +0100

    test(react-native): simplify timestampFromFilename test

--- packages/platforms/react-native/tests/retry-queue/timestamp-from-filename.test.ts
+++ packages/platforms/react-native/tests/retry-queue/timestamp-from-filename.test.ts
@@ -2,16 +2,14 @@ import timestampFromFilename from '../../lib/retry-queue/timestamp-from-filename
 
 describe('timestampFromFilename', () => {
   it.each([
-    ['retry-0-a.json', '0'],
-    ['retry-1-a.json', '1'],
-    ['retry-200000-a.json', '200000'],
-    ['retry-1234567890-a.json', '1234567890'],
-    ['retry-00000000000-a.json', '0'],
-    ['retry-999999999999999999999999999999999999-a.json', '999999999999999999999999999999999999']
+    ['retry-0-a.json', 0],
+    ['retry-1-a.json', 1],
+    ['retry-200000-a.json', 200000],
+    ['retry-1234567890-a.json', 1234567890],
+    ['retry-00000000000-a.json', 0],
+    ['retry-999999999999999999999999999999999999-a.json', 1e+36]
   ])('parses %s into the timestamp %s', (input, expected) => {
-    const actual = timestampFromFilename(input)
-    expect(actual).toBe(Number(expected))
-    expect(Number.isNaN(actual)).toBe(false)
+    expect(timestampFromFilename(input)).toBe(expected)
   })
 
   it.each([
