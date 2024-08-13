84cd3360ba39e764c9ffc29e78314bf1f5ca8c95
Ben Wilson <ben.wilson@smartbear.com>
2024-08-13 10:19:47 +0100

    add init method to fixture

--- test/react-native/features/fixtures/scenario-launcher/scenarios/ErrorCorrelationScenario.js
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/ErrorCorrelationScenario.js
@@ -1,8 +1,17 @@
 import BugsnagPerformance from '@bugsnag/react-native-performance'
 import Bugsnag from '@bugsnag/react-native'
 import React, { useEffect, useState, useRef } from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, Text, View, NativeModules } from 'react-native'
 import { getCurrentCommand } from '../lib/CommandRunner'
+
+const { ScenarioLauncher } = NativeModules;
+
+export function init () {
+  console.log({ ScenarioLauncher, NativeModules })
+  if (ScenarioLauncher) {
+    ScenarioLauncher.startBugsnag()
+  }
+}
 
 export const config = {
   appVersion: '1.2.3',
@@ -15,6 +24,25 @@ export function App() {
   const [id, setId] = useState(1)
   const span = useRef()
 
+  function startSpan () {
+    console.log("[BugsnagPerformance] ErrorCorrelationScenario starting span")
+    span.current = BugsnagPerformance.startSpan(`ErrorCorrelationScenario ${id}`)
+  }
+  
+  function endSpan () {
+    if (span.current) {
+      console.log("[BugsnagPerformance] ErrorCorrelationScenario ending span")
+      span.current?.end()
+    } else {
+      console.log("[BugsnagPerformance] ErrorCorrelationScenario no span to end")
+    }
+  }
+
+  function sendError () {
+    console.log("[BugsnagPerformance] ErrorCorrelationScenario sending error")
+    Bugsnag.notify(new Error("ErrorCorrelationScenario"))
+  }
+
   useEffect(() => {
     (async () => {
       console.error(`[BugsnagPerformance] ErrorCorrelationScenario waiting for command...`)
@@ -26,28 +54,30 @@ export function App() {
 
       switch (command.action) {
         case 'start-span':
-          span.current = BugsnagPerformance.startSpan(`ErrorCorrelationScenario ${id}`)
+          startSpan()
           setId(id => id + 1)
           break
         case 'end-span':
-          span.current?.end()
+          endSpan()
           setId(id => id + 1)
           break
         case 'send-error':
-          console.log("[BugsnagPerformance] ErrorCorrelationScenario sending error")
-          Bugsnag.notify(new Error(`ErrorCorrelationScenario`))
+          sendError()
           setId(id => id + 1)
           break
         default:
           throw new Error(`Unknown command: ${JSON.stringify(command)}`)
       }
     })()
-  }, [id])
+  }, [id, startSpan, endSpan, sendError])
 
   return (
     <View>
       <Text>ErrorCorrelationScenario</Text>
       <Text>Current Command ID: {id}</Text>
+      <Button onPress={() => startSpan()} title="startSpan" />
+      <Button onPress={() => sendError()} title="sendError" />
+      <Button onPress={() => endSpan()} title="endSpan" />
     </View>
   )
 }
