ea627c070bc9945fe852d0b2081ee489211af68c
Ben Wilson <ben.wilson@smartbear.com>
2024-08-29 17:49:38 +0100

    test: :white_check_mark: add end to end test for app starts when using wrapApp

---
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/AppStartWrapperScenario.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+import BugsnagPerformance from '@bugsnag/react-native-performance'
+
+export const config = {
+  maximumBatchSize: 1,
+  autoInstrumentAppStarts: false
+}
+
+export const App = BugsnagPerformance.wrapApp(() => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.scenario}>
+        <Text>AppStartScenario</Text>
+      </View>
+    </SafeAreaView>
+  )
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  scenario: {
+    flex: 1
+  }
+})
