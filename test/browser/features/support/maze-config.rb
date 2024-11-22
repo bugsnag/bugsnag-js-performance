fc73fea502cb7a023e74f23f3524939d84b5c616
Dan Skinner <dan.skinner@smartbear.com>
2024-11-22 14:52:39 +0000

    run e2e tests over https

--- test/browser/features/support/maze-config.rb
+++ test/browser/features/support/maze-config.rb
@@ -11,16 +11,14 @@ Maze.config.enforce_bugsnag_integrity = false
 def get_test_url()
   if Maze.config.aws_public_ip
     maze_address = Maze.public_address
-    document_address = Maze.public_document_server_address
   else
     host = ENV.fetch('HOST', 'localhost')
     maze_address = "#{host}:9339"
-    document_address = "#{host}:9339"
   end
 
   UrlGenerator.new(
     URI("https://#{maze_address}"),
-    URI("https://#{document_address}")
+    URI("https://#{maze_address}")
   )
 end
 
