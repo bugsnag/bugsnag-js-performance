ab5d944efe5bd9b3bc69bbf06b6b7522087ce802
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-01-14 11:51:25 +0100

    add setAppState to react-navigation-native-plugin

--- packages/plugin-react-navigation/lib/react-navigation-native-plugin.ts
+++ packages/plugin-react-navigation/lib/react-navigation-native-plugin.ts
@@ -2,17 +2,20 @@ import type { Plugin, SpanFactory } from '@bugsnag/core-performance'
 import type { ReactNativeConfiguration } from '@bugsnag/react-native-performance'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNavigationContainer } from './create-navigation-container'
+import type { AppState } from '../../core/lib/core'
 
 class BugsnagPluginReactNavigationNativePerformance implements Plugin<ReactNativeConfiguration> {
   private spanFactory?: SpanFactory<ReactNativeConfiguration>
+  private setAppState?: (appState: AppState) => void
 
-  configure (_configuration: ReactNativeConfiguration, spanFactory: SpanFactory<ReactNativeConfiguration>) {
+  configure (_configuration: ReactNativeConfiguration, spanFactory: SpanFactory<ReactNativeConfiguration>, setAppState: (appState: AppState) => void) {
     this.spanFactory = spanFactory
+    this.setAppState = setAppState
   }
 
   createNavigationContainer = (Container = NavigationContainer) => {
-    if (!this.spanFactory) throw new Error('Bugsnag: BugsnagPluginReactNavigationNativePerformance not configured')
-    return createNavigationContainer(Container, this.spanFactory) as typeof Container
+    if (!this.spanFactory || !this.setAppState) throw new Error('Bugsnag: BugsnagPluginReactNavigationNativePerformance not configured')
+    return createNavigationContainer(Container, this.spanFactory, this.setAppState) as typeof Container
   }
 }
 
