0f6644d106d3031c3b5733d97cbf7e88c79f1f7e
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-01-31 17:48:57 +0100

    change import for bugsnag

--- packages/instrumented-component-lifecycle/instrumented-component-lifecycle.tsx
+++ packages/instrumented-component-lifecycle/instrumented-component-lifecycle.tsx
@@ -1,53 +1,52 @@
-import type { Clock, SetAppState, SpanFactory, SpanInternal } from '@bugsnag/core-performance'
-import type { BrowserConfiguration } from '@bugsnag/browser-performance'
+import BugsnagPerformance from '@bugsnag/browser-performance'
+import type { Span } from '@bugsnag/core-performance'
 import React from 'react'
-import type { PropsWithChildren, ReactNode } from 'react'
+import type { PropsWithChildren } from 'react'
 
 interface InstrumentedComponentLifecycleProps extends PropsWithChildren {
   name: string
   includeComponentUpdates: boolean
-  children: ReactNode
-  updateProps: Record<string, unknown>
-  spanFactory: SpanFactory<BrowserConfiguration>
-  setAppState: SetAppState
 }
 
 class InstrumentedComponentLifecycle extends React.Component<InstrumentedComponentLifecycleProps> {
-  private componentMountSpan: SpanInternal
-  private componentUpdateSpan: SpanInternal | undefined
-  private readonly spanFactory: SpanFactory<BrowserConfiguration>
-  private readonly clock: Clock
+  private componentMountSpan: Span | undefined
+  private componentUpdateSpan: Span | undefined
+  private componentUnmountSpan: Span | undefined
 
   public static defaultProps: Partial<InstrumentedComponentLifecycleProps> = {
     includeComponentUpdates: true
   }
 
-  public constructor (props: InstrumentedComponentLifecycleProps, spanFactory: SpanFactory<BrowserConfiguration>, clock: Clock) {
+  public constructor (props: InstrumentedComponentLifecycleProps) {
     super(props)
     const { name } = this.props
 
-    this.componentMountSpan = this.props.spanFactory.startSpan(`${name} component mount span`, { startTime: 0 })
-
-    this.spanFactory = spanFactory
-    this.clock = clock
+    this.componentMountSpan = BugsnagPerformance.appState !== 'ready' ? BugsnagPerformance.startSpan(`[ViewLoad/Component]${name}`) : undefined
   }
 
   async componentDidMount () {
-    if (this.componentMountSpan) {
-      this.spanFactory.endSpan(this.componentMountSpan, this.clock.now())
+    if (this.componentMountSpan && BugsnagPerformance.appState === 'ready') {
+      this.componentMountSpan.end();
     }
   }
 
-  public shouldComponentUpdate ({ updateProps, includeComponentUpdates = true }: InstrumentedComponentLifecycleProps): boolean {
-    if (includeComponentUpdates && this.componentMountSpan && updateProps !== this.props.updateProps) {
-      this.componentUpdateSpan = this.spanFactory.startSpan(`${this.props.name} component update span`, { startTime: 0 })
+  public shouldComponentUpdate ({ includeComponentUpdates = true }: InstrumentedComponentLifecycleProps, nextProps: any): boolean {
+    if (includeComponentUpdates && this.componentMountSpan && nextProps !== this.props.children) {
+      this.componentUpdateSpan = BugsnagPerformance.startSpan(`[ViewLoadPhase/Update]${this.props.name}`)
     }
     return true
   }
 
   public componentDidUpdate () {
-    if (this.componentUpdateSpan) {
-      this.spanFactory.endSpan(this.componentUpdateSpan, this.clock.now())
+    if (this.componentUpdateSpan && BugsnagPerformance.appState === 'ready') {
+      this.componentUpdateSpan.end()
+    }
+  }
+
+  public componentWillUnmount() {
+    this.componentUnmountSpan = BugsnagPerformance.appState !== 'ready' ? BugsnagPerformance.startSpan(`[ViewLoadPhase/Component]${this.props.name}`) : undefined
+    if (this.componentUnmountSpan && BugsnagPerformance.appState === 'ready') {
+      this.componentUnmountSpan.end()
     }
   }
 
@@ -58,7 +57,7 @@ class InstrumentedComponentLifecycle extends React.Component<InstrumentedCompone
 
 export function withInstrumentedComponentLifecycle<P extends Record<string, any>> (
   Component: React.ComponentType<P>,
-  options?: Pick<Partial<InstrumentedComponentLifecycleProps>, Exclude<keyof InstrumentedComponentLifecycleProps, 'updateProps' | 'children'>>
+  options?: Partial<InstrumentedComponentLifecycleProps>
 ): React.FC<P> {
   const componentDisplayName = (options && options.name) || Component.displayName || Component.name || 'unknown'
 
