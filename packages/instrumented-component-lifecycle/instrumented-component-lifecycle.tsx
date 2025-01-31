944b0261b5da0e47f4c33d81723f2949d23bf233
AnastasiiaSvietlova <anastasiia.svietlova@smartbear.com>
2025-01-31 12:43:57 +0100

    fix lint

--- packages/instrumented-component-lifecycle/instrumented-component-lifecycle.tsx
+++ packages/instrumented-component-lifecycle/instrumented-component-lifecycle.tsx
@@ -3,12 +3,11 @@ import type { BrowserConfiguration } from '@bugsnag/browser-performance'
 import React from 'react'
 import type { PropsWithChildren, ReactNode } from 'react'
 
-
 interface InstrumentedComponentLifecycleProps extends PropsWithChildren {
   name: string
   includeComponentUpdates: boolean
   children: ReactNode
-  updateProps: { [key: string]: unknown }
+  updateProps: Record<string, unknown>
   spanFactory: SpanFactory<BrowserConfiguration>
   setAppState: SetAppState
 }
@@ -23,7 +22,7 @@ class InstrumentedComponentLifecycle extends React.Component<InstrumentedCompone
     includeComponentUpdates: true
   }
 
-  public constructor(props: InstrumentedComponentLifecycleProps, spanFactory: SpanFactory<BrowserConfiguration>, clock: Clock) {
+  public constructor (props: InstrumentedComponentLifecycleProps, spanFactory: SpanFactory<BrowserConfiguration>, clock: Clock) {
     super(props)
     const { name } = this.props
 
@@ -33,33 +32,33 @@ class InstrumentedComponentLifecycle extends React.Component<InstrumentedCompone
     this.clock = clock
   }
 
-  async componentDidMount() {
+  async componentDidMount () {
     if (this.componentMountSpan) {
       this.spanFactory.endSpan(this.componentMountSpan, this.clock.now())
     }
   }
 
-  public shouldComponentUpdate({ updateProps, includeComponentUpdates = true }: InstrumentedComponentLifecycleProps): boolean {
+  public shouldComponentUpdate ({ updateProps, includeComponentUpdates = true }: InstrumentedComponentLifecycleProps): boolean {
     if (includeComponentUpdates && this.componentMountSpan && updateProps !== this.props.updateProps) {
       this.componentUpdateSpan = this.spanFactory.startSpan(`${this.props.name} component update span`, { startTime: 0 })
     }
     return true
   }
 
-  public componentDidUpdate() {
+  public componentDidUpdate () {
     if (this.componentUpdateSpan) {
       this.spanFactory.endSpan(this.componentUpdateSpan, this.clock.now())
     }
   }
 
-  public render(): React.ReactNode {
+  public render (): React.ReactNode {
     return this.props.children
   }
 }
 
-function withInstrumentedComponentLifecycle<P extends Record<string, any>>(
+function withInstrumentedComponentLifecycle<P extends Record<string, any>> (
   Component: React.ComponentType<P>,
-  options?: Pick<Partial<InstrumentedComponentLifecycleProps>,Exclude<keyof InstrumentedComponentLifecycleProps, "updateProps" | "children">>
+  options?: Pick<Partial<InstrumentedComponentLifecycleProps>, Exclude<keyof InstrumentedComponentLifecycleProps, "updateProps" | "children">>
 ): React.FC<P> {
   const componentDisplayName = (options && options.name) || Component.displayName || Component.name || 'unknown'
 
@@ -73,3 +72,5 @@ function withInstrumentedComponentLifecycle<P extends Record<string, any>>(
 
   return Wrapped;
 }
+
+export { withInstrumentedComponentLifecycle }
