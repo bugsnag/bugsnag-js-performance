0be85d0a7f2dab580cacc1189fcbf1a14a828c5a
jason <lemnik@users.noreply.github.com>
2025-05-29 11:20:05 +0100

    feat(remote-spans): added a new `plugin-react-native-remote-spans` module

---
+++ packages/plugin-react-native-remote-spans/ios/BugsnagRemoteSpans.h
@@ -0,0 +1,10 @@
+#import <React/RCTBridgeModule.h>
+#import <BugsnagRemoteSpansSpec/BugsnagRemoteSpansSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BugsnagRemoteSpans: NSObject <NativeRemoteSpansSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
