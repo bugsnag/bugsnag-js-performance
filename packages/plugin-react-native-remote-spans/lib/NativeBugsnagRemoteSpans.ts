7c90f47ac120b20cb555e30107712b91d70b5dda
jason <lemnik@users.noreply.github.com>
2025-06-03 16:43:43 +0100

    feat(android): add support for remote control of spans on android

--- packages/plugin-react-native-remote-spans/lib/NativeBugsnagRemoteSpans.ts
+++ packages/plugin-react-native-remote-spans/lib/NativeBugsnagRemoteSpans.ts
@@ -1,14 +1,29 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport'
+import type { UnsafeObject } from 'react-native/Libraries/Types/CodegenTypes'
 import { TurboModuleRegistry } from 'react-native'
 
 export type SpanId = {
-  id: string
+  spanId: string
   traceId: string
+}
+
+type SpanAttribute = {
+  name: string
+  value: UnsafeObject
+}
+
+export type SpanUpdateTransaction = {
+  attributes: SpanAttribute[]
+  isEnded: boolean
+  endTimestamp: string | undefined
+  endDatetime: string | undefined
 }
 
 export interface Spec extends TurboModule {
   getSpanIdByName: (spanName: string) => SpanId | undefined
+
+  updateSpan: (spanId: SpanId, updates: SpanUpdateTransaction) => Promise<boolean>
 }
 
 export default TurboModuleRegistry.get<Spec>(
--- packages/plugin-react-native-remote-spans/lib/index.ts
+++ packages/plugin-react-native-remote-spans/lib/index.ts
@@ -1,32 +1,70 @@
 import { TurboModuleRegistry } from 'react-native'
-import type { Spec } from './NativeBugsnagRemoteSpans'
-import type { ParentContext } from '@bugsnag/core-performance'
+import { SpanQuery } from '@bugsnag/core-performance/lib/span-control-provider'
+import type { SpanUpdateTransaction, Spec } from './NativeBugsnagRemoteSpans'
+import type { ParentContext, SpanAttribute, Time } from '@bugsnag/core-performance'
+import type { SpanControlProvider } from '@bugsnag/core-performance/lib/span-control-provider'
 
-export class NativeSpanQuery {
+export class NativeSpanQuery extends SpanQuery<NativeSpanControl> {
   constructor (public readonly name: string) {
+    super()
   }
 }
 
+export interface NativeSpanMutator {
+  end: (endTime?: Time) => void
+  setAttribute: (name: string, value: SpanAttribute) => void
+}
+
 export interface NativeSpanControl extends ParentContext {
+  updateSpan: (update: (mutator: NativeSpanMutator) => void) => Promise<boolean>
 }
 
 const NativeRemoteSpansModule = TurboModuleRegistry.get<Spec>('BugsnagRemoteSpans')
 
 class NativeSpanControlImpl implements NativeSpanControl {
-  constructor (public readonly id: string,
+  constructor (
+    public readonly id: string,
     public readonly traceId: string) {
+  }
+
+  updateSpan (update: (mutator: NativeSpanMutator) => void): Promise<boolean> {
+    const transaction: SpanUpdateTransaction = {
+      attributes: [],
+      isEnded: false,
+      endTimestamp: undefined,
+      endDatetime: undefined
+    }
+
+    update({
+      end: (endTime?: Time) => {
+        if (endTime instanceof Date) {
+          transaction.endDatetime = endTime.toISOString()
+        } else {
+          // FIXME: This needs to be clock.toUnixTimestampNanoseconds(endTime)
+          transaction.endTimestamp = endTime?.toString()
+        }
+
+        transaction.isEnded = true
+      },
+      setAttribute: (name: string, value: SpanAttribute) => {
+        transaction.attributes.push({ name, value: value as any })
+      }
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return NativeRemoteSpansModule!.updateSpan({ spanId: this.id, traceId: this.traceId }, transaction)
   }
 }
 
-export class NativeSpanControlProvider {
-  get (query: any): NativeSpanControl | undefined {
+export class NativeSpanControlProvider implements SpanControlProvider<NativeSpanControl> {
+  getSpanControls<Q> (query: Q): NativeSpanControl | null {
     if (query instanceof NativeSpanQuery && NativeRemoteSpansModule) {
       const spanId = NativeRemoteSpansModule.getSpanIdByName(query.name)
       if (spanId) {
-        return new NativeSpanControlImpl(spanId.id, spanId.traceId)
+        return new NativeSpanControlImpl(spanId.spanId, spanId.traceId)
       }
     }
 
-    return undefined
+    return null
   }
 }
