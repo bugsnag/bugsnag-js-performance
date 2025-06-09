6e4b78b271ee6d978188941afd83bc46382d5b49
Ben Wilson <ben.wilson@smartbear.com>
2025-06-09 12:34:36 +0100

    refactor: :recycle: use defaultRouteResolver instead of / as fallback

--- packages/svelte-kit/lib/svelte-kit-routing-provider.ts
+++ packages/svelte-kit/lib/svelte-kit-routing-provider.ts
@@ -1,18 +1,18 @@
 import type { RouteChangeSpan, RoutingProvider, StartRouteChangeCallback } from '@bugsnag/browser-performance'
-import { onSettle } from '@bugsnag/browser-performance'
+import { onSettle, defaultRouteResolver } from '@bugsnag/browser-performance'
 import type { AfterNavigate, BeforeNavigate } from '@sveltejs/kit'
 
 type NavigateCallback<T> = (callback: (navigation: T) => void) => void
 
 export class SvelteKitRoutingProvider implements RoutingProvider {
-  private currentRoute = '/'
+  private currentRoute: string | undefined
 
   constructor (private beforeNavigate: NavigateCallback<BeforeNavigate>, private afterNavigate: NavigateCallback<AfterNavigate>) {}
 
   // resolveRoute is only ever used to get the current route in SvelteKit
   // so we just return the current route and ignore the parameter
-  resolveRoute (urlOrRoute: URL | string) {
-    return this.currentRoute
+  resolveRoute (url: URL) {
+    return this.currentRoute || defaultRouteResolver(url)
   }
 
   listenForRouteChanges (startRouteChangeSpan: StartRouteChangeCallback) {
@@ -20,13 +20,17 @@ export class SvelteKitRoutingProvider implements RoutingProvider {
 
     this.beforeNavigate(({ to, type }) => {
       const startTime = performance.now()
-      this.currentRoute = to?.route.id ?? '/'
-      const url = to?.url || '/'
+      if (to && to.route.id) {
+        this.currentRoute = to.route.id
+      }
+      const url = to ? to.url : new URL(window.location.href)
       currentSpan = startRouteChangeSpan(url, type, { startTime })
     })
 
     this.afterNavigate(({ to }) => {
-      this.currentRoute = to?.route.id ?? '/'
+      if (to && to.route.id) {
+        this.currentRoute = to.route.id
+      }
 
       onSettle((endTime) => {
         if (currentSpan) {
