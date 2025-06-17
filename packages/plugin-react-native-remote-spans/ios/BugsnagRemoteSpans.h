44741b0602bbcf8c67edd186024e59d406859662
Yousif Ahmed <yousif@bugsnag.com>
2025-06-17 10:44:47 +0100

    fix: fix remote spans ios module

--- packages/plugin-react-native-remote-spans/ios/BugsnagRemoteSpans.h
+++ packages/plugin-react-native-remote-spans/ios/BugsnagRemoteSpans.h
@@ -3,7 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface BugsnagRemoteSpans: NSObject <NativeRemoteSpansSpec>
+@interface BugsnagRemoteSpans: NSObject <NativeBugsnagRemoteSpansSpec>
 
 @end
 
--- packages/plugin-react-native-remote-spans/ios/BugsnagRemoteSpans.mm
+++ packages/plugin-react-native-remote-spans/ios/BugsnagRemoteSpans.mm
@@ -7,10 +7,17 @@ RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getSpanIdByName:(NSString *)spanName) {
   return nil;
 }
 
+RCT_EXPORT_METHOD(updateSpan:(NSDictionary *)spanId
+                updates:(NSDictionary *)updates
+                resolve:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject) {
+  resolve(nil);
+}
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativeRemoteSpansSpecJSI>(params);
+    return std::make_shared<facebook::react::NativeBugsnagRemoteSpansSpecJSI>(params);
 }
 
 @end
