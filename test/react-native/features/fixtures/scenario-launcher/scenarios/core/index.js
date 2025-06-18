f2d0a7dd1eaf000961c07aa73f26aa7947abe516
Yousif Ahmed <yousif@bugsnag.com>
2025-06-18 13:00:18 +0100

    test(react-native): only include native integration scenarios for native integration builds

--- test/react-native/features/fixtures/scenario-launcher/scenarios/core/index.js
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/core/index.js
@@ -17,9 +17,3 @@ export * as NamedSpansPluginScenario from './NamedSpansPluginScenario'
 
 // React Navigation Scenarios
 export * as ReactNavigationScenario from './ReactNavigationScenario'
-
-// Native Integration Scenarios
-export * as NativeIntegrationScenario from './NativeIntegrationScenario'
-export * as RemoteParentContextNativeScenario from './RemoteParentContextNativeScenario'
-export * as RemoteParentContextJSScenario from './RemoteParentContextJSScenario'
-export * as RemoteSpansScenario from './RemoteSpansScenario'
---
+++ test/react-native/features/fixtures/scenario-launcher/scenarios/native-integration/index.js
@@ -0,0 +1,5 @@
+// Native Integration Scenarios
+export * as NativeIntegrationScenario from './NativeIntegrationScenario'
+export * as RemoteParentContextNativeScenario from './RemoteParentContextNativeScenario'
+export * as RemoteParentContextJSScenario from './RemoteParentContextJSScenario'
+export * as RemoteSpansScenario from './RemoteSpansScenario'
