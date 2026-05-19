f257e5899e5bb64bf1f08687ec203bde3697ef52
SB-jigneshR <jignesh.raiyani@smartbear.com>
2026-05-19 14:13:36 +0530

    PLAT-15943 added otel attribute

--- packages/platforms/browser/lib/auto-instrumentation/resource-load-plugin.ts
+++ packages/platforms/browser/lib/auto-instrumentation/resource-load-plugin.ts
@@ -106,8 +106,8 @@ export class ResourceLoadPlugin implements Plugin<BrowserConfiguration> {
           }
 
           if (entry.encodedBodySize && entry.decodedBodySize) {
-            span.setAttribute('http.response_content_length', entry.encodedBodySize)
-            span.setAttribute('http.response_content_length_uncompressed', entry.decodedBodySize)
+            span.setAttribute('http.response.header.content-length', entry.encodedBodySize)
+            span.setAttribute('http.response.body.size', entry.decodedBodySize)
           }
 
           if (entry.responseStatus) {
